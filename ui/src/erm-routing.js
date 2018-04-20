bbae73a362353eb73fce083bdae1e8e64c4ee7eb
Ian Ibbotson <ianibbo@gmail.com>
2018-04-20 14:41:19 +0100

    Working on packages and platforms

--- ui/src/erm-routing.js
+++ ui/src/erm-routing.js
@@ -3,6 +3,8 @@ import Route from 'react-router-dom/Route'
 import Switch from 'react-router-dom/Switch'
 import Home from './home'
 import Dash from './dash'
+import Packages from './packages'
+import Platforms from './platforms'
 import app from './lib/folio-stripes/app'
 
 import * as mobx from 'mobx'
@@ -25,11 +27,15 @@ class ERMRouting extends Component {
   }
   
   render() {
+    // Steve: Can you add a comment here explaining how the default route works here, or what mechanism
+    // is in force that causes the Home page to be rendered. I can't make the NoMatch route work.
     return (
       <div className="erm" >
         <Switch >
-          <Route history={this.props.history} path="/erm/dash" render={() => <Dash app={app} />} />
           <Route history={this.props.history} path="/erm" render={() => <Home app={app} />} />
+          <Route history={this.props.history} path="/erm/platforms" render={() => <Platforms app={app} />} />
+          <Route history={this.props.history} path="/erm/packages" render={() => <Packages app={app} />} />
+          <Route history={this.props.history} path="/erm/dash" render={() => <Dash app={app} />} />
           <Route component={() => { NoMatch(); }} />
         </Switch>
       </div>
