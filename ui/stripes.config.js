764756ebad9365916a8f01451e4842c06370a924
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-04-30 09:54:15 +0100

    Accidental commital again.

--- ui/stripes.config.js
+++ ui/stripes.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  okapi: { 'url':'http://folio.k-int.com/okapi-gw', 'tenant':'diku' },
+  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: {
 //     autoLogin: { username: 'diku_admin', password: 'admin' },
 //     logCategories: 'core,redux,connect,connect-fetch,substitute,path,mpath,mquery,action,event,perm,interface,xhr,erm',
