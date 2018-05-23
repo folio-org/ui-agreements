4214d9b9d1c3e45aa32c374ecd53a5c963c964ec
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-05-23 14:16:29 +0100

    Missed one.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import ERMRouting from './routing'
+import Routing from './routing'
 import 'whatwg-fetch'
 import '../scss/styles.scss'
 
