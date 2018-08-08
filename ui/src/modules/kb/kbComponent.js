8896e55cc464c954395fcd5abe13567435699284
Ian Ibbotson <ianibbo@gmail.com>
2018-08-08 16:20:07 +0100

    Work on selected pci in kb component

--- ui/src/modules/kb/kbComponent.js
+++ ui/src/modules/kb/kbComponent.js
@@ -15,7 +15,8 @@ class KbComponent extends React.Component {
 
     this.state = {
       showDetailPane: false,
-      showFilterPane: true
+      showFilterPane: true,
+      selectedDetailRecord: null
     }
 
     this.handleTest = this.handleTest.bind(this);
@@ -48,7 +49,7 @@ class KbComponent extends React.Component {
    */
   handleSelectPCI(pci) {
     console.log("handleSelectPCI",pci);
-    this.setState({showDetailPane:true});
+    this.setState({showDetailPane:true, selectedDetailRecord:pci});
   }
 
   render() {
