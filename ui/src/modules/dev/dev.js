3277d0e2cebe38720f22a7c5ca7d641a7bb85027
Ian Ibbotson <ianibbo@gmail.com>
2018-08-26 10:52:44 +0100

    Trying to refactor dev to use the mobx way of doing things

--- ui/src/modules/dev/dev.js
+++ ui/src/modules/dev/dev.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {observable} from 'mobx'
+import { observable, action, computed } from 'mobx'
 import { observer } from 'mobx-react'
 import { hot } from 'react-hot-loader'
 import { Container, Row, Col } from 'reactstrap'
@@ -11,20 +11,42 @@ import TriPanel from '../../components/layout/tri-panel'
 import UrlParamResourceSearch from '../../lib/resource/url-param-resource-search'
 import {tableFormatters, textHighlighter} from '../../lib/helpers'
 
-const Dev = observer((props) => {
+import AddActivityComponent from '../activities/addActivityComponent'
+
+class Dev extends React.Component {
   
-  const filterGroups = {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
   }
+
+  @observable currentActivity = null;
+
+  @action.bound
+  startActivity(activity) {
+    console.log("startActivity(%s)",activity);
+    this.currentActivity=activity;
+  }
+
   
-  return (
-    <div>
-      <h1>Development actions</h1>
-      <ul>
-        <li><button className="btn btn-primary">Import Ebsco Package Test</button></li>
-        <li><button className="btn btn-primary">Create POL Test</button></li>
-      </ul>
-    </div>
-  )
-})
+  render() {
+    return (
+      <div>
+        <h1>Development actions</h1>
+        <ul>
+          <li><button className="btn btn-primary">Import Ebsco Package Test</button></li>
+          <li><button className="btn btn-primary">Test Purchase Activity (Explicit List)</button></li>
+          <li><button className="btn btn-primary">Test Purchase Activity (All via query)</button></li>
+          <li><button className="btn btn-primary" onClick={() => this.startActivity("startActivity")}>Test Add To Agreement Activity (Explicit List)</button></li>
+          <li><button className="btn btn-primary">Test Add To Agreement Activity (All via query)</button></li>
+        </ul>
+        <AddActivityComponent app={this.props.app} currentActivity={'fhfh'}/>
+      </div>
+    )
+  }
+
+}
 
 export default hot(module)(Dev)
