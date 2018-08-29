0c23cc94ae9dc8025c2f8184c27049ecff36ebcc
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-08-29 13:13:23 +0100

    WIP: Changing tri panel.

--- ui/src/components/layout/tri-panel.js
+++ ui/src/components/layout/tri-panel.js
@@ -1,55 +1,96 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 import { hot } from 'react-hot-loader'
 import { Container, Row, Col } from 'reactstrap'
 
+@observer
+class TriPanel extends Component {
+  
+  static propTypes = {
+    left: PropTypes.object,
+    center: PropTypes.object.isRequired,
+    right: PropTypes.object,
+    totalCols: PropTypes.number,
+    leftCols: PropTypes.number,
+    rightCols: PropTypes.number,
+  }
+  
+  @observable
+  totalCols = 12
+  
+  @observable
+  leftCols = 2
+  
+  @observable
+  rightCols = 5
+  
+  @observable
+  left
+  
+  @observable
+  right
+  
+  @observable
+  center
+  
+  constructor ( props ) {
+    super (props)
 
-const TriPanel = observer(({left, center, right}) => {
-  
-  let totalCols = 12
-  
-  let leftCols = 0
-  let rightCols = 0
-  
-  if (left) {
-    leftCols = 2
-  }
-  
-  if (right) {
-    rightCols = 5
-  }
+    this.left = props.left
+    this.right = props.right
+    this.center = props.center
     
-  let centerCols = totalCols - (leftCols + rightCols)
+    if (props.totalCols) {
+      this.totalCols = props.totalCols
+    }
+    
+    if (props.leftCols) {
+      this.leftCols = props.leftCols
+    }
+    
+    if (props.totalCols) {
+      this.rightCols = props.rightCols
+    }
+  }
   
-  let leftComponent = left ? <Col lg={leftCols} xs={totalCols} className="position-fixed" >
-    { left }
-  </Col> : null
-  
-  let rightComponent = right ? <Col lg={{size:(rightCols), offset:(leftCols + centerCols) }} xs={totalCols} >
-    { right }
-  </Col> : null
+  render = () => {
+    
+    let visibleLeftCols = 0
+    let visibleRightCols = 0
+    
+    if (this.left) {
+      visibleLeftCols = this.leftCols
+    }
+    
+    if (this.right) {
+      visibleRightCols = this.rightCols
+    }
 
-  
-  return (
-    <Container fluid={true}>
-      <Row>
-        { leftComponent }
-        
-        <Col lg={{size:(centerCols), offset:(leftCols) }} xs={totalCols} >
-          { center }
-        </Col>
+    let centerCols = totalCols - (visibleLeftCols + visibleRightCols)
+
+    let leftComponent = this.left ? <Col lg={visibleLeftCols} xs={totalCols} className="position-fixed" >
+      { left }
+    </Col> : null
+
+    let rightComponent = this.right ? <Col lg={{size:(visibleRightCols), offset:(visibleLeftCols + centerCols) }} xs={totalCols} >
+      { right }
+    </Col> : null
+
+    return (
+      <Container fluid={true}>
+        <Row>
+          { leftComponent }
           
-        { rightComponent }
-      </Row>
-    </Container>
-  )
-})
-
-TriPanel.propTypes = {
-  left: PropTypes.object,
-  center: PropTypes.object.isRequired,
-  right: PropTypes.object,
+          <Col lg={{size:(centerCols), offset:(visibleLeftCols) }} xs={totalCols} >
+            { this.center }
+          </Col>
+            
+          { rightComponent }
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default hot(module)(TriPanel)
