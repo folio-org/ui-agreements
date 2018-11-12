d65a5650974509625e741f782e316fac79f8f275
Mark Deutsch <mark.deutsch@duke.edu>
2018-11-12 13:31:46 -0500

    Tests: Added tab navigation test

--- test/ui-testing/demo.js
+++ test/ui-testing/demo.js
@@ -1,47 +1,56 @@
 /* global Nightmare, describe, it, before, after */
 
 module.exports.test = (uiTestCtx) => {
-  describe('Module test: ui-agreements:', function() {
+  describe('Module test: ui-agreements:', function test() {
     const { config, helpers: { login, logout } } = uiTestCtx;
     const nightmare = new Nightmare(config.nightmare);
 
     this.timeout(Number(config.test_timeout));
 
-    describe('Login > navigate to app > verify message > logout', () => {
+    describe('Login > navigate to app > verify tabs work > logout', () => {
       before((done) => {
         login(nightmare, config, done);
       });
+
       after((done) => {
         logout(nightmare, config, done);
       });
-      it('should open app and see stripes-new-app-greeting', (done) => {
+
+      it('should open app and see selected Dashboard tab', (done) => {
         nightmare
-          .wait('#clickable-erm-module')
-          .click('#clickable-erm-module')
-          .wait('#erm-module-display')
-          .wait('#stripes-new-app-greeting')
-          .then(result => { done(); })
+          .wait('#clickable-agreements-module')
+          .click('#clickable-agreements-module')
+          .wait('#agreements-module-display')
+          .wait('nav #dashboard')
+          .evaluate(() => document.querySelector('nav #dashboard').className)
+          .then(className => {
+            if (!className.includes('primary')) throw Error('Dashboard tab is not selected');
+            done();
+          })
           .catch(done);
       });
-    });
 
-    describe('Login > navigate to app settings > verify message > logout', () => {
-      before((done) => {
-        login(nightmare, config, done);
-      });
-      after((done) => {
-        logout(nightmare, config, done);
-      });
-      it('should open app settings and see stripes-new-app-settings-message', (done) => {
+      it('should click Agreements tab and see updated URL', (done) => {
         nightmare
-          .wait(config.select.settings)
-          .click(config.select.settings)
-          .wait('a[href="/settings/erm"]')
-          .click('a[href="/settings/erm"]')
-          .wait('a[href="/settings/erm/general"]')
-          .click('a[href="/settings/erm/general"]')
-          .wait('#stripes-new-app-settings-message')
-          .then(result => { done(); })
+          .click('nav #agreements')
+          .wait(1000)
+          .evaluate(() => document.location.pathname)
+          .then(pathName => {
+            if (!pathName.includes('/erm/agreements')) throw Error('URL is incorrect');
+            done();
+          })
+          .catch(done);
+      });
+
+      it('should click E-resources tab and see updated URL', (done) => {
+        nightmare
+          .click('nav #eresources')
+          .wait(1000)
+          .evaluate(() => document.location.pathname)
+          .then(pathName => {
+            if (!pathName.includes('/erm/eresources')) throw Error('URL is incorrect');
+            done();
+          })
           .catch(done);
       });
     });
