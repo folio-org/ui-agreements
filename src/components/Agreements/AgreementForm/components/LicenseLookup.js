75520befd6a79044a000b94d95a7aa39c7887b82
Mark Deutsch <mark.deutsch@duke.edu>
2019-03-05 11:45:54 -0500

    LicenseLookup: write explanation

--- src/components/Agreements/AgreementForm/components/LicenseLookup.js
+++ src/components/Agreements/AgreementForm/components/LicenseLookup.js
@@ -7,10 +7,6 @@ import { LicenseCard } from '@folio/stripes-erm-components';
 
 export default class LicenseLookup extends React.Component {
   static propTypes = {
-    meta: PropTypes.shape({
-      error: PropTypes.node,
-      name: PropTypes.string,
-    }),
     input: PropTypes.shape({
       onChange: PropTypes.func,
       value: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -49,6 +45,22 @@ export default class LicenseLookup extends React.Component {
   )
 
   render() {
+    // TL; DR, don't change this to track the license object as part of `this.state` instead of
+    // `this.props` without understanding the consequences.
+    //
+    // The parent should pass down the `license` object that was passed to it with
+    // onSelectLicense(). We move the holding of the `license` object to the parent
+    // because it's actually not possible to use `state` for this because of the
+    // way React recycles components rendered from an array.
+    // Eg, If we render this component several times when mapped out of an array, and if
+    // the `key` for it is just the array index, then upon deleting the nth entry, the
+    // n+k'th component will "slide" up to the n+k-1'th spot. However, React will not
+    // change the `state` as part of that operation since the key will remain the same.
+    // Alternatively, if we set the key as the license's `id`, then in `handleLicenseSelected`
+    // we'll call setState to change the state.license, followed by `input.onChange`, which
+    // will trigger a rerender of our parent. The parent will now render a _new_ component with
+    // key being the license ID we passed it, wiping away the state we just set.
+
     return this.props.license ? this.renderLicense() : this.renderLookup();
   }
 }
