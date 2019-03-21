86a7a4c6ef40ba39c4a0b0d6612898019b1d3f75
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-03-21 15:06:34 +0100

    ERM-98: modify routes/Agreements.js to fetch license terms modify LicenseBusinessTerms.js to use LicenseTermsList component modify package.json to use stripes-erm-components 1.2.1 add license.noControllingLicense to en.json

--- src/components/Agreements/ViewAgreement/Sections/LicenseBusinessTerms.js
+++ src/components/Agreements/ViewAgreement/Sections/LicenseBusinessTerms.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Accordion } from '@folio/stripes/components';
+import { LicenseTermsList } from '@folio/stripes-erm-components';
 
 export default class LicenseBusinessTerms extends React.Component {
   static propTypes = {
+    parentResources: PropTypes.shape({
+      terms: PropTypes.object,
+    }),
     id: PropTypes.string,
     onToggle: PropTypes.func,
     open: PropTypes.bool,
   };
 
-  render() {
-    return (
-      <Accordion
-        id={this.props.id}
-        label={<FormattedMessage id="ui-agreements.agreements.licenseAndBusTerms" />}
-        open={this.props.open}
-        onToggle={this.props.onToggle}
-      >
-        -
-      </Accordion>
-    );
-  }
+ renderLicenseTermsList = (controllingLicense) => {
+   const license = controllingLicense.remoteId_object;
+   const terms = get(this.props.parentResources.terms, ['records'], []);
+   return (
+     <LicenseTermsList
+       license={license}
+       terms={terms}
+     />
+   );
+ }
+
+ render() {
+   const licenses = get(this.props, ['agreement', 'linkedLicenses'], []);
+   const controllingLicense = licenses.find(l => l.status.value === 'controlling');
+
+   return (
+     <Accordion
+       id={this.props.id}
+       label={<FormattedMessage id="ui-agreements.agreements.licenseAndBusTerms" />}
+       open={this.props.open}
+       onToggle={this.props.onToggle}
+     >
+       { controllingLicense ?
+         this.renderLicenseTermsList(controllingLicense) :
+         <FormattedMessage id="ui-agreements.license.noControllingLicense" />
+         }
+     </Accordion>
+   );
+ }
 }
