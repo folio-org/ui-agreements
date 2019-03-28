4d1e5ab885601b29c41964573e5fa0860f2fcead
Mark Deutsch <mark.deutsch@duke.edu>
2019-03-28 14:15:49 -0400

    Lint

--- src/settings/GeneralSettings.js
+++ src/settings/GeneralSettings.js
@@ -4,15 +4,6 @@ import { FormattedMessage } from 'react-intl';
 
 import { stripesConnect, withStripes } from '@folio/stripes/core';
 import { ConfigManager } from '@folio/stripes/smart-components';
-import { Field } from 'redux-form';
-
-import {
-  Button,
-  Checkbox,
-  Col,
-  Pane,
-  Row
-} from '@folio/stripes/components';
 
 import GeneralSettingsForm from './GeneralSettingsForm';
 
@@ -35,7 +26,7 @@ class GeneralSettings extends React.Component {
     try {
       const value = settings.length === 0 ? '' : settings[0].value;
       loadedValues = JSON.parse(value);
-    } catch (e) {}
+    } catch (e) {} // eslint-disable-line no-empty
 
     return {
       ...this.defaultValues,
