5868b467f6d6894547cb6ea258fe5e09a33e64a7
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-04-15 17:10:56 -0400

    Add tags filter to search Agreements by tags, refs ERM-174

--- src/routes/Agreements.js
+++ src/routes/Agreements.js
@@ -82,6 +82,10 @@ class Agreements extends React.Component {
       type: 'okapi',
       path: 'erm/refdataValues/SubscriptionAgreementOrg/role',
     },
+    tags: {
+      type: 'okapi',
+      path: 'tags?limit=100',
+    },
     contactRoleValues: {
       type: 'okapi',
       path: 'erm/refdataValues/InternalContact/role',
