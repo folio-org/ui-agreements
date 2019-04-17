4a689f5d9e95df30ea2f9ff0788da160ef835678
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-04-16 22:23:55 -0400

    Cleanup for the tags tests, refs ERM-173

--- test/ui-testing/tags-crud.js
+++ test/ui-testing/tags-crud.js
@@ -5,7 +5,7 @@ module.exports.test = (uiTestCtx) => {
   const number = Math.round(Math.random() * 100000);
   const testTag = `test${Math.floor(Math.random() * 10000)}`;
   const agreement = {
-    name: `Linked License Agreement #${number}`,
+    name: `Tags Agreement #${number}`,
   };
 
   describe('Login > Enable Tags > Find user > Create Tags > Logout\n', function test() {
@@ -36,8 +36,8 @@ module.exports.test = (uiTestCtx) => {
           .wait('#tags_enabled')
           .wait(222)
           .evaluate(() => {
-            const element = document.querySelector('[value="true"]');
-            element.click();
+            const list = document.querySelectorAll('[value="true"]');
+            list.forEach(el => (el.click()));
           })
           .then(() => {
             nightmare
@@ -69,26 +69,6 @@ module.exports.test = (uiTestCtx) => {
         AgreementCRUD.createAgreement(nightmare, done, agreement);
       });
 
-      it(`should search for and find agreement: ${agreement.name}`, done => {
-        nightmare
-          .wait('#input-agreement-search')
-          .insert('#input-agreement-search', agreement.name)
-          .click('[data-test-search-and-sort-submit]')
-          .waitUntilNetworkIdle(1000)
-          .wait('#agreementInfo')
-          .evaluate(expectedValues => {
-            const node = document.querySelector('[data-test-agreement-name]');
-            if (!node || !node.innerText) throw Error('No agreement name node found.');
-
-            const name = node.innerText;
-            if (name !== expectedValues.name) {
-              throw Error(`Name of found agreement is incorrect. Expected "${expectedValues.name}" and got "${name}" `);
-            }
-          }, agreement)
-          .then(done)
-          .catch(done);
-      });
-
       it('should add a new Tag', done => {
         nightmare
           .wait('#clickable-show-tags')
@@ -96,7 +76,6 @@ module.exports.test = (uiTestCtx) => {
           .wait('#input-tag-input')
           .type('#input-tag-input', testTag)
           .click('#input-tag-input')
-          .wait(2222)
           .type('#input-tag-input', '\u000d')
           .wait((tagValue) => {
             return Array.from(
@@ -110,7 +89,7 @@ module.exports.test = (uiTestCtx) => {
       it('should have the badge count equal to 1', done => {
         nightmare
           .wait('#clickable-show-tags')
-          .evaluate(() => parseInt(document.querySelector('#clickable-show-tags').textContent) === 1)
+          .wait(() => parseInt(document.querySelector('#clickable-show-tags').textContent) === 1)
           .then(done)
           .catch(done);
       });
