7c08432b88898f63ff0dc881ec1ba00d646edde8
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-05-20 16:55:55 -0400

    Display interfaces for each linked Organizations in Agreements

--- src/components/Agreements/ViewAgreement/Sections/Organizations.js
+++ src/components/Agreements/ViewAgreement/Sections/Organizations.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
-import { Accordion, Badge, Icon, Layout } from '@folio/stripes/components';
+import { Card } from '@folio/stripes-erm-components';
+import { Accordion, Badge, Icon, Layout, MultiColumnList } from '@folio/stripes/components';
+import { stripesConnect } from '@folio/stripes/core';
 
-export default class Organizations extends React.Component {
+
+class Organizations extends React.Component {
   static propTypes = {
     agreement: PropTypes.shape({
       attachedLicenceId: PropTypes.string,
@@ -14,7 +17,7 @@ export default class Organizations extends React.Component {
           org: PropTypes.shape({
             id: PropTypes.string,
             name: PropTypes.string.isRequired,
-            vendorsUuid: PropTypes.string,
+            orgsUuid: PropTypes.string,
           }).isRequired,
           role: PropTypes.shape({
             label: PropTypes.string.isRequired,
@@ -24,32 +27,64 @@ export default class Organizations extends React.Component {
     }).isRequired,
     id: PropTypes.string,
     onToggle: PropTypes.func,
+    organizations: PropTypes.object,
     open: PropTypes.bool,
   };
 
-  renderOrgList = (orgs) => {
+  renderOrgList = () => {
+    const { organizations } = this.props;
     return (
       <React.Fragment>
-        {orgs.map(o => (
-          o.org ?
-            <Layout data-test-organizations-org className="marginTopHalf" key={`${o.org.id}-${o.role.value}`}>
-              {o.org.orgsUuid ?
-                <Link to={`/organizations/view/${o.org.orgsUuid}`}>{o.org.name}</Link> :
-                o.org.name
-              }
-              {o.role && `, ${o.role.label}`}
-            </Layout> : null
-        ))}
+        {organizations && organizations.map(o => {
+          const interfaces = get(o.org, ['orgsUuid_object', 'interfaces'], []);
+          return (
+            o.org ?
+              <Card
+                data-test-organizations-org
+                key={`${o.org.id}-${o.role.value}`}
+                header={
+                  <span className="my-card-header">
+                    {
+                      o.org.orgsUuid ?
+                        <Link to={`/organizations/view/${o.org.orgsUuid}`}>
+                          {o.org.name}
+                          .
+                          {o.role.label}
+                        </Link> : o.org.name
+                    }
+                  </span>
+                }
+              >
+                {interfaces && interfaces.length ?
+                  <MultiColumnList
+                    contentData={interfaces}
+                    visibleColumns={['name', 'uri', 'username', 'password', 'type']}
+                    columnMapping={{
+                      name: <FormattedMessage id="ui-agreements.interface.name" />,
+                      uri: <FormattedMessage id="ui-agreements.interface.uri" />,
+                      username: <FormattedMessage id="ui-agreements.interface.username" />,
+                      password: <FormattedMessage id="ui-agreements.interface.password" />,
+                      type: <FormattedMessage id="ui-agreements.interface.type" />,
+                    }}
+                    columnWidths={{
+                      name: 150,
+                      uri: 250,
+                      username: 130,
+                      password: 130,
+                      type: 150,
+                    }}
+                  /> : <FormattedMessage id="ui-agreements.interface.notFound" />}
+              </Card> : null
+          );
+        })}
       </React.Fragment>
     );
   }
 
   renderOrganizations = () => {
     const { orgs } = this.props.agreement;
-
     if (!orgs || !orgs.length) return <FormattedMessage id="ui-agreements.organizations.agreementHasNone" />;
-
-    return this.renderOrgList(orgs);
+    return this.renderOrgList();
   }
 
   renderBadge = () => {
@@ -74,3 +109,5 @@ export default class Organizations extends React.Component {
     );
   }
 }
+
+export default stripesConnect(Organizations);
--- src/components/Agreements/ViewAgreement/ViewAgreement.js
+++ src/components/Agreements/ViewAgreement/ViewAgreement.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { cloneDeep, difference, get } from 'lodash';
+import { cloneDeep, difference, get, keyBy } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { withTags } from '@folio/stripes/smart-components';
 import {
@@ -66,6 +66,13 @@ class ViewAgreement extends React.Component {
         term: ':{id}',
       },
       shouldRefresh,
+    },
+    interface: {
+      type: 'okapi',
+      path: 'organizations-storage/interfaces',
+      throwErrors: false,
+      accumulate: 'true',
+      fetch: false,
     },
     financesAgreementLines: {
       type: 'okapi',
@@ -140,6 +147,7 @@ class ViewAgreement extends React.Component {
   componentDidMount() {
     this.fetchUsers();
     this.fetchInvoices();
+    this.fetchInterfaces();
   }
 
   componentDidUpdate(prevProps) {
@@ -149,12 +157,33 @@ class ViewAgreement extends React.Component {
     const finances = get(this.props.resources.financesAgreementLines, ['records'], []).map(f => f.id);
     const prevAgreement = get(prevProps.resources.selectedAgreement, ['records', 0], {});
     const agreement = get(this.props.resources.selectedAgreement, ['records', 0], {});
+    const prevOrgs = get(prevProps.resources.selectedAgreement, ['records', 0, 'orgs'], []).map(o => o.id);
+    const orgs = get(this.props.resources.selectedAgreement, ['records', 0, 'orgs'], []).map(o => o.id);
+
+
+    if ((prevAgreement.id !== agreement.id) || (difference(orgs, prevOrgs).length)) {
+      this.fetchInterfaces();
+    }
 
     if ((prevAgreement.id !== agreement.id) || (difference(users, prevUsers).length)) {
       this.fetchUsers();
     }
     if ((prevAgreement.id !== agreement.id) || (difference(finances, prevFinances).length)) {
       this.fetchInvoices();
+    }
+  }
+
+  fetchInterfaces = () => {
+    const { orgs } = this.getAgreement();
+    const ids = [];
+    if (orgs && orgs.length) {
+      orgs.forEach(org => {
+        const interfaces = get(org.org, ['orgsUuid_object', 'interfaces'], []);
+        ids.push(...interfaces.map(id => `id==${id}`));
+      });
+      const query = [...new Set(ids)].join(' or ');
+      this.props.mutator.interface.reset();
+      this.props.mutator.interface.GET({ params: { query } });
     }
   }
 
@@ -202,6 +231,22 @@ class ViewAgreement extends React.Component {
     if (isPending) return undefined;
 
     return get(this.props.resources.invoices, ['records'], []);
+  }
+
+  getOrganizations() {
+    const isPending = get(this.props.resources.interface, ['isPending'], true);
+    if (isPending) return undefined;
+    const interfaces = get(this.props.resources, ['interface', 'records', 0, 'interfaces'], []);
+    const interfaceMap = keyBy(interfaces, 'id');
+    const { orgs } = this.getAgreement();
+    const organizations = cloneDeep(orgs);
+    organizations.forEach(org => {
+      const orgInterfaces = get(org.org, ['orgsUuid_object', 'interfaces'], []);
+      orgInterfaces.forEach((orgInterface, index) => {
+        orgInterfaces[index] = interfaceMap[orgInterface];
+      });
+    });
+    return organizations;
   }
 
   getInitialValues() {
@@ -272,6 +317,7 @@ class ViewAgreement extends React.Component {
       agreementLines: this.getAgreementLines(),
       contacts: this.getContacts(),
       financesAgreementLines: this.getFinancesAgreementLines(),
+      organizations: this.getOrganizations(),
       invoices: this.getInvoices(),
       onToggle: this.handleSectionToggle,
       parentMutator: {
@@ -366,6 +412,7 @@ class ViewAgreement extends React.Component {
               agreement={this.getAgreement()}
               agreementLines={this.getAgreementLines()}
               contacts={this.getContacts()}
+              organizations={this.getOrganizations()}
               onCancel={this.props.onCloseEdit}
               parentMutator={{
                 ...this.props.parentMutator,
