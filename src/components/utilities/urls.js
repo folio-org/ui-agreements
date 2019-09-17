1cb6a24d5b35f785847da8c974cbff19fe4bfd05
Mark Deutsch <mark.deutsch@duke.edu>
2019-09-17 09:21:51 -0400

    Use URL generators for link creation

--- src/components/utilities/urls.js
+++ src/components/utilities/urls.js
@@ -4,7 +4,12 @@ const urls = {
   agreementEdit: id => `/erm/agreements/${id}/edit`,
   agreementCreate: () => '/erm/agreements/create',
 
+  amendmentView: (licenseId, amendmentId) => `/licenses/${licenseId}/amendments/${amendmentId}`,
+
   basket: () => '/erm/basket',
+
+  eholdingsPackageView: id => `/eholdings/packages/${id}`,
+  eholdingsResourceView: id => `/eholdings/resources/${id}`,
 
   eresources: () => '/erm/eresources',
   eresourceView: id => `/erm/eresources/${id}`,
@@ -14,8 +19,11 @@ const urls = {
   noteEdit: id => `/erm/notes/${id}/edit`,
   noteCreate: () => '/erm/notes/create',
 
-  eholdingsPackageView: id => `/eholdings/packages/${id}`,
-  eholdingsResourceView: id => `/eholdings/resources/${id}`,
+  orgView: id => `/organizations/view/${id}`,
+
+  poLineView: id => `/orders/lines/view/${id}`,
+
+  udpView: id => `/eusage/view/${id}`
 };
 
 export default urls;
