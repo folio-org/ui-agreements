57c41c4da526113d2b62973cbcd1ff7b47429c38
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-10-09 15:30:39 -0400

    use lodash get to get the id of the current period

--- src/components/AgreementSections/OtherPeriods.js
+++ src/components/AgreementSections/OtherPeriods.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { get } from 'lodash';
 import { Badge, Accordion, MultiColumnList } from '@folio/stripes/components';
 
 import FormattedUTCDate from '../FormattedUTCDate';
 
-export default class Lines extends React.Component {
+export default class OtherPeriods extends React.Component {
   static propTypes = {
     agreement: PropTypes.shape({
       periods: PropTypes.arrayOf(PropTypes.shape({
@@ -43,14 +44,13 @@ export default class Lines extends React.Component {
 
   render() {
     const {
-      agreement,
       agreement: { currentPeriod = {}, periods = [] },
       id,
       onToggle,
       open,
     } = this.props;
 
-    const otherPeriods = periods.filter(period => period.id !== currentPeriod.id);
+    const otherPeriods = periods.filter(period => period.id !== get(currentPeriod, 'id'));
 
     return (
       <Accordion
