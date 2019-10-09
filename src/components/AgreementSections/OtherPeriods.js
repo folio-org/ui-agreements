a15f3f39121417208002d325352bb9c35433b9e6
Mark Deutsch <mark.deutsch@duke.edu>
2019-10-09 15:52:39 -0400

    Update OtherPeriods.js

--- src/components/AgreementSections/OtherPeriods.js
+++ src/components/AgreementSections/OtherPeriods.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Badge, Accordion, MultiColumnList } from '@folio/stripes/components';
 
