3d1cbf7a23d2762f0db57ec2dd79c773f871c386
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2019-10-28 11:14:24 -0400

    Init work on agreements Bigtest, refs ERM-529

--- test/bigtest/interactors/package-content.js
+++ test/bigtest/interactors/package-content.js
@@ -8,8 +8,6 @@ import {
 } from '@bigtest/interactor';
 
 export default @interactor class PackageContentInteractor {
-  // isDeleteButtonPresent = isPresent('[data-test-external-data-source-delete]');
-  // isEditButtonPresent = isPresent('[data-test-external-data-source-edit]');
   isPackageYesButtonPresent = isPresent('#clickable-filter-class-package');
   clickIsPackageYesButton = clickable('#clickable-filter-class-package');
   searchField = fillable('[data-test-eresource-search-input]');
@@ -29,31 +27,4 @@ export default @interactor class PackageContentInteractor {
 
   processDate = fillable('#period-start-date-0');
   createAgreement = clickable('#clickable-create-agreement');
-
-
-  // clickEditButton = clickable('[data-test-external-data-source-edit]');
-
-  // isNamePresent = isPresent('[data-test-external-data-source-name]');
-  // isTypePresent = isPresent('[data-test-external-data-source-type]');
-  // isRecordTypePresent = isPresent('[data-test-external-data-source-recordtype]');
-  // isURIPresent = isPresent('[data-test-external-data-source-uri]');
-  // isActivePresent = isPresent('[data-test-external-data-source-isactive]');
-  // isSupportsHarvestingPresent = isPresent('[data-test-external-data-source-supportsharvesting]');
-  // isActivationEnabledPresent = isPresent('[data-test-external-data-source-activationenabled]');
-  // isListPrefixPresent = isPresent('[data-test-external-data-source-listprefix]');
-  // isFullPrefixPresent = isPresent('[data-test-external-data-source-fullprefix]');
-  // isPrincipalPresent = isPresent('[data-test-external-data-source-principal]');
-  // isCredentialsPresent = isPresent('[data-test-external-data-source-credentials]');
-
-  // name = new KeyValueInteractor('[data-test-external-data-source-name]');
-  // type = new KeyValueInteractor('[data-test-external-data-source-type]');
-  // recordType = new KeyValueInteractor('[data-test-external-data-source-recordtype]');
-  // uri = new KeyValueInteractor('[data-test-external-data-source-uri]');
-  // isActive = new KeyValueInteractor('[data-test-external-data-source-isactive]');
-  // isSupportsHarvesting = new KeyValueInteractor('[data-test-external-data-source-supportsharvesting]');
-  // isActivationEnabled = new KeyValueInteractor('[data-test-external-data-source-activationenabled]');
-  // listPrefix = new KeyValueInteractor('[data-test-external-data-source-listprefix]');
-  // fullPrefix = new KeyValueInteractor('[data-test-external-data-source-fullprefix]');
-  // principal = new KeyValueInteractor('[data-test-external-data-source-principal]');
-  // credentials = new KeyValueInteractor('[data-test-external-data-source-credentials]');
 }
