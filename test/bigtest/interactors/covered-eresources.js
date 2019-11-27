341f8a69610d8c43ae7db4763458bbf1897c8983
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-11-27 10:15:02 +0100

    ERM-558: erm/sas/<uuid>/export options not working in all cases
    add export button 'disabled' check to existing package-content-test

--- test/bigtest/interactors/covered-eresources.js
+++ test/bigtest/interactors/covered-eresources.js
@@ -1,43 +1,17 @@
 import {
-  attribute,
   interactor,
   clickable,
+  property,
 } from '@bigtest/interactor';
 
 import MultiColumnListInteractor from '@folio/stripes-components/lib/MultiColumnList/tests/interactor';
-
-/* import {
-  interactor,
-  is,
-  attribute,
-  hasClass
-} from '@bigtest/interactor';
-
-import css from '../Button.css';
-
-export default interactor(class ButtonInteractor {
-  static defaultScope = `.${css.button}`;
-  id = attribute('id');
-  href = attribute('href');
-  isAnchor = is('a');
-  isButton = is('button');
-  rendersDefault = hasClass(css.default);
-  rendersPrimary = hasClass(css.primary);
-  rendersBottomMargin0 = hasClass(css.marginBottom0)
-}); */
-
-@interactor class ExportButton {
-  disabled = attribute('disabled');
-  id = attribute('id');
-  // disabled = attribute('disabled');
-}
 
 export default @interactor class CoveredEresourcesListInteractor {
   clickAll = clickable('#clickable-pci-all');
   clickCurrent = clickable('#clickable-pci-current');
   clickFuture = clickable('#clickable-pci-future');
   clickDropped = clickable('#clickable-pci-dropped');
-  exportButton = new ExportButton('#clickable-dropdown-export-eresources');
+  isBtnDisabled = property('[data-test-export-button]', 'disabled');
 
   list = new MultiColumnListInteractor('#eresources-covered');
   eresourceName = index => this.list.rows(index).cells(0).content;
