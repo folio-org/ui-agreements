7feb00e7543733d6cab9b88458cf82854e0160a4
EthanFreestone <ethan.freestone@k-int.co.uk>
2019-12-16 13:50:15 +0000

    Fixed stupid git mistake, linting

--- src/components/UsageDataProvidersFieldArray/UsageDataProviderField.js
+++ src/components/UsageDataProvidersFieldArray/UsageDataProviderField.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { get } from 'lodash';
-import { Button, Card, Layout } from '@folio/stripes/components';
+import { Button, Card, Col, KeyValue, Layout, Row } from '@folio/stripes/components';
 import { AppIcon, Pluggable } from '@folio/stripes/core';
 
 import css from '../styles.css';
@@ -64,7 +64,7 @@ export default class UsageDataProviderField extends React.Component {
   renderUDP = () => {
     const { udp } = this.props;
     return (
-      <Row id={id}>
+      <Row>
         <Col>
           <KeyValue label={<FormattedMessage id="ui-agreements.viewUDP.name" />}>
             <span data-test-udp-card-name>
