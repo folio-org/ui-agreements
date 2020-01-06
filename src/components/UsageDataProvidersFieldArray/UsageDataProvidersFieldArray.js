67944bb091106d936deb19cdd7c449e68dbce394
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-01-06 12:41:19 +0000

    added indexing to 'remove usage data providers' tooltip

--- src/components/UsageDataProvidersFieldArray/UsageDataProvidersFieldArray.js
+++ src/components/UsageDataProvidersFieldArray/UsageDataProvidersFieldArray.js
@@ -41,7 +41,7 @@ class UsageDataProvidersFieldArray extends React.Component {
   renderUDPFields = (items) => {
     return items.map((udp, index) => (
       <EditCard
-        deleteButtonTooltipText={<FormattedMessage id="ui-agreements.usageData.removeUsageDataProvider" />}
+        deleteButtonTooltipText={<FormattedMessage id="ui-agreements.usageData.removeUsageDataProviderIndexed" values={{ index: index + 1 }} />}
         deleteBtnProps={{
           'id': `udp-delete-${index}`,
           'data-test-delete-field-button': true
