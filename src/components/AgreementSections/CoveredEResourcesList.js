e78aaa15593ec1387e1eb426586442b178b01799
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2020-03-03 08:57:01 -0500

    Hide e-resource list elements if no agreement lines exist

--- src/components/AgreementSections/CoveredEResourcesList.js
+++ src/components/AgreementSections/CoveredEResourcesList.js
@@ -200,10 +200,10 @@ export default class CoveredEResourcesList extends React.Component {
   }
 
   render() {
-    const { agreement: { eresources }, eresourcesFilterPath } = this.props;
+    const { agreement: { eresources, lines }, eresourcesFilterPath } = this.props;
     const exportDisabled = eresourcesFilterPath === 'dropped' || eresourcesFilterPath === 'future';
 
-    return (
+    return lines?.length ? (
       <IfEResourcesEnabled>
         <Layout className="marginTop1">
           <Headline margin="small" tag="h4">
@@ -238,6 +238,6 @@ export default class CoveredEResourcesList extends React.Component {
         {eresources ? this.renderList() : <Spinner />}
         <Callout ref={this.callout} />
       </IfEResourcesEnabled>
-    );
+    ) : null;
   }
 }
