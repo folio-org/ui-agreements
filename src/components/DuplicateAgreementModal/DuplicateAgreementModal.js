138d9a6d319e33f1e03fe5dbe6a9a12291f759ee
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2020-04-07 11:18:34 -0400

    cleanup

--- src/components/DuplicateAgreementModal/DuplicateAgreementModal.js
+++ src/components/DuplicateAgreementModal/DuplicateAgreementModal.js
@@ -3,22 +3,22 @@ import { FormattedMessage } from 'react-intl';
 import { DuplicateModal } from '@folio/stripes-erm-components';
 
 export default class DuplicateAgreementModal extends React.Component {
-  render() {
-    const cloneableProperties = [
-      { key: 'agreementInfo', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.agreementInfo" /> },
-      { key: 'internalContacts', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.internalContacts" /> },
-      { key: 'agreementLines', value : <FormattedMessage id="ui-agreements.duplicateAgreementModal.agreementLines" /> },
-      { key: 'linkedLicenses', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.linkedLicenses" /> },
-      { key: 'externalLicenses', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.externalLicenses" /> },
-      { key: 'organizations', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.organizations" /> },
-      { key: 'supplementaryInformation', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.supplementaryInformation" /> },
-      { key: 'usageData', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.usageData" /> },
-    ];
+  cloneableProperties = [
+    { key: 'agreementInfo', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.agreementInfo" /> },
+    { key: 'internalContacts', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.internalContacts" /> },
+    { key: 'agreementLines', value : <FormattedMessage id="ui-agreements.duplicateAgreementModal.agreementLines" /> },
+    { key: 'linkedLicenses', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.linkedLicenses" /> },
+    { key: 'externalLicenses', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.externalLicenses" /> },
+    { key: 'organizations', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.organizations" /> },
+    { key: 'supplementaryInformation', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.supplementaryInformation" /> },
+    { key: 'usageData', value: <FormattedMessage id="ui-agreements.duplicateAgreementModal.usageData" /> },
+  ];
 
+  render() {
     return (
       <DuplicateModal
         {...this.props}
-        cloneableProperties={cloneableProperties}
+        cloneableProperties={this.cloneableProperties}
         translationKey="agreement"
       />
     );
