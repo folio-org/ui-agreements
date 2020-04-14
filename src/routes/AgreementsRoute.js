171b82a276592965e2eef96f7ec1d713707cd087
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2020-04-14 19:43:12 +0200

    ERM-827 Add support for "Alternative name" for agreements
    * update searchKey in AgreementsRoute

--- src/routes/AgreementsRoute.js
+++ src/routes/AgreementsRoute.js
@@ -23,7 +23,7 @@ class AgreementsRoute extends React.Component {
       perRequest: 100,
       limitParam: 'perPage',
       params: getSASParams({
-        searchKey: 'name', // ,alternateNames.name',
+        searchKey: 'name,alternateNames.name',
         filterKeys: {
           contacts: 'contacts.user',
           contactRole: 'contacts.role',
