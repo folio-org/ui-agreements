4affcb3316782ec93d052bbee389c855cbef9200
CalamityC <42971992+CalamityC@users.noreply.github.com>
2020-04-21 16:03:57 +0200

    ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display (#487)
    * EFRM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add Embargo component and use it in Coverage.js
    * add Embargo test to test/bigtest/tests/coverage-tests.js
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add Embargo component and use it in Coverage.js
    * add Embargo test to test/bigtest/tests/coverage-tests.js
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix change request
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add tests w/o movingWallEnd or embargo to test/bigtest/tests/coverage-tests.js
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * switch back to textRight for serial coverage start date
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix coverage-tests and Coverage component
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix dates in coverage-test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix dates in coverage-test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * display embargo of line
    
    * EFRM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add Embargo component and use it in Coverage.js
    * add Embargo test to test/bigtest/tests/coverage-tests.js
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add Embargo component and use it in Coverage.js
    * add Embargo test to test/bigtest/tests/coverage-tests.js
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix change request
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add tests w/o movingWallEnd or embargo to test/bigtest/tests/coverage-tests.js
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * switch back to textRight for serial coverage start date
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix coverage-tests and Coverage component
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix dates in coverage-test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix dates in coverage-test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * display embargo of line
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * test serialLine embargo
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add embargo variable
    
    * remove whitespace
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * fix test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * extend prop types in Coverage
    * fix test
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * set className to flex in MovingWall Layout
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * set tagName in FormattedMesage
    
    * ERM-805 Add Embargo to "E-resource in package" and "Agreement line" display
    
    * add embargo-test
    
    Co-authored-by: aditya matukumalli <aditya.matukumalli@gmail.com>
    Co-authored-by: Mark Deutsch <mark.deutsch@duke.edu>

---
+++ src/components/Embargo/Embargo.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Layout } from '@folio/stripes/components';
+
+export default class Embargo extends React.Component {
+    static propTypes = {
+      embargo: PropTypes.shape({
+        movingWallStart: PropTypes.shape({
+          length: PropTypes.number,
+          unit: PropTypes.string
+        }),
+        movingWallEnd: PropTypes.shape({
+          length: PropTypes.number,
+          unit: PropTypes.string
+        })
+      })
+    }
+
+    renderEmbargoEnd(embargo) {
+      if (embargo.movingWallEnd) {
+        const unitId = `ui-agreements.${embargo.movingWallEnd.unit}`;
+        const number = embargo.movingWallEnd.length;
+        return (
+          <Layout
+            className="flex"
+            data-test-embargo-end
+            data-test-embargo-end-length={number}
+            data-test-embargo-end-unit={embargo.movingWallEnd.unit}
+          >
+            <FormattedMessage id="ui-agreements.embargo.end" tagName="div" />
+            <FormattedMessage id={unitId} tagName="div" values={{ count: number }} />
+          </Layout>
+        );
+      }
+      return null;
+    }
+
+    renderEmbargoStart(embargo) {
+      if (embargo.movingWallStart) {
+        const unitId = `ui-agreements.${embargo.movingWallStart.unit}`;
+        const number = embargo.movingWallStart.length;
+        return (
+          <Layout
+            className="flex"
+            data-test-embargo-start
+            data-test-embargo-start-length={number}
+            data-test-embargo-start-unit={embargo.movingWallStart.unit}
+          >
+            <FormattedMessage id="ui-agreements.embargo.start" tagName="div" />
+            <FormattedMessage id={unitId} tagName="div" values={{ count: number }} />
+          </Layout>
+        );
+      }
+      return null;
+    }
+
+    render() {
+      const { embargo } = this.props;
+      if (!embargo) return null;
+
+      return (
+        <Layout className="full" data-test-embargo>
+          {this.renderEmbargoStart(embargo)}
+          {this.renderEmbargoEnd(embargo)}
+        </Layout>
+      );
+    }
+}
