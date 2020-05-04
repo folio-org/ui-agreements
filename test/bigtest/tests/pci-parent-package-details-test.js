186223d4d7fbc9e11b6ce422fda24d8278bb03e4
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2020-05-03 20:58:55 -0400

    Remove commmented code

--- test/bigtest/tests/pci-parent-package-details-test.js
+++ test/bigtest/tests/pci-parent-package-details-test.js
@@ -4,47 +4,13 @@ import chai from 'chai';
 import spies from 'chai-spies';
 import { StaticRouter as Router } from 'react-router-dom';
 import { mountWithContext } from '../helpers/mountWithContext';
-// import PackageContentItemView from '../../../src/components/views/PackageContentItem';
 import ParentPackageDetails from '../../../src/components/EResourceSections/ParentPackageDetails';
-// import { PCIFormCoverage } from '../../../src/components/EResourceSections';
 
 import { pci } from './resources';
 import ParentPackageDetailsInteractor from '../interactors/pci-parent-package-details';
 
 chai.use(spies);
 const { expect } = chai;
-
-// const onClose = spy();
-
-// const entitlementOptions = [];
-// const entitlements = [];
-
-// const data = {
-//   pci,
-//   entitlementOptions,
-//   entitlements,
-//   isLoading: false
-// };
-
-// const handlers = { onClose };
-
-// describe.only('PackageContentItemView', () => {
-//   // describe('view', () => {
-//   //   beforeEach(async () => {
-//   //     await mountWithContext(
-//   //       <Router>
-//   //         <PackageContentItemView
-//   //           data={data}
-//   //           handlers={handlers}
-//   //         />
-//   //       </Router>
-//   //     );
-//   //   });
-
-//   //   it('onClone callback should be called', () => {
-//   //     expect(onClone).to.have.been.called();
-//   //   });
-//   // });
 
 describe('Parent package details', () => {
   const interactor = new ParentPackageDetailsInteractor();
