69e176b62e43c7053b68b72b3c25f26697f4d078
Mark Deutsch <mark.deutsch@duke.edu>
2020-05-08 14:28:36 -0400

    ERM-871: Render resource cards on AgLine view

--- src/components/AgreementLineSections/Info.js
+++ src/components/AgreementLineSections/Info.js
@@ -10,7 +10,10 @@ import {
   Row,
 } from '@folio/stripes/components';
 
+import PackageCard from '../PackageCard';
 import TitleCard from '../TitleCard';
+
+import { resourceClasses } from '../../constants';
 
 const propTypes = {
   line: PropTypes.shape({
@@ -29,42 +32,72 @@ const propTypes = {
 
 const Info = ({
   line,
-}) => (
-  <>
-    <Headline size="x-large" tag="h2">
-      <FormattedMessage id="ui-agreements.line.lineInformation" />
-    </Headline>
-    <KeyValue label={<FormattedMessage id="ui-agreements.line.parentAgreement" />}>
-      <div data-test-agreement-line-agreement>
-        {line.owner?.name ?? <NoValue />}
-      </div>
-    </KeyValue>
-    <Row>
-      <Col md={3} xs={6}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.activeFrom" />}>
-          <div data-test-agreement-line-active-from>
-            {line.startDate ?? <NoValue />}
-          </div>
-        </KeyValue>
-      </Col>
-      <Col md={3} xs={6}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.activeTo" />}>
-          <div data-test-agreement-line-active-to>
-            {line.endDate ?? <NoValue />}
-          </div>
-        </KeyValue>
-      </Col>
-      <Col md={6} xs={12}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.note" />}>
-          <div data-test-agreement-line-note>
-            {line.note ?? <NoValue />}
-          </div>
-        </KeyValue>
-      </Col>
-    </Row>
-    <TitleCard title={line.resource?._object} />
-  </>
-);
+}) => {
+  const resource = line.resource?._object ?? {};
+  const resourceClass = line.resource?.class;
+
+  return (
+    <>
+      <Headline size="x-large" tag="h2">
+        <FormattedMessage id="ui-agreements.line.lineInformation" />
+      </Headline>
+      <KeyValue label={<FormattedMessage id="ui-agreements.line.parentAgreement" />}>
+        <div data-test-agreement-line-agreement>
+          {line.owner?.name ?? <NoValue />}
+        </div>
+      </KeyValue>
+      <Row>
+        <Col md={3} xs={6}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.eresources.activeFrom" />}>
+            <div data-test-agreement-line-active-from>
+              {line.startDate ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+        <Col md={3} xs={6}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.eresources.activeTo" />}>
+            <div data-test-agreement-line-active-to>
+              {line.endDate ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+        <Col md={6} xs={12}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.note" />}>
+            <div data-test-agreement-line-note>
+              {line.note ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+      </Row>
+      { resourceClass === resourceClasses.PACKAGE ?
+        <>
+          <Headline size="large" tag="h3">
+            <FormattedMessage id="ui-agreements.eresources.packageDetails" />
+          </Headline>
+          <PackageCard pkg={resource} />
+        </>
+        :
+        <>
+          <Headline size="large" tag="h3">
+            <FormattedMessage id="ui-agreements.eresources.titleDetails" />
+          </Headline>
+          <KeyValue label={<FormattedMessage id="ui-agreements.eresources.titleOnPlatformURL" />}>
+            {resource.pti?.url ?
+              <a href={resource.pti?.url} rel="noopener noreferrer" target="_blank">{resource.pti?.url}</a>
+              :
+              <NoValue />
+            }
+          </KeyValue>
+          <TitleCard title={resource} />
+          <Headline size="large" tag="h3">
+            <FormattedMessage id="ui-agreements.eresources.parentPackageDetails" />
+          </Headline>
+          <PackageCard pkg={resource.pkg} />
+        </>
+      }
+    </>
+  );
+};
 
 Info.propTypes = propTypes;
 export default Info;
