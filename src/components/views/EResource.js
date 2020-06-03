4b8a4bb2ff68bf7183ec39216bedb5ded284c101
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2020-06-03 11:05:36 -0400

    Convert EResource to a functional component and useIntl

--- src/components/views/EResource.js
+++ src/components/views/EResource.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import { Button, IconButton, LoadingPane, Pane, PaneMenu } from '@folio/stripes/components';
 import { IfPermission, TitleManager } from '@folio/stripes/core';
@@ -10,48 +10,47 @@ import Title from './Title';
 import PCI from './PCI';
 import { resourceClasses } from '../../constants';
 
-export default class EResource extends React.Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      eresource: PropTypes.shape({
-        class: PropTypes.string,
-        name: PropTypes.string,
-        type: PropTypes.object,
-      }),
+const propTypes = {
+  data: PropTypes.shape({
+    eresource: PropTypes.shape({
+      class: PropTypes.string,
+      name: PropTypes.string,
+      type: PropTypes.object,
     }),
-    handlers: PropTypes.shape({
-      onClose: PropTypes.func.isRequired,
-      onEdit: PropTypes.func.isRequired,
-      onNeedMorePackageContents: PropTypes.func.isRequired,
-      onToggleTags: PropTypes.func.isRequired
-    }).isRequired,
-    helperApp: PropTypes.func,
-    isLoading: PropTypes.bool,
-  }
+  }),
+  handlers: PropTypes.shape({
+    onClose: PropTypes.func.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onNeedMorePackageContents: PropTypes.func.isRequired,
+    onToggleTags: PropTypes.func.isRequired
+  }).isRequired,
+  helperApp: PropTypes.func,
+  isLoading: PropTypes.bool,
+};
 
-  renderEditEresourcePaneMenu = () => {
-    const {
-      data: { eresource = {} },
-      handlers,
-    } = this.props;
+const EResource = ({
+  data: { eresource = {} },
+  data = {},
+  helperApp,
+  handlers,
+  isLoading
+}) => {
+  const intl = useIntl();
 
+  const renderEditEresourcePaneMenu = () => {
     return (eresource.class === resourceClasses.PCI ||
       eresource.class === resourceClasses.TITLEINSTANCE) ?
       (
         <IfPermission perm="ui-agreements.resources.edit">
           <PaneMenu>
             {handlers.onToggleTags &&
-              <FormattedMessage id="ui-agreements.agreements.showTags">
-                {ariaLabel => (
-                  <IconButton
-                    ariaLabel={ariaLabel}
-                    badgeCount={eresource?.tags?.length ?? 0}
-                    icon="tag"
-                    id="clickable-show-tags"
-                    onClick={handlers.onToggleTags}
-                  />
-                )}
-              </FormattedMessage>
+              <IconButton
+                ariaLabel={intl.formatMessage({ id: 'ui-agreements.agreements.showTags' })}
+                badgeCount={eresource?.tags?.length ?? 0}
+                icon="tag"
+                id="clickable-show-tags"
+                onClick={handlers.onToggleTags}
+              />
             }
             {eresource.class === resourceClasses.PCI &&
             <Button
@@ -66,47 +65,41 @@ export default class EResource extends React.Component {
           </PaneMenu>
         </IfPermission>
       ) : null;
+  };
+
+  const paneProps = {
+    defaultWidth: '55%',
+    dismissible: true,
+    onClose: handlers.onClose,
+  };
+
+  if (isLoading) return <LoadingPane data-loading id="pane-view-eresource" {...paneProps} />;
+
+  let EResourceViewComponent = Package;
+
+  if (data.eresource?.class === resourceClasses.TITLEINSTANCE) {
+    EResourceViewComponent = Title;
+  } else if (data.eresource?.class === resourceClasses.PCI) {
+    EResourceViewComponent = PCI;
   }
 
-  render() {
-    const {
-      data = {},
-      handlers,
-      helperApp,
-      isLoading,
-    } = this.props;
+  return (
+    <>
+      <Pane
+        id="pane-view-eresource"
+        lastMenu={renderEditEresourcePaneMenu()}
+        onClose={handlers.onClose}
+        paneTitle={data.eresource.name}
+        {...paneProps}
+      >
+        <TitleManager record={data.eresource.name}>
+          <EResourceViewComponent data={data} handlers={handlers} />
+        </TitleManager>
+      </Pane>
+      {helperApp(data.eresource)}
+    </>
+  );
+};
 
-    const paneProps = {
-      defaultWidth: '55%',
-      dismissible: true,
-      onClose: handlers.onClose,
-    };
-
-    if (isLoading) return <LoadingPane data-loading id="pane-view-eresource" {...paneProps} />;
-
-    let EResourceViewComponent = Package;
-
-    if (data.eresource?.class === resourceClasses.TITLEINSTANCE) {
-      EResourceViewComponent = Title;
-    } else if (data.eresource?.class === resourceClasses.PCI) {
-      EResourceViewComponent = PCI;
-    }
-
-    return (
-      <>
-        <Pane
-          id="pane-view-eresource"
-          lastMenu={this.renderEditEresourcePaneMenu()}
-          onClose={handlers.onClose}
-          paneTitle={data.eresource.name}
-          {...paneProps}
-        >
-          <TitleManager record={data.eresource.name}>
-            <EResourceViewComponent data={data} handlers={handlers} />
-          </TitleManager>
-        </Pane>
-        {helperApp(data.eresource)}
-      </>
-    );
-  }
-}
+EResource.propTypes = propTypes;
+export default EResource;
