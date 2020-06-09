0147075e2bc07d1d38f99d9a07ce68ef121d294a
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2020-06-09 09:44:21 -0400

    Use the unmountTestingRoot helper to cleanup component context after running the test

--- test/bigtest/helpers/setup-application.js
+++ test/bigtest/helpers/setup-application.js
@@ -1,12 +1,14 @@
 import setupStripesCore from '@folio/stripes-core/test/bigtest/helpers/setup-application';
+import { beforeEach } from '@bigtest/mocha';
+import { unmountTestingRoot } from '@folio/stripes-erm-components';
 import mirageOptions from '../network';
-
 import findPOLineModule from './findPOLineModule';
 
 export default function setupApplication({
   scenarios,
   hasAllPerms = true
 } = {}) {
+  beforeEach(async () => unmountTestingRoot());
   setupStripesCore({
     mirageOptions,
     modules: [
