b6cb7fb0ac4492511c0281cc30bf3d21e1aa09e0
aditya matukumalli <aditya.matukumalli@gmail.com>
2020-10-26 16:31:34 -0400

    fix proptypes

--- src/routes/PlatformViewRoute.js
+++ src/routes/PlatformViewRoute.js
@@ -29,7 +29,7 @@ class PlatformViewRoute extends React.Component {
       }).isRequired
     }).isRequired,
     resources: PropTypes.shape({
-      platform: this.propTypes.object,
+      platform: PropTypes.object,
     }).isRequired,
   };
 
