ce61c81c0b5db847ed9d0d674c9e89d59f1f3a3f
aditya matukumalli <aditya.matukumalli@gmail.com>
2020-12-23 16:07:37 -0500

    Add RTL tests for EntitlementAgreementsList, refs ERM-1275 (#705)

--- src/components/DuplicateAgreementModal/DuplicateAgreementModal.test.js
+++ src/components/DuplicateAgreementModal/DuplicateAgreementModal.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import DuplicateAgreementModal from './DuplicateAgreementModal';
 
-test('renders expected CustomCoverageIcon', () => {
+test('renders expected duplicate agreement modal', () => {
   const { getByTestId } = render(<DuplicateAgreementModal />);
   expect(getByTestId('duplicateModal')).toBeInTheDocument();
 });
