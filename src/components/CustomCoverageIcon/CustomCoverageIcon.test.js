277faa670323c0a6fe06c5654ddf88e00d587b98
aditya matukumalli <aditya.matukumalli@gmail.com>
2020-12-23 10:37:16 -0500

    cleanup

--- src/components/CustomCoverageIcon/CustomCoverageIcon.test.js
+++ src/components/CustomCoverageIcon/CustomCoverageIcon.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
-import userEvent from '@testing-library/user-event';
 import CustomCoverageIcon from './CustomCoverageIcon';
 
 test('renders expected CustomCoverageIcon', () => {
