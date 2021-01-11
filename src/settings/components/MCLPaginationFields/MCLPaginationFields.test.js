317a94c6cc0881c01bb9bc07f3b7a0989e628268
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2021-01-11 20:34:47 +0100

    test onSubmit, refs ERM-1460

--- src/settings/components/MCLPaginationFields/MCLPaginationFields.test.js
+++ src/settings/components/MCLPaginationFields/MCLPaginationFields.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
 import userEvent from '@testing-library/user-event';
@@ -14,27 +13,26 @@ describe('MCLPaginationFields', () => {
       <TestForm onSubmit={onSubmit}>
         <MCLPaginationFields />
       </TestForm>
-
     );
-
     mclList.forEach((mcl) => {
       expect(getByTestId(mcl)).toBeInTheDocument();
     });
   });
 
-  test('type value and render', () => {
+  test('submit values', () => {
     const { getByTestId } = renderWithIntl(
       <TestForm onSubmit={onSubmit}>
         <MCLPaginationFields />
       </TestForm>
-
     );
-
     mclList.forEach((mcl) => {
       userEvent.type(getByTestId(mcl), '15');
-      expect(getByTestId(mcl)).toHaveValue(15);
     });
+    userEvent.click(getByTestId('submit'));
+    expect(onSubmit.mock.calls.length).toBe(1);
+    expect(onSubmit.mock.calls[0][0]).toEqual(
+      { 'pageSize': { 'agreementEresources': 15, 'agreementLines': 15, 'entitlementOptions': 15, 'entitlements': 15, 'packageContents': 15 } }
+    );
   });
-
-  // Add validation tests once TextField interactor is made available
 });
+// Add validation tests once TextField interactor is made available
