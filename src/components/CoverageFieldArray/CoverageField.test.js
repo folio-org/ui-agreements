5d625ad0c67fe78824d07a802725a77cf9ab0979
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-01-13 13:28:40 +0000

    linting

--- src/components/CoverageFieldArray/CoverageField.test.js
+++ src/components/CoverageFieldArray/CoverageField.test.js
@@ -15,19 +15,19 @@ describe('CoverageField', () => {
         <CoverageField
           index={0}
           input={{
-            name: "coverageTest"
+            name: 'coverageTest'
           }}
         />
       </TestForm>
     );
 
     expect(getByTestId('coverageField')).toBeInTheDocument();
-    expect(getByRole('textbox', {name: "ui-agreements.agreements.startDate"})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.startVolume"})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.startIssue"})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: "ui-agreements.agreements.endDate"})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.endVolume"})).toBeInTheDocument();
-    expect(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.endIssue"})).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'ui-agreements.agreements.startDate' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.startVolume' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.startIssue' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'ui-agreements.agreements.endDate' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.endVolume' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.endIssue' })).toBeInTheDocument();
   });
 
   // Test start date/end date validation.
@@ -38,15 +38,15 @@ describe('CoverageField', () => {
         <CoverageField
           index={0}
           input={{
-            name: "coverageTest"
+            name: 'coverageTest'
           }}
         />
       </TestForm>
     );
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreements.startDate"}), '01/01/2021')
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreements.endDate"}), '01/01/2002')
-    
-    /* 
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreements.startDate' }), '01/01/2021');
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreements.endDate' }), '01/01/2002');
+
+    /*
      * This actually works with getByText, because currently the validation only
      * appears on the startDate field until you click away from the endDate field.
      * This behaviour should not be relied upon though, since the validation is set up on two fields.
@@ -61,21 +61,21 @@ describe('CoverageField', () => {
         <CoverageField
           index={0}
           input={{
-            name: "coverageTest"
+            name: 'coverageTest'
           }}
         />
       </TestForm>
     );
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreements.startDate"}), '10/10/1996');
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreements.endDate"}), '02/06/1999');
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.startVolume"}), '1');
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.endVolume"}), '5');
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.startIssue"}), '3rd');
-    userEvent.type(getByRole('textbox', {name: "ui-agreements.agreementLines.customCoverage.endIssue"}), '9th');
-    
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreements.startDate' }), '10/10/1996');
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreements.endDate' }), '02/06/1999');
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.startVolume' }), '1');
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.endVolume' }), '5');
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.startIssue' }), '3rd');
+    userEvent.type(getByRole('textbox', { name: 'ui-agreements.agreementLines.customCoverage.endIssue' }), '9th');
+
     userEvent.click(getByTestId('submit'));
     expect(onSubmit.mock.calls.length).toBe(1);
-    const submittedValues = onSubmit.mock.calls[0][0]
+    const submittedValues = onSubmit.mock.calls[0][0];
     const expectedPayload = {
       coverageTest: {
         startDate: '1996-10-10',
@@ -85,7 +85,7 @@ describe('CoverageField', () => {
         startIssue: '3rd',
         endIssue: '9th'
       }
-    }
+    };
     expect(submittedValues).toEqual(expectedPayload);
   });
 });
