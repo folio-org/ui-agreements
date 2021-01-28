8ac0680f97cf98e2f526cd941b7e059ab6654d10
Peter <boehm@hebis.uni-frankfurt.de>
2021-01-28 15:06:00 +0100

    ERM-1281: Add test coverage for ui-agreements <NoPermissions> (#743)
    * ERM-1281
    
    * ERM-1281: reverting the invocation of translations

---
+++ src/components/NoPermissions/NoPermissions.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
+import NoPermissions from './NoPermissions';
+import translationsProperties from '../../../test/helpers';
+
+describe('NoPermissions', () => {
+  test('renders Permission Error message', () => {
+    const { getByRole, getByText } = renderWithIntl(
+      <NoPermissions />, translationsProperties
+    );
+    expect(getByRole('heading', { name: 'stripes-smart-components.permissionError' })).toBeInTheDocument();
+    expect(getByText('stripes-smart-components.permissionsDoNotAllowAccess')).toBeInTheDocument();
+  });
+});
