9a9ea9af0e64c722bc4f48224b1e69ec390693a1
Peter <boehm@hebis.uni-frankfurt.de>
2021-01-28 10:37:28 +0100

    ERM-1281: reverting the invocation of translations

--- src/components/NoPermissions/NoPermissions.test.js
+++ src/components/NoPermissions/NoPermissions.test.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
-// import userEvent from '@testing-library/user-event';
 import NoPermissions from './NoPermissions';
-
 import translationsProperties from '../../../test/helpers';
-
-// const onSubmit = jest.fn();
 
 describe('NoPermissions', () => {
   test('renders Permission Error message', () => {
