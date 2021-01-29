1c892ca79222194ddc1d4add064268655f3434f8
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-01-29 13:27:48 +0000

    refactored period information to its own component with selector for prev/next/current

--- src/components/AgreementSections/Info.js
+++ src/components/AgreementSections/Info.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import {
   Col,
-  FormattedUTCDate,
   Headline,
   KeyValue,
   MultiColumnList,
@@ -12,107 +11,96 @@ import {
 } from '@folio/stripes/components';
 
 import { statuses } from '../../constants';
+import InfoPeriods from './InfoPeriods';
 
-export default class Info extends React.Component {
-  static propTypes = {
-    agreement: PropTypes.shape({
-      agreementStatus: PropTypes.shape({
-        label: PropTypes.string,
-        value: PropTypes.string,
-      }),
-      alternateNames: PropTypes.arrayOf(PropTypes.object),
-      cancellationDeadline: PropTypes.string,
-      currentPeriod: PropTypes.shape({
-        note: PropTypes.string,
-      }),
-      description: PropTypes.string,
-      isPerpetual: PropTypes.shape({
-        label: PropTypes.string,
-      }),
-      name: PropTypes.string,
-      reasonForClosure: PropTypes.shape({
-        label: PropTypes.string,
-      }),
-      renewalPriority: PropTypes.shape({
-        label: PropTypes.string,
-      }),
-    }),
-  };
-
-  render() {
-    const { agreement } = this.props;
-    return (
-      <div data-test-agreement-info>
-        <Row>
-          <Col xs={12}>
-            <div data-test-agreement-name>
-              <Headline
-                size="xx-large"
-                tag="h2"
-              >
-                {agreement.name}
-              </Headline>
+export default function Info ({ agreement }) {
+  return (
+    <div data-test-agreement-info>
+      <Row>
+        <Col xs={12}>
+          <div data-test-agreement-name>
+            <Headline
+              size="xx-large"
+              tag="h2"
+            >
+              {agreement.name}
+            </Headline>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreements.isPerpetual" />}>
+            <div data-test-agreement-is-perpetual>
+              {agreement?.isPerpetual?.label ?? <NoValue />}
             </div>
-          </Col>
-        </Row>
-        <Row>
+          </KeyValue>
+        </Col>
+        <Col xs={3}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreements.renewalPriority" />}>
+            <div data-test-agreement-renewal-priority>
+              {agreement?.renewalPriority?.label ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+        {agreement?.agreementStatus?.value === statuses.CLOSED &&
           <Col xs={3}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.agreements.cancellationDeadline" />}>
-              <div data-test-agreement-cancellation-deadline>
-                {agreement.cancellationDeadline ? <FormattedUTCDate value={agreement.cancellationDeadline} /> : <NoValue />}
+            <KeyValue label={<FormattedMessage id="ui-agreements.agreements.reasonForClosure" />}>
+              <div data-test-agreement-reason-for-closure>
+                {agreement?.reasonForClosure?.label ?? <NoValue />}
               </div>
             </KeyValue>
           </Col>
-          <Col xs={3}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.agreements.isPerpetual" />}>
-              <div data-test-agreement-is-perpetual>
-                {agreement?.isPerpetual?.label ?? <NoValue />}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col xs={3}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.agreements.renewalPriority" />}>
-              <div data-test-agreement-renewal-priority>
-                {agreement?.renewalPriority?.label ?? <NoValue />}
-              </div>
-            </KeyValue>
-          </Col>
-          {agreement?.agreementStatus?.value === statuses.CLOSED &&
-            <Col xs={3}>
-              <KeyValue label={<FormattedMessage id="ui-agreements.agreements.reasonForClosure" />}>
-                <div data-test-agreement-reason-for-closure>
-                  {agreement?.reasonForClosure?.label ?? <NoValue />}
-                </div>
-              </KeyValue>
-            </Col>
-          }
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.agreements.agreementDescription" />}>
-              <div data-test-agreement-description style={{ whiteSpace: 'pre-wrap' }}>
-                {agreement.description || <NoValue />}
-              </div>
-            </KeyValue>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.agreements.currentPeriodNote" />}>
-              <div data-test-agreement-current-period-note style={{ whiteSpace: 'pre-wrap' }}>
-                {agreement?.currentPeriod?.note ?? <NoValue />}
-              </div>
-            </KeyValue>
-          </Col>
-        </Row>
-        {agreement?.alternateNames?.length !== 0 &&
-          <MultiColumnList
-            columnMapping={{ name: <FormattedMessage id="ui-agreements.alternativeNames" /> }}
-            contentData={agreement.alternateNames}
-            visibleColumns={['name']}
-          />
         }
-      </div>
-    );
-  }
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreements.agreementDescription" />}>
+            <div data-test-agreement-description style={{ whiteSpace: 'pre-wrap' }}>
+              {agreement.description || <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+      </Row>
+      {agreement?.alternateNames?.length !== 0 &&
+        <MultiColumnList
+          columnMapping={{ name: <FormattedMessage id="ui-agreements.alternativeNames" /> }}
+          contentData={agreement.alternateNames}
+          visibleColumns={['name']}
+        />
+      }
+      <InfoPeriods periods={agreement.periods}/>
+    </div>
+  );
 }
+
+Info.propTypes = { 
+  agreement: PropTypes.shape({
+    agreementStatus: PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string,
+    }),
+    alternateNames: PropTypes.arrayOf(PropTypes.object),
+    cancellationDeadline: PropTypes.string,
+    currentPeriod: PropTypes.shape({
+      note: PropTypes.string,
+    }),
+    description: PropTypes.string,
+    isPerpetual: PropTypes.shape({
+      label: PropTypes.string,
+    }),
+    name: PropTypes.string,
+    reasonForClosure: PropTypes.shape({
+      label: PropTypes.string,
+    }),
+    periods: PropTypes.arrayOf(PropTypes.shape({
+      cancellationDeadline: PropTypes.string,
+      endDate: PropTypes.string,
+      periodStatus: PropTypes.string,
+      startDate: PropTypes.string.isRequired,
+    })),
+    renewalPriority: PropTypes.shape({
+      label: PropTypes.string,
+    }),
+  }),
+};
---
+++ src/components/AgreementSections/InfoPeriods.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import {
+  Button,
+  ButtonGroup,
+  Col,
+  FormattedUTCDate,
+  Headline,
+  KeyValue,
+  Layout,
+  NoValue,
+  Row,
+} from '@folio/stripes/components';
+
+export default function InfoPeriods ({ periods }) {
+
+  /* 
+   * This could be handled by callback, but since we need to know for display whether next/prev/current exist
+   * it seems more prudent to just fetch all three at once and then do our logic at this stage
+   */
+  const displayPeriodOptions = {
+    currentPeriod: periods?.find(p => p.periodStatus === 'current'),
+    nextPeriod: periods?.find(p => p.periodStatus === 'next'),
+    previousPeriod: periods?.find(p => p.periodStatus === 'previous')
+  }
+
+  /* 
+   * Set up which period is currently selected. Heirachy is Current > Next > Previous
+   * The empty array as a second argument to useEffect should mean this code only runs once to prevent infinite loops
+   */
+  const [selectedPeriod, setSelectedPeriod] = useState('current')
+  useEffect(() => {
+    if (displayPeriodOptions.currentPeriod === undefined) {
+      if (displayPeriodOptions.nextPeriod !== undefined) {
+        setSelectedPeriod('next')
+      } else {
+        setSelectedPeriod('previous')
+      }
+    }
+  },[])
+
+  const displayPeriod = displayPeriodOptions[`${selectedPeriod}Period`]
+
+
+  const PeriodButton = (periodStatus) => {
+    return (
+      <Button
+        buttonStyle={selectedPeriod === periodStatus ? 'primary' : 'default'}
+        disabled={displayPeriodOptions[`${periodStatus}Period`] === undefined}
+        marginBottom0
+        onClick={() => setSelectedPeriod(periodStatus)}
+      >
+        <FormattedMessage id={`ui-agreements.agreementPeriods.${periodStatus}`} />
+      </Button>
+    );
+  }
+
+  return (
+    <>
+      <Row>
+        <Col xs={12}>
+          <div data-test-period-header>
+            <Headline
+              size="large"
+              tag="h3"
+            >
+              <FormattedMessage id="ui-agreements.agreementPeriods.period" />
+            </Headline>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Layout className="textCentered">
+            <ButtonGroup>
+              {PeriodButton('previous')}
+              {PeriodButton('current')}
+              {PeriodButton('next')}
+            </ButtonGroup>
+          </Layout>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreementPeriods.periodStart" />}>
+            <div data-test-period-info-start-date>
+              {<FormattedUTCDate value={displayPeriod?.startDate} /> ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+        <Col xs={3}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreementPeriods.periodEnd" />}>
+            <div data-test-period-info-end-date>
+              {<FormattedUTCDate value={displayPeriod?.endDate} /> ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+        <Col xs={3}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreements.cancellationDeadline" />}>
+            <div data-test-period-info-cancellation-deadline>
+              {displayPeriod?.cancellationDeadline ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.agreementPeriods.periodNote" />}>
+            <div data-test-period-info-note>
+              {displayPeriod?.note ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+InfoPeriods.propTypes = { 
+  periods: PropTypes.arrayOf(PropTypes.shape({
+    cancellationDeadline: PropTypes.string,
+    endDate: PropTypes.string,
+    periodStatus: PropTypes.string,
+    startDate: PropTypes.string.isRequired,
+  })),
+};
