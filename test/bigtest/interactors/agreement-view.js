5ffd4300cdf54c6add55191bba0420623fd77b14
aditya matukumalli <aditya.matukumalli@gmail.com>
2021-02-04 12:18:18 -0500

    wait for the actions button to show up in tests

--- test/bigtest/interactors/agreement-view.js
+++ test/bigtest/interactors/agreement-view.js
@@ -6,11 +6,6 @@ import {
 
 import DuplicateModalInteractor from '@folio/stripes-erm-components/lib/DuplicateModal/tests/interactor';
 import AgreementLinesInteractor from './agreement-lines';
-
-@interactor class HeaderDropdown {
-  click = clickable('button');
-}
-
 @interactor class HeaderDropdownMenu {
   isDuplicateButtonPresent = isPresent('#clickable-dropdown-duplicate-agreement')
   clickDuplicate = clickable('#clickable-dropdown-duplicate-agreement');
@@ -19,7 +14,9 @@ import AgreementLinesInteractor from './agreement-lines';
 export default @interactor class AgreementViewInteractor {
   isViewAgreement = isPresent('#pane-view-agreement');
   isLoading = isPresent('#pane-view-agreement[data-loading]');
-  headerDropdown = new HeaderDropdown('[data-pane-header-actions-dropdown]');
+  actionsDropdownButtonPresent = isPresent('[data-test-pane-header-actions-button]');
+  clickActionsDropdownButton = clickable('[data-test-pane-header-actions-button]');
+
   headerDropdownMenu = new HeaderDropdownMenu();
   duplicateAgreementModal = new DuplicateModalInteractor();
   expandAll = clickable('[data-tast-expand-button]');
@@ -28,5 +25,9 @@ export default @interactor class AgreementViewInteractor {
 
   whenLoaded() {
     return this.when(() => this.isLoading === false).timeout(5000);
+  }
+
+  whenActionsDropDownButtonLoaded() {
+    return this.when(() => this.actionsDropdownButtonPresent === true).timeout(5000);
   }
 }
