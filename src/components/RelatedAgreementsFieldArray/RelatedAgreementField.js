20fbbe173cf73921030f759e0afbda78afdf11e8
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2021-03-16 10:31:44 +0100

    ERM-1547 Related agreements: Improve error management
    * convert RelatedAgreementField to functional component

--- src/components/RelatedAgreementsFieldArray/RelatedAgreementField.js
+++ src/components/RelatedAgreementsFieldArray/RelatedAgreementField.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect, useForm, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import {
@@ -19,71 +18,71 @@ import { AppIcon, Pluggable } from '@folio/stripes/core';
 import { urls } from '../utilities';
 import css from '../styles.css';
 
-export default class RelatedAgreementField extends React.Component {
-  static propTypes = {
-    id: PropTypes.string,
-    input: PropTypes.shape({
-      name: PropTypes.string,
-      value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-    }).isRequired,
-    meta: PropTypes.shape({
-      error: PropTypes.node,
-      touched: PropTypes.bool,
-    }).isRequired,
-    onAgreementSelected: PropTypes.func.isRequired,
-    agreement: PropTypes.shape({
-      agreementStatus: PropTypes.shape({
-        label: PropTypes.string
-      }),
-      endDate: PropTypes.string,
-      id: PropTypes.string,
-      name: PropTypes.string,
-      label: PropTypes.string,
-      startDate: PropTypes.string,
+const propTypes = {
+  id: PropTypes.string,
+  input: PropTypes.shape({
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  }).isRequired,
+  meta: PropTypes.shape({
+    error: PropTypes.node,
+    touched: PropTypes.bool,
+  }).isRequired,
+  onAgreementSelected: PropTypes.func.isRequired,
+  agreement: PropTypes.shape({
+    agreementStatus: PropTypes.shape({
+      label: PropTypes.string
     }),
-  }
+    endDate: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    label: PropTypes.string,
+    startDate: PropTypes.string,
+  }),
+};
+export default function RelatedAgreementField({
+  id,
+  input,
+  meta,
+  onAgreementSelected,
+  agreement,
+}) {
 
-  static defaultProps = {
-    agreement: {},
-  }
+  let triggerButton = useRef(null);
 
-  componentDidMount() {
-    if (!get(this.props, 'input.value.id') && get(this.triggerButton, 'current')) {
-      this.triggerButton.current.focus();
+  useEffect(() => {
+    if (!input.value.id && triggerButton.current) {
+      triggerButton.current.focus();
     }
-  }
+  });
 
-  renderLinkAgreementButton = value => {
-    const {
-      id,
-      input: { name },
-      onAgreementSelected,
-    } = this.props;
+  const renderLinkAgreementButton = value => {
+    const name = input.name;
 
     return (
       <Pluggable
         dataKey={id}
         onAgreementSelected={onAgreementSelected}
-        renderTrigger={(props) => {
-          this.triggerButton = props.buttonRef;
+        renderTrigger={(pluggableRenderProps) => {
+          triggerButton = pluggableRenderProps.buttonRef;
 
-          const agreementName = get(this.props.agreement, 'name');
+          const agreementName = agreement.name;
           const buttonProps = {
             'aria-haspopup': 'true',
-            'buttonRef': this.triggerButton,
+            'buttonRef': triggerButton,
             'buttonStyle': value ? 'default' : 'primary',
             'id': `${id}-find-agreement-btn`,
             'marginBottom0': true,
             'name': name,
-            'onClick': props.onClick
+            'onClick': pluggableRenderProps.onClick
           };
 
           if (value) {
             return (
               <Tooltip
-                id={`${this.props.id}-license-button-tooltip`}
+                id={`${id}-license-button-tooltip`}
                 text={<FormattedMessage id="ui-agreements.relatedAgreements.replaceAgreementSpecific" values={{ agreementName }} />}
-                triggerRef={this.triggerButton}
+                triggerRef={triggerButton}
               >
                 {({ ariaIds }) => (
                   <Button
@@ -109,11 +108,9 @@ export default class RelatedAgreementField extends React.Component {
         <FormattedMessage id="ui-agreements.relatedAgreements.noPlugin" />
       </Pluggable>
     );
-  }
+  };
 
-  renderAgreement = () => {
-    const { agreement } = this.props;
-
+  const renderAgreement = () => {
     return (
       <div>
         <Link
@@ -144,53 +141,53 @@ export default class RelatedAgreementField extends React.Component {
         </Row>
       </div>
     );
-  }
+  };
 
-  renderEmpty = () => (
-    <div>
-      <Layout className="textCentered">
-        <strong>
-          <FormattedMessage id="ui-agreements.relatedAgreements.noneLinked" />
-        </strong>
-      </Layout>
-      <Layout className="textCentered">
-        <FormattedMessage id="ui-agreements.relatedAgreements.linkToStart" />
-      </Layout>
-    </div>
-  )
+  const renderEmpty = () => {
+    return (
+      <div>
+        <Layout className="textCentered">
+          <strong>
+            <FormattedMessage id="ui-agreements.relatedAgreements.noneLinked" />
+          </strong>
+        </Layout>
+        <Layout className="textCentered">
+          <FormattedMessage id="ui-agreements.relatedAgreements.linkToStart" />
+        </Layout>
+      </div>
+    );
+  };
 
-  renderError = () => (
+  const renderError = () => (
     <Layout className={`textCentered ${css.error}`}>
       <strong>
-        {this.props.meta.error}
+        {meta.error}
       </strong>
     </Layout>
-  )
+  );
 
-  render() {
-    const {
-      id,
-      input: { value },
-      meta: { error, touched }
-    } = this.props;
-
-    return (
-      <Card
-        cardStyle={value ? 'positive' : 'negative'}
-        headerEnd={this.renderLinkAgreementButton(value)}
-        headerStart={(
-          <AppIcon app="agreements" size="small">
-            <strong>
-              <FormattedMessage id="ui-agreements.agreement" />
-            </strong>
-          </AppIcon>
+  return (
+    <Card
+      cardStyle={input.value ? 'positive' : 'negative'}
+      headerEnd={renderLinkAgreementButton(input.value)}
+      headerStart={(
+        <AppIcon app="agreements" size="small">
+          <strong>
+            <FormattedMessage id="ui-agreements.agreement" />
+          </strong>
+        </AppIcon>
         )}
-        id={id}
-        roundedBorder
-      >
-        {value ? this.renderAgreement() : this.renderEmpty()}
-        {touched && error ? this.renderError() : null}
-      </Card>
-    );
-  }
+      id={id}
+      roundedBorder
+    >
+      {input.value ? renderAgreement() : renderEmpty()}
+      {meta.touched && meta.error ? renderError() : null}
+    </Card>
+  );
 }
+
+RelatedAgreementField.propTypes = propTypes;
+RelatedAgreementField.defaultProps = {
+  agreement: {},
+};
+
