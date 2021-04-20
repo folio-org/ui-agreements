138f0241ab6025955e19ba278434d96d1ebd2a9f
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-04-20 11:54:48 +0100

    lookup component in registry is now a full lookup card

--- src/RegistryLookup.js
+++ src/RegistryLookup.js
@@ -1,12 +1,23 @@
 import React, { useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import { Link } from 'react-router-dom';
+
 import {
   Button,
+  Card,
+  Col,
+  FormattedUTCDate,
+  KeyValue,
+  Layout,
+  NoValue,
+  Row,
   Tooltip
 } from '@folio/stripes/components';
 
-import { Pluggable } from '@folio/stripes/core';
+import { AppIcon, Pluggable } from '@folio/stripes/core';
+
+import { urls } from './components/utilities';
 
 // This must return a function to render a link button
 
@@ -63,11 +74,66 @@ const AgreementLookupComponent = ({id, input: { name, value }, onResourceSelecte
     </Pluggable>
   );
 
+  const renderAgreement = () => (
+    <div>
+      <Link
+        data-test-agreement-card-name
+        to={urls.agreementView(resource.id)}
+      >
+        {resource.name}
+      </Link>
+      <Row>
+        <Col md={4} xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-agreements.agreements.startDate" />}
+            value={resource.startDate ? <FormattedUTCDate value={resource.startDate} /> : <NoValue />}
+          />
+        </Col>
+        <Col md={4} xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-agreements.agreements.endDate" />}
+            value={resource.endDate ? <FormattedUTCDate value={resource.endDate} /> : <NoValue />}
+          />
+        </Col>
+        <Col md={4} xs={12}>
+          <KeyValue
+            label={<FormattedMessage id="ui-agreements.agreements.agreementStatus" />}
+            value={resource?.agreementStatus?.label ?? <NoValue />}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+
+  const renderEmpty = () => (
+    <div>
+      <Layout className="textCentered">
+        <strong>
+          <FormattedMessage id="ui-agreements.relatedAgreements.noneLinked" />
+        </strong>
+      </Layout>
+      <Layout className="textCentered">
+        <FormattedMessage id="ui-agreements.relatedAgreements.linkToStart" />
+      </Layout>
+    </div>
+  );
+
   return (
-    <>
-      {renderAgreementLinkButton(value)}
-      {JSON.stringify(resource, null, 2)}
-    </>
+    <Card
+      cardStyle={value ? 'positive' : 'negative'}
+      headerEnd={renderAgreementLinkButton(value)}
+      headerStart={(
+        <AppIcon app="agreements" size="small">
+          <strong>
+            <FormattedMessage id="ui-agreements.agreement" />
+          </strong>
+        </AppIcon>
+        )}
+      id={id}
+      roundedBorder
+    >
+      {value ? renderAgreement() : renderEmpty()}
+    </Card>
   );
 };
 
