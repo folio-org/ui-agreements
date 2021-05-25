348ad512238450fd0e73af5e0cbc396ae36c891a
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-05-25 15:46:45 +0100

    added necessary components to agreements Registry

--- src/setUpRegistry.js
+++ src/setUpRegistry.js
@@ -1,13 +1,5 @@
 import React from 'react';
-
-import {
-  FormattedUTCDate,
-  NoValue,
-} from '@folio/stripes/components';
-
-
 import { InternalContactsArrayDisplay } from './components/registryRenderFunctions'
-
 import AgreementLookup from './AgreementLookup';
 
 const setUpRegistry = (registry) => {
@@ -15,13 +7,6 @@ const setUpRegistry = (registry) => {
   const agreementReg = registry.registerResource('agreement');
   agreementReg.addViewAll('/erm/agreements');
   agreementReg.addViewTemplate(agreement => `/erm/agreements/${agreement.id}`);
-
-  // Testing custom render function.
-  agreementReg.setRenderFunction('currentPeriodStartDate', record => {
-    const date = record.periods.find(p => p.periodStatus === 'current')?.startDate;
-    return date ? <FormattedUTCDate value={date} /> :
-    <NoValue />;
-  });
 
   agreementReg.setRenderFunction('internalContacts', record => {
     return <InternalContactsArrayDisplay contacts={record.contacts}/>;
