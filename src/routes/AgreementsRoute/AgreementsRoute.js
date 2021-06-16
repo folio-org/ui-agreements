e51025d5875fde788ae1c1fb2672a2888dc01dda
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2021-06-16 16:58:48 +0200

    add sortKeys to AgreementsRoute manifest, refs ERM-1691

--- src/routes/AgreementsRoute/AgreementsRoute.js
+++ src/routes/AgreementsRoute/AgreementsRoute.js
@@ -35,6 +35,9 @@ class AgreementsRoute extends React.Component {
           role: 'orgs.role',
           tags: 'tags.value',
         },
+        sortKeys: {
+          agreementStatus: 'agreementStatus.label',
+        },
       }),
     },
     agreementStatusValues: {
