a7ba97b14847df3324665ae9d417194848c84d00
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-07-09 16:21:18 +0100

    converted ExternalLicenses to functional

--- src/components/AgreementSections/ExternalLicenses.js
+++ src/components/AgreementSections/ExternalLicenses.js
@@ -1,65 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Accordion, Badge, Layout } from '@folio/stripes/components';
 import { DocumentCard } from '@folio/stripes-erm-components';
-import { withStripes } from '@folio/stripes/core';
+import { useStripes } from '@folio/stripes/core';
 
-class ExternalLicenses extends React.Component {
-  static propTypes = {
-    agreement: PropTypes.shape({
-      externalLicenseDocs: PropTypes.arrayOf(
-        PropTypes.shape({
-          dateCreated: PropTypes.string,
-          lastUpdated: PropTypes.string,
-          location: PropTypes.string,
-          name: PropTypes.string.isRequired,
-          note: PropTypes.string,
-          url: PropTypes.string,
-        }),
-      ),
-    }),
-    handlers: PropTypes.shape({
-      onDownloadFile: PropTypes.func,
-    }).isRequired,
-    id: PropTypes.string,
-    stripes: PropTypes.object
-  };
+const ExternalLicenses = ({
+  agreement: {
+    externalLicenseDocs: externalLicenses = []
+  } = {},
+  handlers,
+  id
+}) => {
+  const stripes = useStripes();
 
-  renderExternalLicense = license => {
-    const { stripes } = this.props;
+  const renderExternalLicense = license => {
     return (
       <DocumentCard
         key={license.id}
         hasDownloadPerm={stripes.hasPerm('ui-agreements.agreements.file.download')}
-        onDownloadFile={this.props.handlers.onDownloadFile}
+        onDownloadFile={handlers.onDownloadFile}
         {...license}
       />
     );
-  }
+  };
 
-  renderEmpty = () => (
+  const renderEmpty = () => (
     <Layout className="padding-bottom-gutter">
       <FormattedMessage id="ui-agreements.emptyAccordion.externalLicenses" />
     </Layout>
-  )
+  );
 
-  render() {
-    const { id } = this.props;
-    const externalLicenses = get(this.props, 'agreement.externalLicenseDocs', []);
+  return (
+    <Accordion
+      displayWhenClosed={<Badge>{externalLicenses.length}</Badge>}
+      displayWhenOpen={<Badge>{externalLicenses.length}</Badge>}
+      id={id}
+      label={<FormattedMessage id="ui-agreements.license.externalLicenses" />}
+    >
+      { externalLicenses.length ? externalLicenses.map(renderExternalLicense) : renderEmpty() }
+    </Accordion>
+  );
+};
 
-    return (
-      <Accordion
-        displayWhenClosed={<Badge>{externalLicenses.length}</Badge>}
-        displayWhenOpen={<Badge>{externalLicenses.length}</Badge>}
-        id={id}
-        label={<FormattedMessage id="ui-agreements.license.externalLicenses" />}
-      >
-        { externalLicenses.length ? externalLicenses.map(this.renderExternalLicense) : this.renderEmpty() }
-      </Accordion>
-    );
-  }
-}
+ExternalLicenses.propTypes = {
+  agreement: PropTypes.shape({
+    externalLicenseDocs: PropTypes.arrayOf(
+      PropTypes.shape({
+        dateCreated: PropTypes.string,
+        lastUpdated: PropTypes.string,
+        location: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        note: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ),
+  }),
+  handlers: PropTypes.shape({
+    onDownloadFile: PropTypes.func,
+  }).isRequired,
+  id: PropTypes.string,
+};
 
-export default withStripes(ExternalLicenses);
+export default ExternalLicenses;
