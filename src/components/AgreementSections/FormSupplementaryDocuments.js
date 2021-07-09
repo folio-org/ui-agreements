5b6772d259d6702568f34b3eba9d720a194b37b6
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-07-09 16:25:24 +0100

    converted FormSupplementaryDocuments to functional

--- src/components/AgreementSections/FormSupplementaryDocuments.js
+++ src/components/AgreementSections/FormSupplementaryDocuments.js
@@ -5,46 +5,45 @@ import { FieldArray } from 'react-final-form-arrays';
 
 import { Accordion } from '@folio/stripes/components';
 import { DocumentsFieldArray } from '@folio/stripes-erm-components';
-import { withStripes } from '@folio/stripes/core';
+import { useStripes } from '@folio/stripes/core';
 
-class FormSupplementaryDocuments extends React.Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      documentCategories: PropTypes.arrayOf(PropTypes.object),
-    }).isRequired,
-    handlers: PropTypes.shape({
-      onDownloadFile: PropTypes.func.isRequired,
-      onUploadFile: PropTypes.func.isRequired,
-    }),
-    id: PropTypes.string,
-    onToggle: PropTypes.func,
-    open: PropTypes.bool,
-    stripes: PropTypes.object
-  };
+const FormSupplementaryDocuments = ({ data, handlers, id, onToggle, open }) => {
+  const stripes = useStripes();
 
-  render() {
-    const { data, handlers, id, onToggle, open, stripes } = this.props;
-    return (
-      <Accordion
-        id={id}
-        label={<FormattedMessage id="ui-agreements.supplementaryDocuments" />}
-        onToggle={onToggle}
-        open={open}
-      >
-        <FieldArray
-          addDocBtnLabel={<FormattedMessage id="ui-agreements.supplementaryDocs.addSupplementaryDoc" />}
-          component={DocumentsFieldArray}
-          deleteBtnTooltipMsgId="ui-agreements.doc.removeSupplementaryInformation"
-          documentCategories={data.documentCategories}
-          hasDownloadPerm={stripes.hasPerm('ui-agreements.agreements.file.download')}
-          isEmptyMessage={<FormattedMessage id="ui-agreements.supplementaryDocs.agreementHasNone" />}
-          name="supplementaryDocs"
-          onDownloadFile={handlers.onDownloadFile}
-          onUploadFile={handlers.onUploadFile}
-        />
-      </Accordion>
-    );
-  }
-}
+  return (
+    <Accordion
+      id={id}
+      label={<FormattedMessage id="ui-agreements.supplementaryDocuments" />}
+      onToggle={onToggle}
+      open={open}
+    >
+      <FieldArray
+        addDocBtnLabel={<FormattedMessage id="ui-agreements.supplementaryDocs.addSupplementaryDoc" />}
+        component={DocumentsFieldArray}
+        deleteBtnTooltipMsgId="ui-agreements.doc.removeSupplementaryInformation"
+        documentCategories={data.documentCategories}
+        hasDownloadPerm={stripes.hasPerm('ui-agreements.agreements.file.download')}
+        isEmptyMessage={<FormattedMessage id="ui-agreements.supplementaryDocs.agreementHasNone" />}
+        name="supplementaryDocs"
+        onDownloadFile={handlers.onDownloadFile}
+        onUploadFile={handlers.onUploadFile}
+      />
+    </Accordion>
+  );
+};
 
-export default withStripes(FormSupplementaryDocuments);
+FormSupplementaryDocuments.propTypes = {
+  data: PropTypes.shape({
+    documentCategories: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+  handlers: PropTypes.shape({
+    onDownloadFile: PropTypes.func.isRequired,
+    onUploadFile: PropTypes.func.isRequired,
+  }),
+  id: PropTypes.string,
+  onToggle: PropTypes.func,
+  open: PropTypes.bool,
+  stripes: PropTypes.object
+};
+
+export default FormSupplementaryDocuments;
