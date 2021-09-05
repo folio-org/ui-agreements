ccc565e9a0450890c7522a225c213e9f4729e623
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-05 19:03:29 +0100

    Added test with initial values and with type packages

--- src/components/AgreementLineSections/FormeresourceCard/FormEresourceCard.test.js
+++ src/components/AgreementLineSections/FormeresourceCard/FormEresourceCard.test.js
@@ -3,46 +3,27 @@ import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { TestForm, renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
 import FormEresourceCard from './FormEresourceCard';
+import { packagesResource, resource, initialValuesData, line, handlers, initialValues } from './testResources';
 import translationsProperties from '../../../../test/helpers';
 
 jest.mock('../../PackageCardExternal', () => () => <div>PackageCardExternal</div>);
+jest.mock('../../PackageCard', () => () => <div>PackageCard</div>);
+
+const isSuppressFromDiscoveryEnabled = jest.fn().mockImplementation(res => res);
 const onSubmit = jest.fn();
 
-const data = {
-  'component': 'ƒ FormEresourceCard() {}',
-  'headerEnd': '<ForwardRef />',
-};
-
-const resource = {
-  'contentType': 'Aggregated Full Text',
-  'customCoverage': {
-    'beginCoverage': '',
-    'endCoverage': ''
-  },
-  'isCustom': false,
-  'isSelected': true,
-  'name': 'i-law.com',
-  'packageId': 3581,
-  'packageType': 'Variable',
-  'providerId': 936,
-  'providerName': 'Informa Law & Finance',
-  'selectedCount': 181,
-  'titleCount': 181,
-  'visibilityData': {
-    'isHidden': false,
-    'reason': ''
-  },
-  'id': '936-3581',
-  'type': 'packages'
-};
-
-let renderComponent;
 describe('FormEresourceCard', () => {
+  let renderComponent;
+  describe('renders with no initialValues', () => {
     beforeEach(() => {
       renderComponent = renderWithIntl(
         <TestForm onSubmit={onSubmit}>
-          <FormEresourceCard data={data} resource={resource} />
-        </TestForm>, translationsProperties
+          <FormEresourceCard
+            isSuppressFromDiscoveryEnabled={isSuppressFromDiscoveryEnabled}
+            resource={resource}
+          />
+        </TestForm>,
+        translationsProperties
       );
     });
 
@@ -50,4 +31,27 @@ describe('FormEresourceCard', () => {
       const { getByText } = renderComponent;
       expect(getByText('PackageCardExternal')).toBeInTheDocument();
     });
+  });
+
+  describe('renders with initialValues', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <TestForm initialValues={initialValues} onSubmit={onSubmit}>
+          <FormEresourceCard
+            handlers={handlers}
+            initialValuesData={initialValuesData}
+            isSuppressFromDiscoveryEnabled={isSuppressFromDiscoveryEnabled}
+            line={line}
+            resource={packagesResource}
+          />
+        </TestForm>,
+        translationsProperties
+      );
+    });
+
+    test('renders the PackageCard', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('PackageCard')).toBeInTheDocument();
+    });
+  });
 });
---
+++ src/components/AgreementLineSections/FormeresourceCard/testResources.js
@@ -0,0 +1,217 @@
+const packagesResource = {
+    'id': 'bb60ec83-3af5-40c5-a689-0b2e214e0bf7',
+    'dateCreated': '2021-09-05T02:32:37Z',
+    'tags': '[]',
+    'lastUpdated': '2021-09-05T02:32:37Z',
+    'vendor': {
+    'id': '3d947738-8feb-4e68-9f12-ada13616ba76',
+    'name': 'Association for Computing Machinery',
+    'orgsUuid_object': {
+        'error': 400,
+        'message': 'Bad Request'
+    }
+    },
+    'coverage': '[]',
+    'source': 'GOKb',
+    'remoteKb': {
+    'id': 'd11f210b-6856-4ee2-9d12-12f95123e7db',
+    'cursor': '2021-09-04T06:00:41Z',
+    'active': true,
+    'trustedSourceTI': false,
+    'activationEnabled': false,
+    'readonly': false,
+    'syncStatus': 'idle',
+    'lastCheck': 1630856719085,
+    'name': 'GOKb_TEST',
+    'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+    'fullPrefix': 'gokb',
+    'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+    'supportsHarvesting': true,
+    'rectype': 1
+    },
+    'name': 'ACM Digtal Library',
+    'suppressFromDiscovery': false,
+    'reference': 'ACM_Digtal_Library_2',
+    'resourceCount': 0,
+    'class': 'org.olf.kb.Pkg'
+};
+
+const resource = {
+    'contentType': 'Aggregated Full Text',
+    'customCoverage': {
+        'beginCoverage': '',
+        'endCoverage': ''
+    },
+    'isCustom': false,
+    'isSelected': true,
+    'name': 'i-law.com',
+    'packageId': 3581,
+    'packageType': 'Variable',
+    'providerId': 936,
+    'providerName': 'Informa Law & Finance',
+    'selectedCount': 181,
+    'titleCount': 181,
+    'visibilityData': {
+        'isHidden': false,
+        'reason': ''
+    },
+    'id': '936-3581',
+    'type': 'packages'
+};
+
+const initialValuesData = {
+    'basket': '[]',
+    'isEholdingsEnabled': true,
+    'isLoading': false,
+    'lineId': '986dd5b8-b252-485e-9f7b-2021ab38c422',
+    'onSubmit': 'ƒ () {}'
+};
+
+const line = {
+    'id': '986dd5b8-b252-485e-9f7b-2021ab38c422',
+    'dateCreated': '2021-09-05T12:45:57Z',
+    'activeTo': '2021-09-30',
+    'tags': [],
+    'lastUpdated': '2021-09-05T12:45:57Z',
+    'owner': {
+        'id': 'a350a2d9-75ff-48e5-8d7e-164da901397a',
+        'cancellationDeadline': '2021-09-26',
+        'dateCreated': '2021-09-05T12:43:47Z',
+        'isPerpetual': {
+        'id': '2c91809c7bb3a08e017bb3a79752000a',
+        'value': 'yes',
+        'label': 'Yes'
+        },
+        'name': 'MR agr packages',
+        'orgs': '[]',
+        'externalLicenseDocs': '[]',
+        'outwardRelationships': '[]',
+        'customProperties': '{}',
+        'contacts': '[]',
+        'tags': '[]',
+        'lastUpdated': '2021-09-05T13:30:42Z',
+        'inwardRelationships': '[]',
+        'renewalPriority': {
+        'id': '2c91809c7bb3a08e017bb3a797790011',
+        'value': 'definitely_renew',
+        'label': 'Definitely renew'
+        },
+        'endDate': '2021-09-30',
+        'startDate': '2021-09-01',
+        'linkedLicenses': '[]',
+        'docs': '[]',
+        'periods': [
+        {
+            'id': '12cf839c-a513-42b0-96cc-ead840205c8f',
+            'startDate': '2021-09-01',
+            'cancellationDeadline': '2021-09-26',
+            'owner': {
+            'id': 'a350a2d9-75ff-48e5-8d7e-164da901397a'
+            },
+            'note': 'This is period note',
+            'endDate': '2021-09-30',
+            'periodStatus': 'current'
+        }
+        ],
+        'usageDataProviders': '[]',
+        'agreementStatus': {
+        'id': '2c91809c7bb3a08e017bb3a797c6001a',
+        'value': 'active',
+        'label': 'Active'
+        },
+        'supplementaryDocs': '[]',
+        'description': 'This is description',
+        'items': [
+        {
+            'id': '986dd5b8-b252-485e-9f7b-2021ab38c422'
+        }
+        ],
+        'alternateNames': '[]'
+    },
+    'resource': {
+        'id': 'bb60ec83-3af5-40c5-a689-0b2e214e0bf7',
+        'class': 'org.olf.kb.Pkg',
+        'name': 'ACM Digtal Library',
+        'suppressFromDiscovery': false,
+        'tags': '[]',
+        'customCoverage': false,
+        '_object': {
+        'id': 'bb60ec83-3af5-40c5-a689-0b2e214e0bf7',
+        'dateCreated': '2021-09-05T02:32:37Z',
+        'tags': [],
+        'lastUpdated': '2021-09-05T02:32:37Z',
+        'vendor': {
+            'id': '3d947738-8feb-4e68-9f12-ada13616ba76',
+            'name': 'Association for Computing Machinery',
+            'orgsUuid_object': {
+            'error': 400,
+            'message': 'Bad Request'
+            }
+        },
+        'coverage': [],
+        'source': 'GOKb',
+        'remoteKb': {
+            'id': 'd11f210b-6856-4ee2-9d12-12f95123e7db',
+            'cursor': '2021-09-04T06:00:41Z',
+            'active': true,
+            'trustedSourceTI': false,
+            'activationEnabled': false,
+            'readonly': false,
+            'syncStatus': 'idle',
+            'lastCheck': 1630856719085,
+            'name': 'GOKb_TEST',
+            'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+            'fullPrefix': 'gokb',
+            'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+            'supportsHarvesting': true,
+            'rectype': 1
+        },
+        'name': 'ACM Digtal Library',
+        'suppressFromDiscovery': false,
+        'reference': 'ACM_Digtal_Library_2',
+        'resourceCount': 0,
+        'class': 'org.olf.kb.Pkg'
+        }
+    },
+    'activeFrom': '2021-09-01',
+    'poLines': [],
+    'suppressFromDiscovery': true,
+    'note': 'This is note',
+    'description': 'This is agreement line description',
+    'customCoverage': false,
+    'explanation': 'Agreement includes a package containing this item',
+    'startDate': '2021-09-01',
+    'endDate': '2021-09-30',
+    'contentUpdated': null,
+    'haveAccess': true
+};
+
+const handlers = {
+    isSuppressFromDiscoveryEnabled: () => {},
+    onClose: () => {}
+};
+
+const initialValues = {
+    'id': '986dd5b8-b252-485e-9f7b-2021ab38c422',
+    'dateCreated': '2021-09-05T12:45:57Z',
+    'activeTo': '2021-09-30',
+    'tags': '[]',
+    'lastUpdated': '2021-09-05T12:45:57Z',
+    'owner': '{agreementStatus: {…}, alternateNames: Array(0), ca…}',
+    'resource': '{_object: {…}, class: "org.olf.kb.Pkg", customCover…}',
+    'activeFrom': '2021-09-01',
+    'poLines': '[]',
+    'suppressFromDiscovery': true,
+    'note': 'This is note',
+    'description': 'This is agreement line description',
+    'customCoverage': false,
+    'explanation': 'Agreement includes a package containing this item',
+    'startDate': '2021-09-01',
+    'endDate': '2021-09-30',
+    'contentUpdated': null,
+    'haveAccess': true,
+    'linkedResource': '{activeFrom: "2021-09-01", activeTo: "2021-09-30", …}'
+};
+
+export { packagesResource, resource, initialValuesData, line, handlers, initialValues };
+
