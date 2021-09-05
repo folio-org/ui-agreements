6a783cd5d2092fe3b43dee31504f2fbddea7d210
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-05 15:44:57 +0100

    Added test for type packages

--- src/components/AgreementLineSections/Info/Info.test.js
+++ src/components/AgreementLineSections/Info/Info.test.js
@@ -3,162 +3,29 @@ import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
 import { MemoryRouter } from 'react-router-dom';
 import { KeyValue } from '@folio/stripes-testing';
+import { externalLine, externalEesource, packagesLine, packagesResource } from './testReasource';
 import translationsProperties from '../../../../test/helpers';
 import Info from './Info';
 
-jest.mock('../../PackageCardExternal', () => () => <div>PackageCardExternal</div>);
+jest.mock('../../PackageCardExternal', () => () => (<div>PackageCardExternal</div>));
+jest.mock('../../PackageCard', () => () => (<div>PackageCard</div>));
 
-const isSuppressFromDiscoveryEnabled = jest.fn().mockImplementation(resource => resource);
+const isSuppressFromDiscoveryEnabled = jest.fn().mockImplementation((res) => res);
 
-const line = {
-    'id': '0bc141e5-9cb8-432f-bd61-69069ada4f03',
-    'type': 'external',
-    'description': 'This is description.',
-    'authority': 'EKB-PACKAGE',
-    'reference': '122380-2556389',
-    'explanation': null,
-    'startDate': '2021-08-04',
-    'endDate': '2021-08-28',
-    'activeFrom': '2021-08-04',
-    'activeTo': '2021-08-28',
-    'contentUpdated': null,
-    'haveAccess': true,
-    'suppressFromDiscovery': true,
-    'note': 'This is note.',
-    'tags': '[]',
-    'owner': {
-      'id': '59cf8946-1496-4434-8c9c-4dbe218ad83c',
-      'dateCreated': '2021-08-23T08:13:21Z',
-      'isPerpetual': {
-        'id': '2c91809c7b70aeb1017b70b5e700001d',
-        'value': 'yes',
-        'label': 'Yes'
-      },
-      'name': 'MR test Info',
-      'orgs': [],
-      'externalLicenseDocs': [],
-      'outwardRelationships': [],
-      'customProperties': {},
-      'contacts': [],
-      'tags': [],
-      'lastUpdated': '2021-08-23T08:13:21Z',
-      'inwardRelationships': [],
-      'renewalPriority': {
-        'id': '2c91809c7b70aeb1017b70b5e71d0024',
-        'value': 'definitely_renew',
-        'label': 'Definitely renew'
-      },
-      'endDate': '2021-08-28',
-      'startDate': '2021-08-04',
-      'linkedLicenses': [],
-      'docs': [],
-      'periods': [
-        '{endDate: "2021-08-28", id: "6b1b4903-3f14-4f51-add…}'
-      ],
-      'usageDataProviders': [],
-      'agreementStatus': {
-        'id': '2c91809c7b70aeb1017b70b5e767002d',
-        'value': 'active',
-        'label': 'Active'
-      },
-      'supplementaryDocs': [],
-      'description': 'This is description.',
-      'cancellationDeadline': null,
-      'items': [
-        '{id: "0bc141e5-9cb8-432f-bd61-69069ada4f03"}'
-      ],
-      'alternateNames': []
-    },
-    'customCoverage': false,
-    'reference_object': {
-      'label': 'i-Scholar',
-      'type': 'Package',
-      'provider': 'Informatics Publishing Limited',
-      'titleCount': 1028,
-      'selectedCount': 0,
-      'contentType': 'E-Journal',
-      'providerName': 'Informatics Publishing Limited'
-    },
-    'poLines': '[]'
-  };
-
- const resource = {
-    'id': '0bc141e5-9cb8-432f-bd61-69069ada4f03',
-    'type': 'external',
-    'description': 'This is description.',
-    'authority': 'EKB-PACKAGE',
-    'reference': '122380-2556389',
-    'explanation': null,
-    'startDate': '2021-08-04',
-    'endDate': '2021-08-28',
-    'activeFrom': '2021-08-04',
-    'activeTo': '2021-08-28',
-    'contentUpdated': null,
-    'haveAccess': true,
-    'suppressFromDiscovery': true,
-    'note': 'This is note.',
-    'tags': '[]',
-    'owner': {
-      'id': '59cf8946-1496-4434-8c9c-4dbe218ad83c',
-      'dateCreated': '2021-08-23T08:13:21Z',
-      'isPerpetual': {
-        'id': '2c91809c7b70aeb1017b70b5e700001d',
-        'value': 'yes',
-        'label': 'Yes'
-      },
-      'name': 'Mr test Info',
-      'orgs': [],
-      'externalLicenseDocs': [],
-      'outwardRelationships': [],
-      'customProperties': {},
-      'contacts': [],
-      'tags': [],
-      'lastUpdated': '2021-08-23T08:13:21Z',
-      'inwardRelationships': [],
-      'renewalPriority': {
-        'id': '2c91809c7b70aeb1017b70b5e71d0024',
-        'value': 'definitely_renew',
-        'label': 'Definitely renew'
-      },
-      'endDate': '2021-08-28',
-      'startDate': '2021-08-04',
-      'linkedLicenses': [],
-      'docs': [],
-      'periods': [
-        '{endDate: "2021-08-28", id: "6b1b4903-3f14-4f51-add…}'
-      ],
-      'usageDataProviders': [],
-      'agreementStatus': {
-        'id': '2c91809c7b70aeb1017b70b5e767002d',
-        'value': 'active',
-        'label': 'Active'
-      },
-      'supplementaryDocs': [],
-      'description': 'This is description.',
-      'cancellationDeadline': null,
-      'items': [
-        '{id: "0bc141e5-9cb8-432f-bd61-69069ada4f03"}'
-      ],
-      'alternateNames': []
-    },
-    'customCoverage': false,
-    'reference_object': '{contentType: "E-Journal", label: "i-Scholar", prov…}',
-    'poLines': '[]'
-};
-
-    let renderComponent;
-    describe('Info', () => {
+describe('Info', () => {
+  let renderComponent;
+  describe('Info with external type', () => {
     beforeEach(() => {
-        renderComponent = renderWithIntl(
-          <MemoryRouter>
-            <Info
-              isSuppressFromDiscoveryEnabled={isSuppressFromDiscoveryEnabled}
-              line={line}
-              resource={resource}
-            />
-          </MemoryRouter>,
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <Info
+            isSuppressFromDiscoveryEnabled={isSuppressFromDiscoveryEnabled}
+            line={externalLine}
+            resource={externalEesource}
+          />
+        </MemoryRouter>,
         translationsProperties
-        );
+      );
     });
 
     test('renders Info component', async () => {
@@ -167,7 +34,7 @@ const line = {
     });
 
     test('displays parent agreements name', async () => {
-        await KeyValue('Parent agreement').has({ value: 'MR test Info' });
+      await KeyValue('Parent agreement').has({ value: 'MR test Info' });
     });
 
     test('displays parent agreement activeFrom date', async () => {
@@ -194,4 +61,52 @@ const line = {
       const { getByText } = renderComponent;
       expect(getByText('PackageCardExternal')).toBeInTheDocument();
     });
+  });
+  describe('Info with packages type', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <Info
+            isSuppressFromDiscoveryEnabled={isSuppressFromDiscoveryEnabled}
+            line={packagesLine}
+            resource={packagesResource}
+          />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders Info component', async () => {
+      const { getByTestId } = renderComponent;
+      expect(getByTestId('lineInfo')).toBeInTheDocument();
+    });
+
+    test('displays parent agreements name', async () => {
+      await KeyValue('Parent agreement').has({ value: 'MR agr packages' });
+    });
+
+    test('displays parent agreement activeFrom date', async () => {
+      await KeyValue('Active from').has({ value: '2021-09-01' });
+    });
+
+    test('displays parent agreement activeTo date', async () => {
+      await KeyValue('Active to').has({ value: '2021-09-30' });
+    });
+
+    test('displays parent agreement suppressFromDiscovery', async () => {
+      await KeyValue('Suppress from discovery').has({ value: 'Yes' });
+    });
+
+    test('dispalys parent agreement note', async () => {
+      await KeyValue('Note').has({ value: 'This is note' });
+    });
+
+    test('dispalys parent agreement description', async () => {
+      await KeyValue('Description').has({ value: 'This is agreement line description' });
+    });
+    test('renders the PackageCard component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('PackageCard')).toBeInTheDocument();
+    });
+  });
 });
---
+++ src/components/AgreementLineSections/Info/testReasource.js
@@ -0,0 +1,255 @@
+const packagesLine = {
+    'id': '986dd5b8-b252-485e-9f7b-2021ab38c422',
+    'dateCreated': '2021-09-05T12:45:57Z',
+    'activeTo': '2021-09-30',
+    'tags': '[]',
+    'lastUpdated': '2021-09-05T12:45:57Z',
+    'owner': {
+      'id': 'a350a2d9-75ff-48e5-8d7e-164da901397a',
+      'cancellationDeadline': '2021-09-26',
+      'dateCreated': '2021-09-05T12:43:47Z',
+      'isPerpetual': {
+        'id': '2c91809c7bb3a08e017bb3a79752000a',
+        'value': 'yes',
+        'label': 'Yes'
+      },
+      'name': 'MR agr packages',
+      'orgs': [],
+      'externalLicenseDocs': [],
+      'outwardRelationships': [],
+      'customProperties': {},
+      'contacts': [],
+      'tags': [],
+      'lastUpdated': '2021-09-05T13:30:42Z',
+      'inwardRelationships': [],
+      'renewalPriority': {
+        'id': '2c91809c7bb3a08e017bb3a797790011',
+        'value': 'definitely_renew',
+        'label': 'Definitely renew'
+      },
+      'endDate': '2021-09-30',
+      'startDate': '2021-09-01',
+      'linkedLicenses': [],
+      'docs': [],
+      'periods': [
+        {
+          'id': '12cf839c-a513-42b0-96cc-ead840205c8f',
+          'startDate': '2021-09-01',
+          'cancellationDeadline': '2021-09-26',
+          'owner': {
+            'id': 'a350a2d9-75ff-48e5-8d7e-164da901397a'
+          },
+          'note': 'This is period note',
+          'endDate': '2021-09-30',
+          'periodStatus': 'current'
+        }
+      ],
+      'usageDataProviders': [],
+      'agreementStatus': {
+        'id': '2c91809c7bb3a08e017bb3a797c6001a',
+        'value': 'active',
+        'label': 'Active'
+      },
+      'supplementaryDocs': [],
+      'description': 'This is description',
+      'items': [
+        '{id: "986dd5b8-b252-485e-9f7b-2021ab38c422"}'
+      ],
+      'alternateNames': []
+    },
+    'resource': {
+      'id': 'bb60ec83-3af5-40c5-a689-0b2e214e0bf7',
+      'class': 'org.olf.kb.Pkg',
+      'name': 'ACM Digtal Library',
+      'suppressFromDiscovery': false,
+      'tags': [],
+      'customCoverage': false,
+      '_object': {
+        'id': 'bb60ec83-3af5-40c5-a689-0b2e214e0bf7',
+        'dateCreated': '2021-09-05T02:32:37Z',
+        'tags': '[]',
+        'lastUpdated': '2021-09-05T02:32:37Z',
+        'vendor': '{id: "3d947738-8feb-4e68-9f12-ada13616ba76", name: …}',
+        'coverage': '[]',
+        'source': 'GOKb',
+        'remoteKb': '{activationEnabled: false, active: true, cursor: "2…}',
+        'name': 'ACM Digtal Library',
+        'suppressFromDiscovery': false,
+        'reference': 'ACM_Digtal_Library_2',
+        'resourceCount': 0,
+        'class': 'org.olf.kb.Pkg'
+      }
+    },
+    'activeFrom': '2021-09-01',
+    'poLines': '[]',
+    'suppressFromDiscovery': true,
+    'note': 'This is note',
+    'description': 'This is agreement line description',
+    'customCoverage': false,
+    'explanation': 'Agreement includes a package containing this item',
+    'startDate': '2021-09-01',
+    'endDate': '2021-09-30',
+    'contentUpdated': null,
+    'haveAccess': true
+};
+
+const packagesResource = {
+  'id': 'bb60ec83-3af5-40c5-a689-0b2e214e0bf7',
+  'dateCreated': '2021-09-05T02:32:37Z',
+  'tags': '[]',
+  'lastUpdated': '2021-09-05T02:32:37Z',
+  'vendor': {
+    'id': '3d947738-8feb-4e68-9f12-ada13616ba76',
+    'name': 'Association for Computing Machinery',
+    'orgsUuid_object': {
+      'error': 400,
+      'message': 'Bad Request'
+    }
+  },
+  'coverage': '[]',
+  'source': 'GOKb',
+  'remoteKb': '{activationEnabled: false, active: true, cursor: "2…}',
+  'name': 'ACM Digtal Library',
+  'suppressFromDiscovery': false,
+  'reference': 'ACM_Digtal_Library_2',
+  'resourceCount': 0,
+  'class': 'org.olf.kb.Pkg'
+};
+
+const externalLine = {
+    'id': '0bc141e5-9cb8-432f-bd61-69069ada4f03',
+    'type': 'external',
+    'description': 'This is description.',
+    'authority': 'EKB-PACKAGE',
+    'reference': '122380-2556389',
+    'explanation': null,
+    'startDate': '2021-08-04',
+    'endDate': '2021-08-28',
+    'activeFrom': '2021-08-04',
+    'activeTo': '2021-08-28',
+    'contentUpdated': null,
+    'haveAccess': true,
+    'suppressFromDiscovery': true,
+    'note': 'This is note.',
+    'tags': '[]',
+    'owner': {
+      'id': '59cf8946-1496-4434-8c9c-4dbe218ad83c',
+      'dateCreated': '2021-08-23T08:13:21Z',
+      'isPerpetual': {
+        'id': '2c91809c7b70aeb1017b70b5e700001d',
+        'value': 'yes',
+        'label': 'Yes'
+      },
+      'name': 'MR test Info',
+      'orgs': [],
+      'externalLicenseDocs': [],
+      'outwardRelationships': [],
+      'customProperties': {},
+      'contacts': [],
+      'tags': [],
+      'lastUpdated': '2021-08-23T08:13:21Z',
+      'inwardRelationships': [],
+      'renewalPriority': {
+        'id': '2c91809c7b70aeb1017b70b5e71d0024',
+        'value': 'definitely_renew',
+        'label': 'Definitely renew'
+      },
+      'endDate': '2021-08-28',
+      'startDate': '2021-08-04',
+      'linkedLicenses': [],
+      'docs': [],
+      'periods': [
+        '{endDate: "2021-08-28", id: "6b1b4903-3f14-4f51-add…}'
+      ],
+      'usageDataProviders': [],
+      'agreementStatus': {
+        'id': '2c91809c7b70aeb1017b70b5e767002d',
+        'value': 'active',
+        'label': 'Active'
+      },
+      'supplementaryDocs': [],
+      'description': 'This is description.',
+      'cancellationDeadline': null,
+      'items': [
+        '{id: "0bc141e5-9cb8-432f-bd61-69069ada4f03"}'
+      ],
+      'alternateNames': []
+    },
+    'customCoverage': false,
+    'reference_object': {
+      'label': 'i-Scholar',
+      'type': 'Package',
+      'provider': 'Informatics Publishing Limited',
+      'titleCount': 1028,
+      'selectedCount': 0,
+      'contentType': 'E-Journal',
+      'providerName': 'Informatics Publishing Limited'
+    },
+    'poLines': '[]'
+  };
+
+ const externalEesource = {
+    'id': '0bc141e5-9cb8-432f-bd61-69069ada4f03',
+    'type': 'external',
+    'description': 'This is description.',
+    'authority': 'EKB-PACKAGE',
+    'reference': '122380-2556389',
+    'explanation': null,
+    'startDate': '2021-08-04',
+    'endDate': '2021-08-28',
+    'activeFrom': '2021-08-04',
+    'activeTo': '2021-08-28',
+    'contentUpdated': null,
+    'haveAccess': true,
+    'suppressFromDiscovery': true,
+    'note': 'This is note.',
+    'tags': '[]',
+    'owner': {
+      'id': '59cf8946-1496-4434-8c9c-4dbe218ad83c',
+      'dateCreated': '2021-08-23T08:13:21Z',
+      'isPerpetual': {
+        'id': '2c91809c7b70aeb1017b70b5e700001d',
+        'value': 'yes',
+        'label': 'Yes'
+      },
+      'name': 'Mr test Info',
+      'orgs': [],
+      'externalLicenseDocs': [],
+      'outwardRelationships': [],
+      'customProperties': {},
+      'contacts': [],
+      'tags': [],
+      'lastUpdated': '2021-08-23T08:13:21Z',
+      'inwardRelationships': [],
+      'renewalPriority': {
+        'id': '2c91809c7b70aeb1017b70b5e71d0024',
+        'value': 'definitely_renew',
+        'label': 'Definitely renew'
+      },
+      'endDate': '2021-08-28',
+      'startDate': '2021-08-04',
+      'linkedLicenses': [],
+      'docs': [],
+      'periods': [
+        '{endDate: "2021-08-28", id: "6b1b4903-3f14-4f51-add…}'
+      ],
+      'usageDataProviders': [],
+      'agreementStatus': {
+        'id': '2c91809c7b70aeb1017b70b5e767002d',
+        'value': 'active',
+        'label': 'Active'
+      },
+      'supplementaryDocs': [],
+      'description': 'This is description.',
+      'cancellationDeadline': null,
+      'items': [
+        '{id: "0bc141e5-9cb8-432f-bd61-69069ada4f03"}'
+      ],
+      'alternateNames': []
+    },
+    'customCoverage': false,
+    'reference_object': '{contentType: "E-Journal", label: "i-Scholar", prov…}',
+    'poLines': '[]'
+};
+
+export { externalLine, externalEesource, packagesLine, packagesResource };
