12b1d4561d44287a630a406ba10d74314c6f26b0
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-08 13:21:35 +0100

    Added tests for AgreementLineSection EresourceSelector component

--- src/components/AgreementLineSections/EresourceSelector/EresourceSelector.test.js
+++ src/components/AgreementLineSections/EresourceSelector/EresourceSelector.test.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
-import { Button } from '@folio/stripes-testing';
-import { MemoryRouter } from 'react-router-dom';
 import translationsProperties from '../../../../test/helpers';
 import EresourceSelector from './EresourceSelector';
 
 
-const data = {
+const line = {
   'component': 'ƒ EresourceSelector() {}',
   'error': false,
   'name': 'linkedResource',
-  'onAdd': 'ƒ onAdd() {}',
+  'onAdd': () => {},
   'value': ''
 };
 
 describe('EresourceSelector', () => {
   let renderComponent;
   beforeEach(() => {
-    renderWithIntl(
-      <MemoryRouter>
-        <EresourceSelector id="eresourceselector" onAdd={data.onAdd} />
-      </MemoryRouter>,
+    renderComponent = renderWithIntl(
+      <EresourceSelector line={line} />,
       translationsProperties
     );
   });
 
   test('renders eholdings card component', () => {
-    const { getByTestId } = renderComponent;
-    expect(getByTestId('eresourceselector')).toBeInTheDocument();
+    const { getByText } = renderComponent;
+    expect(getByText('eHoldings')).toBeInTheDocument();
    });
 });
 
