8b78e004e0d7e6ed74d68d8a65f0fae2e9ed3f81
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-08 09:43:52 +0100

    Added tests for MCL contents

---
+++ src/components/AgreementLineSections/Coverage/testResources.js
@@ -0,0 +1,457 @@
+const line = {
+    'id': 'e7d75202-3df9-4775-808a-1e38da6e3d60',
+    'dateCreated': '2021-09-08T07:32:34Z',
+    'activeTo': '2021-09-30',
+    'tags': '[]',
+    'lastUpdated': '2021-09-08T07:32:34Z',
+    'owner': {
+    'id': '3f931bb9-5362-4748-bdd2-498462c5bd93',
+    'cancellationDeadline': '2021-09-24',
+    'dateCreated': '2021-09-08T06:56:51Z',
+    'isPerpetual': {
+        'id': '2c91809c7bc31559017bc31d3d270025',
+        'value': 'yes',
+        'label': 'Yes'
+    },
+    'name': 'MR agr test',
+    'orgs': [],
+    'externalLicenseDocs': [],
+    'outwardRelationships': [],
+    'customProperties': {},
+    'contacts': [],
+    'tags': [],
+    'lastUpdated': '2021-09-08T07:29:16Z',
+    'inwardRelationships': [],
+    'renewalPriority': {
+        'id': '2c91809c7bc31559017bc31d3d4d002c',
+        'value': 'definitely_renew',
+        'label': 'Definitely renew'
+    },
+    'endDate': '2021-09-30',
+    'startDate': '2021-09-01',
+    'linkedLicenses': [],
+    'docs': [],
+    'periods': [
+        {
+        'id': '613fd48c-5080-40be-8956-2887ff7d3593',
+        'startDate': '2021-09-01',
+        'cancellationDeadline': '2021-09-24',
+        'owner': {
+            'id': '3f931bb9-5362-4748-bdd2-498462c5bd93'
+        },
+        'note': 'period note',
+        'endDate': '2021-09-30',
+        'periodStatus': 'current'
+        }
+    ],
+    'usageDataProviders': [],
+    'agreementStatus': {
+        'id': '2c91809c7bc31559017bc31d3d9f0035',
+        'value': 'active',
+        'label': 'Active'
+    },
+    'supplementaryDocs': [],
+    'description': 'This is description',
+    'items': [
+        {
+        'id': 'ce538dd8-4612-4808-b1cb-7d48fe3d3beb'
+        },
+        {
+        'id': 'e7d75202-3df9-4775-808a-1e38da6e3d60'
+        },
+        {
+        'id': '7a246f87-46e0-40f3-ad14-aa77c895c52a'
+        }
+    ],
+    'alternateNames': []
+    },
+    'resource': {
+    'id': 'abe6cfde-ee56-4491-9e53-b70acb9a984b',
+    'class': 'org.olf.kb.PackageContentItem',
+    'name': "'\"Institutions, industrial upgrading, and economic performance in Ja...' on Platform 'Elgaronline' in Package Edward Elgar:Edward Elgar E-Book Archive in Business & Management, ...",
+    'suppressFromDiscovery': false,
+    'tags': [],
+    'customCoverage': false,
+    '_object': {
+        'id': 'abe6cfde-ee56-4491-9e53-b70acb9a984b',
+        'accessStart': '2008-01-01',
+        'dateCreated': '2021-09-08T02:06:41Z',
+        'tags': '[]',
+        'lastUpdated': '2021-09-08T02:07:07Z',
+        'depth': 'Fulltext',
+        'coverage': '[]',
+        'pti': {
+        'id': '27b9fe76-d1d2-45e8-b02c-474c32720d57',
+        'dateCreated': '2021-09-08T02:06:41Z',
+        'tags': [],
+        'lastUpdated': '2021-09-08T02:06:41Z',
+        'platform': {
+            'id': '7a45fab1-9816-48d0-a8dd-05b1b8361a99',
+            'dateCreated': '2021-09-08T02:01:19Z',
+            'lastUpdated': '2021-09-08T02:01:19Z',
+            'name': 'Elgaronline',
+            'locators': [
+            {
+                'id': '81fe228d-db09-4075-ad0f-e9aa6542bace',
+                'domainName': 'www.elgaronline.com'
+            }
+            ]
+        },
+        'templatedUrls': [
+            '{id: "08c8bca4-31b7-47b4-8de1-febd0ab46b95", name: …}'
+        ],
+        'coverage': [],
+        'titleInstance': {
+            'id': '7b172272-a8df-4ba9-ae23-cd8a4552a686',
+            'subType': {
+            'id': '2c91809c7bc31559017bc31d3cde0015',
+            'value': 'electronic',
+            'label': 'Electronic'
+            },
+            'dateCreated': '2021-09-08T02:06:41Z',
+            'tags': '[]',
+            'lastUpdated': '2021-09-08T02:06:41Z',
+            'publicationType': {
+            'id': '2c91809c7bc31559017bc31d863e0046',
+            'value': 'book',
+            'label': 'Book'
+            },
+            'identifiers': [
+            {
+                'title': {
+                'id': '7b172272-a8df-4ba9-ae23-cd8a4552a686'
+                },
+                'status': {
+                'id': '2c91809c7bc31559017bc31d480d0045',
+                'value': 'approved',
+                'label': 'approved'
+                },
+                'identifier': {
+                'value': '9781845425678',
+                'ns': {
+                    'value': 'isbn'
+                }
+                }
+            }
+            ],
+            'coverage': '[]',
+            'name': "\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"",
+            'type': {
+            'id': '2c91809c7bc31559017bc31d3ce50017',
+            'value': 'monograph',
+            'label': 'Monograph'
+            },
+            'suppressFromDiscovery': false,
+            'work': {
+            'id': 'd6914bfc-4706-4c31-84ad-389950469461'
+            },
+            'class': 'org.olf.kb.TitleInstance',
+            'longName': "\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"",
+            'relatedTitles': '[]'
+        },
+        'url': 'https://doi.org/10.4337/9781845425678',
+        'name': "'\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catc...' on Platform 'Elgaronline'",
+        'suppressFromDiscovery': false,
+        'class': 'org.olf.kb.PlatformTitleInstance',
+        'longName': "'\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"' on Platform 'Elgaronline'"
+        },
+        'pkg': {
+        'id': '18e93b4a-bd70-4e84-8414-a0089f997989',
+        'dateCreated': '2021-09-08T02:06:40Z',
+        'lastUpdated': '2021-09-08T02:06:40Z',
+        'vendor': {
+            'id': 'a59a3daf-4c3f-40b7-8981-67bd3117af7c',
+            'name': 'Edward Elgar',
+            'orgsUuid_object': {
+            'error': 400,
+            'message': 'Bad Request'
+            }
+        },
+        'source': 'GOKb',
+        'remoteKb': {
+            'id': 'a1fef7a6-c3f4-4f85-a952-82d9b64469f2',
+            'cursor': '2021-09-06T09:01:11Z',
+            'active': true,
+            'trustedSourceTI': false,
+            'activationEnabled': false,
+            'readonly': false,
+            'syncStatus': 'idle',
+            'lastCheck': 1631080031125,
+            'name': 'GOKb_TEST',
+            'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+            'fullPrefix': 'gokb',
+            'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+            'supportsHarvesting': true,
+            'rectype': 1
+        },
+        'name': 'Edward Elgar:Edward Elgar E-Book Archive in Business & Management, Economics and Finance:Nationallizenz',
+        'suppressFromDiscovery': false,
+        'reference': 'Edward_Elgar:Edward_Elgar_E-Book_Archive_in_Business_&_Management,_Economics_and_Finance:Nationalliz',
+        'resourceCount': 2540,
+        'class': 'org.olf.kb.Pkg'
+        },
+        'addedTimestamp': 1631066800235,
+        'accessEnd': '2021-03-31',
+        'name': "'\"Institutions, industrial upgrading, and economic performance in Ja...' on Platform 'Elgaronline' in Package Edward Elgar:Edward Elgar E-Book Archive in Business & Management, ...",
+        'lastSeenTimestamp': 1631066800235,
+        'suppressFromDiscovery': false,
+        'longName': "'\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"' on Platform 'Elgaronline' in Package Edward Elgar:Edward Elgar E-Book Archive in Business & Management, Economics and Finance:Nationallizenz",
+        'class': 'org.olf.kb.PackageContentItem'
+    }
+    },
+    'activeFrom': '2021-09-01',
+    'poLines': [
+    {
+        'id': '556abc25-ebbf-3fb2-b478-1bfaff0af4dc',
+        'edition': 'First edition',
+        'checkinItems': false,
+        'agreementId': '09c6ed1b-3984-4d9a-8f9b-e1200b68b61c',
+        'acquisitionMethod': 'Purchase',
+        'alerts': '[]',
+        'cancellationRestriction': false,
+        'cancellationRestrictionNote': '',
+        'claims': [
+        {
+            'claimed': false,
+            'grace': 0
+        }
+        ],
+        'collection': false,
+        'contributors': '[]',
+        'cost': {
+        'listUnitPrice': 0,
+        'listUnitPriceElectronic': 0,
+        'currency': 'USD',
+        'additionalCost': 0,
+        'discount': 0,
+        'discountType': 'percentage',
+        'quantityPhysical': 2,
+        'quantityElectronic': 0,
+        'poLineEstimatedPrice': 0
+        },
+        'description': '',
+        'details': {
+        'receivingNote': '',
+        'productIds': [
+            {
+            'productId': '0747-0088',
+            'productIdType': '913300b2-03ed-469a-8179-c1092c991227'
+            }
+        ],
+        'subscriptionInterval': 0
+        },
+        'donor': '',
+        'fundDistribution': [
+        {
+            'code': 'UNIV-SUBN',
+            'encumbrance': 'eb506834-6c70-4239-8d1a-6414a5b08014',
+            'fundId': '4428a37c-8bae-4f0d-865d-970d83d5ad55',
+            'distributionType': 'percentage',
+            'value': 100
+        }
+        ],
+        'isPackage': false,
+        'locations': [
+        {
+            'locationId': '758258bc-ecc1-41b8-abca-f7b610822ffd',
+            'quantity': 2,
+            'quantityElectronic': 0,
+            'quantityPhysical': 2
+        }
+        ],
+        'orderFormat': 'Other',
+        'paymentStatus': 'Pending',
+        'physical': {
+        'materialType': 'dd0bf600-dbd9-44ab-9ff2-e2a61a6539f1',
+        'materialSupplier': 'e0fb5df2-cdf1-11e8-a8d5-f2801f1b9fd1',
+        'volumes': [
+            'vol. 1'
+        ]
+        },
+        'poLineDescription': '',
+        'poLineNumber': '81-1',
+        'publicationDate': '1915',
+        'publisher': 'American Bar Association',
+        'purchaseOrderId': 'c27e60f9-6361-44c1-976e-0c4821a33a7d',
+        'receiptStatus': 'Pending',
+        'reportingCodes': '[]',
+        'requester': '',
+        'rush': false,
+        'selector': '',
+        'source': 'User',
+        'tags': {
+        'tagList': [
+            'CatalogingRecords'
+        ]
+        },
+        'titleOrPackage': 'ABA Journal',
+        'vendorDetail': {
+        'instructions': '',
+        'noteFromVendor': '',
+        'vendorAccount': '',
+        'referenceNumbers': []
+        },
+        'metadata': {
+        'createdDate': '2021-09-08T01:53:13.925+00:00',
+        'updatedDate': '2021-09-08T01:53:13.925+00:00'
+        }
+    }
+    ],
+    'suppressFromDiscovery': true,
+    'note': 'agreement line note',
+    'description': 'agreement line description',
+    'coverage': [
+    {
+        'id': '2ea54077-5ed8-46c5-8e35-9b85428edf5e',
+        'startDate': '2021-09-01',
+        'endDate': '2021-09-30',
+        'startVolume': '09/04/2021',
+        'startIssue': '09/04/2021',
+        'endVolume': '09/28/2021',
+        'endIssue': '09/28/2021',
+        'summary': 'v09/04/2021/i09/04/2021/2021-09-01 - v09/28/2021/i09/28/2021/2021-09-30'
+    }
+    ],
+    'customCoverage': true,
+    'explanation': 'Agreement includes this item from a package specifically',
+    'startDate': '2021-09-01',
+    'endDate': '2021-09-30',
+    'contentUpdated': null,
+    'haveAccess': true
+};
+
+const resource = {
+    'id': 'abe6cfde-ee56-4491-9e53-b70acb9a984b',
+    'accessStart': '2008-01-01',
+    'dateCreated': '2021-09-08T02:06:41Z',
+    'tags': '[]',
+    'lastUpdated': '2021-09-08T02:07:07Z',
+    'depth': 'Fulltext',
+    'coverage': '[]',
+    'pti': {
+    'id': '27b9fe76-d1d2-45e8-b02c-474c32720d57',
+    'dateCreated': '2021-09-08T02:06:41Z',
+    'tags': [],
+    'lastUpdated': '2021-09-08T02:06:41Z',
+    'platform': {
+        'id': '7a45fab1-9816-48d0-a8dd-05b1b8361a99',
+        'dateCreated': '2021-09-08T02:01:19Z',
+        'lastUpdated': '2021-09-08T02:01:19Z',
+        'name': 'Elgaronline',
+        'locators': [
+        {
+            'id': '81fe228d-db09-4075-ad0f-e9aa6542bace',
+            'domainName': 'www.elgaronline.com'
+        }
+        ]
+    },
+    'templatedUrls': [
+        {
+        'id': '08c8bca4-31b7-47b4-8de1-febd0ab46b95',
+        'url': 'https://doi.org/10.4337/9781845425678',
+        'name': 'defaultUrl',
+        'resource': {
+            'id': '27b9fe76-d1d2-45e8-b02c-474c32720d57'
+        }
+        }
+    ],
+    'coverage': [],
+    'titleInstance': {
+        'id': '7b172272-a8df-4ba9-ae23-cd8a4552a686',
+        'subType': {
+        'id': '2c91809c7bc31559017bc31d3cde0015',
+        'value': 'electronic',
+        'label': 'Electronic'
+        },
+        'dateCreated': '2021-09-08T02:06:41Z',
+        'tags': '[]',
+        'lastUpdated': '2021-09-08T02:06:41Z',
+        'publicationType': {
+        'id': '2c91809c7bc31559017bc31d863e0046',
+        'value': 'book',
+        'label': 'Book'
+        },
+        'identifiers': [
+        {
+            'title': {
+            'id': '7b172272-a8df-4ba9-ae23-cd8a4552a686'
+            },
+            'status': {
+            'id': '2c91809c7bc31559017bc31d480d0045',
+            'value': 'approved',
+            'label': 'approved'
+            },
+            'identifier': {
+            'value': '9781845425678',
+            'ns': {
+                'value': 'isbn'
+            }
+            }
+        }
+        ],
+        'coverage': '[]',
+        'name': "\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"",
+        'type': {
+        'id': '2c91809c7bc31559017bc31d3ce50017',
+        'value': 'monograph',
+        'label': 'Monograph'
+        },
+        'suppressFromDiscovery': false,
+        'work': {
+        'id': 'd6914bfc-4706-4c31-84ad-389950469461'
+        },
+        'class': 'org.olf.kb.TitleInstance',
+        'longName': "\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"",
+        'relatedTitles': '[]'
+    },
+    'url': 'https://doi.org/10.4337/9781845425678',
+    'name': "'\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catc...' on Platform 'Elgaronline'",
+    'suppressFromDiscovery': false,
+    'class': 'org.olf.kb.PlatformTitleInstance',
+    'longName': "'\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"' on Platform 'Elgaronline'"
+    },
+    'pkg': {
+    'id': '18e93b4a-bd70-4e84-8414-a0089f997989',
+    'dateCreated': '2021-09-08T02:06:40Z',
+    'lastUpdated': '2021-09-08T02:06:40Z',
+    'vendor': {
+        'id': 'a59a3daf-4c3f-40b7-8981-67bd3117af7c',
+        'name': 'Edward Elgar',
+        'orgsUuid_object': {
+        'error': 400,
+        'message': 'Bad Request'
+        }
+    },
+    'source': 'GOKb',
+    'remoteKb': {
+        'id': 'a1fef7a6-c3f4-4f85-a952-82d9b64469f2',
+        'cursor': '2021-09-06T09:01:11Z',
+        'active': true,
+        'trustedSourceTI': false,
+        'activationEnabled': false,
+        'readonly': false,
+        'syncStatus': 'idle',
+        'lastCheck': 1631080031125,
+        'name': 'GOKb_TEST',
+        'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+        'fullPrefix': 'gokb',
+        'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+        'supportsHarvesting': true,
+        'rectype': 1
+    },
+    'name': 'Edward Elgar:Edward Elgar E-Book Archive in Business & Management, Economics and Finance:Nationallizenz',
+    'suppressFromDiscovery': false,
+    'reference': 'Edward_Elgar:Edward_Elgar_E-Book_Archive_in_Business_&_Management,_Economics_and_Finance:Nationalliz',
+    'resourceCount': 2540,
+    'class': 'org.olf.kb.Pkg'
+    },
+    'addedTimestamp': 1631066800235,
+    'accessEnd': '2021-03-31',
+    'name': "'\"Institutions, industrial upgrading, and economic performance in Ja...' on Platform 'Elgaronline' in Package Edward Elgar:Edward Elgar E-Book Archive in Business & Management, ...",
+    'lastSeenTimestamp': 1631066800235,
+    'suppressFromDiscovery': false,
+    'longName': "'\"Institutions, industrial upgrading, and economic performance in Japan: the 'flying-geese' paradigm of catch-up growth\"' on Platform 'Elgaronline' in Package Edward Elgar:Edward Elgar E-Book Archive in Business & Management, Economics and Finance:Nationallizenz",
+    'class': 'org.olf.kb.PackageContentItem'
+};
+
+export { line, resource };
