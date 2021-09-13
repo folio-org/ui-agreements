c02df3f2b7b376577394b4d771d36b9b2105c08a
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-13 23:07:43 +0100

    Added tests for coverage type default

--- src/components/AgreementLineSections/Coverage/Coverage.test.js
+++ src/components/AgreementLineSections/Coverage/Coverage.test.js
@@ -2,87 +2,149 @@ import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
 import { Accordion, MultiColumnList, MultiColumnListCell } from '@folio/stripes-testing';
-import { line, resource } from './testResources';
+import { line, resource, defaultLine, defaultResource } from './testResources';
 import translationsProperties from '../../../../test/helpers';
 import Coverage from './Coverage';
 
+
 describe('Coverage', () => {
-  beforeEach(() => {
-    renderWithIntl(
-      <Coverage line={line} resource={resource} />,
-      translationsProperties
-    );
-  });
+    describe('renders with coverage type custom ', () => {
+      beforeEach(() => {
+        renderWithIntl(
+          <Coverage line={line} resource={resource} />,
+          translationsProperties
+        );
+      });
 
-  test('renders Coverage Accordion', async () => {
-    await Accordion('Coverage').exists();
-  });
+      test('renders Coverage Accordion', async () => {
+        await Accordion('Coverage').exists();
+      });
 
-  test('renders expected column count', async () => {
-    await MultiColumnList({ columnCount: 7 }).exists();
-  });
+      test('renders expected column count', async () => {
+        await MultiColumnList({ columnCount: 7 }).exists();
+      });
 
-  test('renders expected columns', async () => {
-    await MultiColumnList({
-      columns: [
-        'Start date',
-        'Start volume',
-        'Start issue',
-        'End date',
-        'End volume',
-        'End issue',
-        'Coverage type',
-      ],
-    }).exists();
-  });
+      test('renders expected columns', async () => {
+        await MultiColumnList({
+          columns: [
+            'Start date',
+            'Start volume',
+            'Start issue',
+            'End date',
+            'End volume',
+            'End issue',
+            'Coverage type',
+          ],
+        }).exists();
+      });
 
-  test('renders expected coverage start date in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 0 }).has({ content: '9/1/2021' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 0 }).has({ content: '10/1/2021' })
-    ]);
-  });
+      test('renders expected coverage start date in the row', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 0 }).has({ content: '9/1/2021' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 0 }).has({ content: '10/1/2021' })
+        ]);
+      });
 
-  test('renders expected coverage start volume in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 1 }).has({ content: '09/04/2021' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 1 }).has({ content: '10/04/2021' })
-    ]);
-  });
+      test('renders expected coverage start volume in the row', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 1 }).has({ content: '1' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 1 }).has({ content: '1' })
+        ]);
+      });
 
-  test('renders expected coverage start issue in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 2 }).has({ content: '09/04/2021' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 2 }).has({ content: '10/04/2021' })
-    ]);
-  });
+      test('renders expected coverage start issue in the row', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 2 }).has({ content: '1' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 2 }).has({ content: '1' })
+        ]);
+      });
 
-  test('renders expected coverage end date in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 3 }).has({ content: '9/30/2021' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 3 }).has({ content: '10/30/2021' })
-    ]);
-  });
+      test('renders expected coverage end date in the row', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 3 }).has({ content: '9/30/2021' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 3 }).has({ content: '10/30/2021' })
+        ]);
+      });
 
-  test('renders expected coverage end volume in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 4 }).has({ content: '09/28/2021' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 4 }).has({ content: '10/28/2021' })
-    ]);
-  });
+      test('renders expected coverage end volume in the row', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 4 }).has({ content: '8' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 4 }).has({ content: '8' })
+        ]);
+      });
 
-  test('renders expected coverage end issue in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 5 }).has({ content: '09/28/2021' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 5 }).has({ content: '10/28/2021' })
-    ]);
-  });
+      test('renders expected coverage end issue in the row', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 5 }).has({ content: '12' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 5 }).has({ content: '12' })
+        ]);
+      });
 
-  test('renders expected coverage type in the row', async () => {
-    Promise.all([
-      await MultiColumnListCell({ row: 0, columnIndex: 6 }).has({ content: 'Custom ' }),
-      await MultiColumnListCell({ row: 1, columnIndex: 6 }).has({ content: 'Custom ' })
-    ]);
+      test('renders expected coverage type in the row ', async () => {
+        Promise.all([
+          await MultiColumnListCell({ row: 0, columnIndex: 6 }).has({ content: 'Custom CustomCoverageIcon' }),
+          await MultiColumnListCell({ row: 1, columnIndex: 6 }).has({ content: 'Custom CustomCoverageIcon' })
+        ]);
+      });
+    });
+
+    describe('renders with coverage type default', () => {
+    beforeEach(() => {
+      renderWithIntl(
+        <Coverage line={defaultLine} resource={defaultResource} />,
+        translationsProperties
+      );
+    });
+
+    test('renders Coverage Accordion', async () => {
+      await Accordion('Coverage').exists();
+    });
+
+    test('renders expected column count', async () => {
+      await MultiColumnList({ columnCount: 7 }).exists();
+    });
+
+    test('renders expected columns', async () => {
+      await MultiColumnList({
+        columns: [
+          'Start date',
+          'Start volume',
+          'Start issue',
+          'End date',
+          'End volume',
+          'End issue',
+          'Coverage type',
+        ],
+      }).exists();
+    });
+
+    test('renders expected coverage start date in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 0 }).has({ content: '11/1/2003' });
+      });
+
+    test('renders expected coverage start volume in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 1 }).has({ content: '5' });
+    });
+
+    test('renders expected coverage start issue in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 2 }).has({ content: 'Nov' });
+    });
+
+    test('renders expected coverage end date in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 3 }).has({ content: '3/15/2013' });
+    });
+
+    test('renders expected coverage end volume in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 4 }).has({ content: '9' });
+    });
+
+    test('renders expected coverage end issue in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 5 }).has({ content: '1-4' });
+    });
+
+    test('renders expected coverage type in the row', async () => {
+        await MultiColumnListCell({ row: 0, columnIndex: 6 }).has({ content: 'Default' });
+    });
   });
 });
 
--- src/components/AgreementLineSections/Coverage/testResources.js
+++ src/components/AgreementLineSections/Coverage/testResources.js
@@ -212,20 +212,20 @@ const line = {
       'id': '7e716bb9-71ef-4ac3-bc53-e30b0f2fa1e6',
       'startDate': '2021-09-01',
       'endDate': '2021-09-30',
-      'startVolume': '09/04/2021',
-      'startIssue': '09/04/2021',
-      'endVolume': '09/28/2021',
-      'endIssue': '09/28/2021',
+      'startVolume': '1',
+      'startIssue': '1',
+      'endVolume': '8',
+      'endIssue': '12',
       'summary': 'v09/04/2021/i09/04/2021/2021-09-01 - v09/28/2021/i09/28/2021/2021-09-30'
     },
     {
       'id': 'dcbea5e1-3828-4073-92f2-79c18d703972',
       'startDate': '2021-10-01',
       'endDate': '2021-10-30',
-      'startVolume': '10/04/2021',
-      'startIssue': '10/04/2021',
-      'endVolume': '10/28/2021',
-      'endIssue': '10/28/2021',
+      'startVolume': '1',
+      'startIssue': '1',
+      'endVolume': '8',
+      'endIssue': '12',
       'summary': 'v10/04/2021/i10/04/2021/2021-10-01 - v10/28/2021/i10/28/2021/2021-10-30'
     }
   ],
@@ -365,7 +365,424 @@ const resource = {
   'class': 'org.olf.kb.PackageContentItem'
 };
 
+const defaultLine = {
+  'id': 'f3db0f83-bf7b-4a4b-b43a-673a323bf387',
+  'dateCreated': '2021-09-13T21:13:14Z',
+  'activeTo': '2021-09-30',
+  'tags': '[]',
+  'lastUpdated': '2021-09-13T21:17:43Z',
+  'owner': {
+    'id': '5d08dcee-d801-41e1-bd8d-0dd49de04119',
+    'cancellationDeadline': '2021-09-28',
+    'dateCreated': '2021-09-13T08:28:15Z',
+    'isPerpetual': {
+      'id': '2c91809c7bdd85f6017bdd8e0b180001',
+      'value': 'yes',
+      'label': 'Yes'
+    },
+    'name': 'MR agr test',
+    'orgs': [],
+    'externalLicenseDocs': [],
+    'outwardRelationships': [],
+    'customProperties': {},
+    'contacts': [],
+    'tags': [],
+    'lastUpdated': '2021-09-13T21:17:43Z',
+    'inwardRelationships': [],
+    'renewalPriority': {
+      'id': '2c91809c7bdd85f6017bdd8e0b53000a',
+      'value': 'definitely_cancel',
+      'label': 'Definitely cancel'
+    },
+    'endDate': '2021-09-30',
+    'startDate': '2021-09-01',
+    'linkedLicenses': [],
+    'docs': [],
+    'periods': [{
+      'id': 'c61a982c-5ff8-417d-86fd-71aabb80b66e',
+      'startDate': '2021-09-01',
+      'cancellationDeadline': '2021-09-28',
+      'owner': {
+        'id': '5d08dcee-d801-41e1-bd8d-0dd49de04119'
+      },
+      'note': 'This is note',
+      'endDate': '2021-09-30',
+      'periodStatus': 'current'
+    }],
+    'usageDataProviders': [],
+    'agreementStatus': {
+      'id': '2c91809c7bdd85f6017bdd8e0ba30011',
+      'value': 'active',
+      'label': 'Active'
+    },
+    'supplementaryDocs': [],
+    'description': 'This is description',
+    'items': [{
+        'id': '49808d2c-6576-448d-9f1d-9321f357c03d'
+      },
+      {
+        'id': 'c1ca6ea3-9c65-46b6-bf35-ef00d420536d'
+      },
+      {
+        'id': 'f3db0f83-bf7b-4a4b-b43a-673a323bf387'
+      },
+      {
+        'id': '736212aa-a81b-4829-acf7-cbaa586bc6de'
+      },
+      {
+        'id': '42a71f30-0561-44dc-9f03-b7199de1795e'
+      },
+      {
+        'id': 'd04ae7c1-8604-4808-b8f4-13531e319a26'
+      }
+    ],
+    'alternateNames': []
+  },
+  'resource': {
+    'id': '551e27a7-ced3-4d28-b7d2-de94d295c1d8',
+    'class': 'org.olf.kb.PackageContentItem',
+    'name': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online' in Package De Gruyter : Journal Archive Package Biology, Chemistry, Geoscience...",
+    'suppressFromDiscovery': false,
+    'tags': [],
+    'coverage': [{
+      'id': '02470753-40d3-4012-ae4a-3162dbcc9dad',
+      'startDate': '2003-11-01',
+      'endDate': '2013-03-15',
+      'startVolume': '5',
+      'startIssue': 'Nov',
+      'endVolume': '9',
+      'endIssue': '1-4',
+      'summary': 'v5/iNov/2003-11-01 - v9/i1-4/2013-03-15'
+    }],
+    'customCoverage': false,
+    '_object': {
+      'id': '551e27a7-ced3-4d28-b7d2-de94d295c1d8',
+      'dateCreated': '2021-09-13T05:19:34Z',
+      'tags': '[]',
+      'lastUpdated': '2021-09-13T05:19:34Z',
+      'depth': 'Fulltext',
+      'coverage': [{
+        'id': '02470753-40d3-4012-ae4a-3162dbcc9dad',
+        'startDate': '2003-11-01',
+        'endDate': '2013-03-15',
+        'startVolume': '5',
+        'startIssue': 'Nov',
+        'endVolume': '9',
+        'endIssue': '1-4',
+        'summary': 'v5/iNov/2003-11-01 - v9/i1-4/2013-03-15'
+      }],
+      'pti': {
+        'id': '210f59b3-bc1d-499a-bd99-4373507e5ea6',
+        'dateCreated': '2021-09-13T05:19:34Z',
+        'tags': [],
+        'lastUpdated': '2021-09-13T05:19:49Z',
+        'platform': {
+          'id': '7645e786-3c51-46de-b1fd-ce13a4850480',
+          'dateCreated': '2021-09-13T05:13:21Z',
+          'lastUpdated': '2021-09-13T05:13:21Z',
+          'name': 'DeGruyter Online',
+          'locators': '[{…}]'
+        },
+        'templatedUrls': [
+          '{id: "cb88bbdb-ae00-4c2f-9fe0-10fdc675af5f", name: …}'
+        ],
+        'coverage': [{
+          'id': '0d09e16c-f653-40fc-95a5-1cb7cd608ad1',
+          'startDate': '2003-11-01',
+          'endDate': '2013-03-15',
+          'summary': 'v*/i*/2003-11-01 - v*/i*/2013-03-15'
+        }],
+        'titleInstance': {
+          'id': 'bdf174a9-2516-4bac-bb98-507c63134460',
+          'subType': '{id: "2c91809c7bdd85f6017bdd8e0c690035", label: "El…}',
+          'dateCreated': '2021-09-13T05:19:34Z',
+          'tags': '[]',
+          'lastUpdated': '2021-09-13T05:19:49Z',
+          'publicationType': '{id: "2c91809c7bdd85f6017bdd8e15e70043", label: "Jo…}',
+          'identifiers': '[{…}, {…}, {…}]',
+          'coverage': '[{…}]',
+          'name': 'BioInorganic reaction mechanisms',
+          'type': '{id: "2c91809c7bdd85f6017bdd8e0c750038", label: "Se…}',
+          'suppressFromDiscovery': false,
+          'work': '{id: "047774d5-9c69-4f1e-bc2d-1ab37d26dd85"}',
+          'class': 'org.olf.kb.TitleInstance',
+          'longName': 'BioInorganic reaction mechanisms',
+          'relatedTitles': '[{…}]'
+        },
+        'url': 'https://www.degruyter.com/openurl?genre=journal&issn=2191-2491',
+        'name': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online'",
+        'suppressFromDiscovery': false,
+        'class': 'org.olf.kb.PlatformTitleInstance',
+        'longName': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online'"
+      },
+      'pkg': {
+        'id': 'db228eda-0ed8-4097-8d22-23d1e4f91ef6',
+        'dateCreated': '2021-09-13T05:19:32Z',
+        'lastUpdated': '2021-09-13T05:19:32Z',
+        'vendor': {
+          'id': 'd93e0df1-516a-4334-aec9-5c7373528af1',
+          'name': 'De Gruyter',
+          'orgsUuid_object': {
+            'error': 400,
+            'message': 'Bad Request'
+          }
+        },
+        'source': 'GOKb',
+        'remoteKb': {
+          'id': '6d3f5f16-a6df-463d-a087-ac90890ec858',
+          'cursor': '2021-09-13T04:00:25Z',
+          'active': true,
+          'trustedSourceTI': true,
+          'activationEnabled': false,
+          'readonly': false,
+          'syncStatus': 'idle',
+          'lastCheck': 1631566818577,
+          'name': 'GOKb_TEST',
+          'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+          'fullPrefix': 'gokb',
+          'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+          'supportsHarvesting': true,
+          'rectype': 1
+        },
+        'name': 'De Gruyter : Journal Archive Package Biology, Chemistry, Geosciences : 2019-07-08',
+        'suppressFromDiscovery': false,
+        'reference': 'De_Gruyter_:_Journal_Archive_Package_Biology,_Chemistry,_Geosciences_:_2019-07-08',
+        'resourceCount': 21,
+        'class': 'org.olf.kb.Pkg'
+      },
+      'addedTimestamp': 1631510372810,
+      'name': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online' in Package De Gruyter : Journal Archive Package Biology, Chemistry, Geoscience...",
+      'lastSeenTimestamp': 1631510372810,
+      'suppressFromDiscovery': false,
+      'longName': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online' in Package De Gruyter : Journal Archive Package Biology, Chemistry, Geosciences : 2019-07-08",
+      'class': 'org.olf.kb.PackageContentItem'
+    }
+  },
+  'activeFrom': '2021-09-01',
+  'poLines': '[]',
+  'suppressFromDiscovery': true,
+  'note': 'This is note',
+  'description': 'This is description',
+  'coverage': [{
+    'id': '02470753-40d3-4012-ae4a-3162dbcc9dad',
+    'startDate': '2003-11-01',
+    'endDate': '2013-03-15',
+    'startVolume': '5',
+    'startIssue': 'Nov',
+    'endVolume': '9',
+    'endIssue': '1-4',
+    'summary': 'v5/iNov/2003-11-01 - v9/i1-4/2013-03-15'
+  }],
+  'customCoverage': false,
+  'explanation': 'Agreement includes this item from a package specifically',
+  'startDate': '2021-09-01',
+  'endDate': '2021-09-30',
+  'contentUpdated': null,
+  'haveAccess': true
+};
+
+const defaultResource = {
+  'id': '551e27a7-ced3-4d28-b7d2-de94d295c1d8',
+  'dateCreated': '2021-09-13T05:19:34Z',
+  'tags': '[]',
+  'lastUpdated': '2021-09-13T05:19:34Z',
+  'depth': 'Fulltext',
+  'coverage': [{
+    'id': '02470753-40d3-4012-ae4a-3162dbcc9dad',
+    'startDate': '2003-11-01',
+    'endDate': '2013-03-15',
+    'startVolume': '5',
+    'startIssue': 'Nov',
+    'endVolume': '9',
+    'endIssue': '1-4',
+    'summary': 'v5/iNov/2003-11-01 - v9/i1-4/2013-03-15'
+  }],
+  'pti': {
+    'id': '210f59b3-bc1d-499a-bd99-4373507e5ea6',
+    'dateCreated': '2021-09-13T05:19:34Z',
+    'tags': [],
+    'lastUpdated': '2021-09-13T05:19:49Z',
+    'platform': {
+      'id': '7645e786-3c51-46de-b1fd-ce13a4850480',
+      'dateCreated': '2021-09-13T05:13:21Z',
+      'lastUpdated': '2021-09-13T05:13:21Z',
+      'name': 'DeGruyter Online',
+      'locators': [{
+        'id': 'f3f4965c-6d40-4072-87b4-df89d14d2fb4',
+        'domainName': 'www.degruyter.com'
+      }]
+    },
+    'templatedUrls': [{
+      'id': 'cb88bbdb-ae00-4c2f-9fe0-10fdc675af5f',
+      'url': 'https://www.degruyter.com/openurl?genre=journal&issn=2191-2491',
+      'name': 'defaultUrl',
+      'resource': {
+        'id': '210f59b3-bc1d-499a-bd99-4373507e5ea6'
+      }
+    }],
+    'coverage': [
+      '{endDate: "2013-03-15", id: "0d09e16c-f653-40fc-95a…}'
+    ],
+    'titleInstance': {
+      'id': 'bdf174a9-2516-4bac-bb98-507c63134460',
+      'subType': {
+        'id': '2c91809c7bdd85f6017bdd8e0c690035',
+        'value': 'electronic',
+        'label': 'Electronic'
+      },
+      'dateCreated': '2021-09-13T05:19:34Z',
+      'tags': '[]',
+      'lastUpdated': '2021-09-13T05:19:49Z',
+      'publicationType': {
+        'id': '2c91809c7bdd85f6017bdd8e15e70043',
+        'value': 'journal',
+        'label': 'Journal'
+      },
+      'identifiers': [{
+          'title': {
+            'id': 'bdf174a9-2516-4bac-bb98-507c63134460'
+          },
+          'status': {
+            'id': '2c91809c7bdd85f6017bdd8e16350045',
+            'value': 'approved',
+            'label': 'approved'
+          },
+          'identifier': {
+            'value': '2598375-1',
+            'ns': {
+              'value': 'zdb'
+            }
+          }
+        },
+        {
+          'title': {
+            'id': 'bdf174a9-2516-4bac-bb98-507c63134460'
+          },
+          'status': {
+            'id': '2c91809c7bdd85f6017bdd8e16350045',
+            'value': 'approved',
+            'label': 'approved'
+          },
+          'identifier': {
+            'value': '151865',
+            'ns': {
+              'value': 'ezb'
+            }
+          }
+        },
+        {
+          'title': {
+            'id': 'bdf174a9-2516-4bac-bb98-507c63134460'
+          },
+          'status': {
+            'id': '2c91809c7bdd85f6017bdd8e16350045',
+            'value': 'approved',
+            'label': 'approved'
+          },
+          'identifier': {
+            'value': '2191-2491',
+            'ns': {
+              'value': 'issn'
+            }
+          }
+        }
+      ],
+      'coverage': [{
+        'id': '93a4058f-bc54-4d94-967c-7f4bb1738aae',
+        'startDate': '2003-11-01',
+        'endDate': '2013-03-15',
+        'summary': 'v*/i*/2003-11-01 - v*/i*/2013-03-15'
+      }],
+      'name': 'BioInorganic reaction mechanisms',
+      'type': {
+        'id': '2c91809c7bdd85f6017bdd8e0c750038',
+        'value': 'serial',
+        'label': 'Serial'
+      },
+      'suppressFromDiscovery': false,
+      'work': {
+        'id': '047774d5-9c69-4f1e-bc2d-1ab37d26dd85'
+      },
+      'class': 'org.olf.kb.TitleInstance',
+      'longName': 'BioInorganic reaction mechanisms',
+      'relatedTitles': [{
+        'id': 'c64f556c-68b1-4b28-a1c6-e66236e0d3b4',
+        'subType': {
+          'id': '2c91809c7bdd85f6017bdd8e0c630034',
+          'value': 'print',
+          'label': 'Print'
+        },
+        'publicationType': {
+          'id': '2c91809c7bdd85f6017bdd8e15e70043',
+          'value': 'journal',
+          'label': 'Journal'
+        },
+        'identifiers': [{
+          'title': '{id: "c64f556c-68b1-4b28-a1c6-e66236e0d3b4"}',
+          'status': '{id: "2c91809c7bdd85f6017bdd8e16350045", label: "ap…}',
+          'identifier': '{ns: {…}, value: "2191-2483"}'
+        }],
+        'name': 'BioInorganic reaction mechanisms',
+        'type': {
+          'id': '2c91809c7bdd85f6017bdd8e0c750038',
+          'value': 'serial',
+          'label': 'Serial'
+        },
+        'longName': 'BioInorganic reaction mechanisms'
+      }]
+    },
+    'url': 'https://www.degruyter.com/openurl?genre=journal&issn=2191-2491',
+    'name': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online'",
+    'suppressFromDiscovery': false,
+    'class': 'org.olf.kb.PlatformTitleInstance',
+    'longName': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online'"
+  },
+  'pkg': {
+    'id': 'db228eda-0ed8-4097-8d22-23d1e4f91ef6',
+    'dateCreated': '2021-09-13T05:19:32Z',
+    'lastUpdated': '2021-09-13T05:19:32Z',
+    'vendor': {
+      'id': 'd93e0df1-516a-4334-aec9-5c7373528af1',
+      'name': 'De Gruyter',
+      'orgsUuid_object': {
+        'error': 400,
+        'message': 'Bad Request'
+      }
+    },
+    'source': 'GOKb',
+    'remoteKb': {
+      'id': '6d3f5f16-a6df-463d-a087-ac90890ec858',
+      'cursor': '2021-09-13T04:00:25Z',
+      'active': true,
+      'trustedSourceTI': true,
+      'activationEnabled': false,
+      'readonly': false,
+      'syncStatus': 'idle',
+      'lastCheck': 1631566818577,
+      'name': 'GOKb_TEST',
+      'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+      'fullPrefix': 'gokb',
+      'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+      'supportsHarvesting': true,
+      'rectype': 1
+    },
+    'name': 'De Gruyter : Journal Archive Package Biology, Chemistry, Geosciences : 2019-07-08',
+    'suppressFromDiscovery': false,
+    'reference': 'De_Gruyter_:_Journal_Archive_Package_Biology,_Chemistry,_Geosciences_:_2019-07-08',
+    'resourceCount': 21,
+    'class': 'org.olf.kb.Pkg'
+  },
+  'addedTimestamp': 1631510372810,
+  'name': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online' in Package De Gruyter : Journal Archive Package Biology, Chemistry, Geoscience...",
+  'lastSeenTimestamp': 1631510372810,
+  'suppressFromDiscovery': false,
+  'longName': "'BioInorganic reaction mechanisms' on Platform 'DeGruyter Online' in Package De Gruyter : Journal Archive Package Biology, Chemistry, Geosciences : 2019-07-08",
+  'class': 'org.olf.kb.PackageContentItem'
+};
+
 export {
   line,
-  resource
+  resource,
+  defaultLine,
+  defaultResource
 };
