04b042cafc3a3fc375197b63efcfb2b740c7895e
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-13 15:38:33 +0100

    Added test for BasketSelector component

--- src/components/AgreementLineSections/FormEresource/FormEresource.test.js
+++ src/components/AgreementLineSections/FormEresource/FormEresource.test.js
@@ -5,9 +5,7 @@ import { Button } from '@folio/stripes-testing';
 import translationsProperties from '../../../../test/helpers';
 import {
   eholdingsData,
-  eholdingsValues,
   basketData,
-  basketValues,
   basketSelectordata,
   handlers,
   initialValues
@@ -31,18 +29,17 @@ describe('FormEresource', () => {
         translationsProperties
       );
 
-      test('renders the FormEresourceCard component', () => {
-        const { getByText } = renderComponent;
-        expect(getByText('FormEresourceCard')).toBeInTheDocument();
+      test('renders the submit button', async () => {
+        await Button('Submit').exists();
       });
     });
   });
 
-  describe('with no initialValues', () => {
+  describe('renders agreementLineSource type eholdings', () => {
     beforeEach(() => {
       renderComponent = renderWithIntl(
         <TestForm onSubmit={onSubmit}>
-          <FormEresource />
+          <FormEresource basket={eholdingsData.basket} line={eholdingsData.line} resourec={eholdingsData.resource} values={eholdingsData.values} />
         </TestForm>,
         translationsProperties
       );
@@ -53,61 +50,26 @@ describe('FormEresource', () => {
       expect(getByRole('button', { name: 'Submit' })).toBeInTheDocument();
     });
 
-    test('renders the submit button', async () => {
-      await Button('Submit').exists();
-    });
-
     test('renders the EresourceSelector component', () => {
       const { getByText } = renderComponent;
       expect(getByText('EresourceSelector')).toBeInTheDocument();
     });
   });
 
-  describe('with agreementLine type basket', () => {
-    beforeEach(() => {
-      renderComponent = renderWithIntl(
-        <TestForm onSubmit={onSubmit}>
-          <FormEresource basket={basketData.basket} line={basketData.line} resourec={basketData.resource} values={basketValues} />
-        </TestForm>,
-        translationsProperties
-      );
 
-      test('renders the FormEresourceCard component', () => {
-        const { getByText } = renderComponent;
-        expect(getByText('FormEresourceCard')).toBeInTheDocument();
-      });
-    });
-  });
+  // describe('renders agreementLineSource with empty basket', () => {
+  //   beforeEach(() => {
+  //     renderComponent = renderWithIntl(
+  //       <TestForm onSubmit={onSubmit}>
+  //         <FormEresource basket={basketSelectordata.basket} line={basketSelectordata.line} resource={basketData.resource} values={basketSelectordata.values} />
+  //       </TestForm>,
+  //       translationsProperties
+  //     );
 
-  describe('with agreementLine type eholdings', () => {
-    beforeEach(() => {
-      renderComponent = renderWithIntl(
-        <TestForm onSubmit={onSubmit}>
-          <FormEresource basket={eholdingsData.basket} line={eholdingsData.line} resource={eholdingsData.resource} values={eholdingsValues} />
-        </TestForm>,
-        translationsProperties
-      );
-
-      test('renders the FormEresourceCard component', () => {
-        const { getByText } = renderComponent;
-        expect(getByText('FormEresourceCard')).toBeInTheDocument();
-      });
-    });
-  });
-
-  describe('with agreementLine empty basket', () => {
-    beforeEach(() => {
-      renderComponent = renderWithIntl(
-        <TestForm onSubmit={onSubmit}>
-          <FormEresource basket={basketSelectordata.basket} line={basketSelectordata.line} resource={basketData.resource} values={basketSelectordata.values} />
-        </TestForm>,
-        translationsProperties
-      );
-
-      test('renders the BasketSelector component', () => {
-        const { getByText } = renderComponent;
-        expect(getByText('BasketSelector')).toBeInTheDocument();
-      });
-    });
-  });
+  //     test('renders the BasketSelector component', () => {
+  //       const { getByText } = renderComponent;
+  //       expect(getByText('BasketSelector')).toBeInTheDocument();
+  //     });
+  //   });
+  // });
 });
