7c5e8dd0b1244560b291edde54fa6deee967ec1a
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-15 14:06:54 +0100

    Added test for error message and linkEresourceToStart message

--- src/components/AgreementLineSections/EresourceSelector/EresourceSelector.test.js
+++ src/components/AgreementLineSections/EresourceSelector/EresourceSelector.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { FormattedMessage } from 'react-intl';
 import translationsProperties from '../../../../test/helpers';
 import EresourceSelector from './EresourceSelector';
 
-
 const line = {
   'component': 'ƒ EresourceSelector() {}',
-  'error': false,
+  'error': '<Memo />',
   'name': 'linkedResource',
   'onAdd': () => {},
   'value': ''
@@ -17,7 +17,7 @@ describe('EresourceSelector', () => {
   let renderComponent;
   beforeEach(() => {
     renderComponent = renderWithIntl(
-      <EresourceSelector line={line} />,
+      <EresourceSelector error={<FormattedMessage id="ui-agreements.agreementLine.provideEresource" />} line={line} />,
       translationsProperties
     );
   });
@@ -25,6 +25,16 @@ describe('EresourceSelector', () => {
   test('renders eholdings card component', () => {
     const { getByText } = renderComponent;
     expect(getByText('eHoldings')).toBeInTheDocument();
+   });
+
+   test('renders linkEresourceToStart message', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Link an e-resource to get started')).toBeInTheDocument();
+   });
+
+   test('renders error message', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Please provide an e-resource or description to continue')).toBeInTheDocument();
    });
 });
 
