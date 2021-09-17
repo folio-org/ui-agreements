39bde0605da010fb58e3074579e6a9ab8feef037
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-17 14:31:46 +0100

    Added another description block for the error message

--- src/components/AgreementLineSections/EresourceSelector/EresourceSelector.test.js
+++ src/components/AgreementLineSections/EresourceSelector/EresourceSelector.test.js
@@ -6,7 +6,7 @@ import translationsProperties from '../../../../test/helpers';
 import EresourceSelector from './EresourceSelector';
 
 const line = {
-  'component': 'ƒ EresourceSelector() {}',
+  'component': () => {},
   'error': '<Memo />',
   'name': 'linkedResource',
   'onAdd': () => {},
@@ -14,27 +14,43 @@ const line = {
 };
 
 describe('EresourceSelector', () => {
-  let renderComponent;
-  beforeEach(() => {
-    renderComponent = renderWithIntl(
-      <EresourceSelector error={<FormattedMessage id="ui-agreements.agreementLine.provideEresource" />} line={line} />,
-      translationsProperties
-    );
+  describe('renders EresourceSelector component', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <EresourceSelector line={line} />,
+        translationsProperties
+      );
+    });
+
+    test('renders eholdings card component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('eHoldings')).toBeInTheDocument();
+    });
+
+    test('renders linkEresourceToStart message', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Link an e-resource to get started')).toBeInTheDocument();
+    });
+
+    test('renders noEresourceLinked message', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('No e-resource added')).toBeInTheDocument();
+    });
   });
 
-  test('renders eholdings card component', () => {
-    const { getByText } = renderComponent;
-    expect(getByText('eHoldings')).toBeInTheDocument();
-   });
+  describe('renders EresourceSelector with error message', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <EresourceSelector error={<FormattedMessage id="ui-agreements.agreementLine.provideEresource" />} />,
+        translationsProperties
+      );
+    });
 
-   test('renders linkEresourceToStart message', () => {
-    const { getByText } = renderComponent;
-    expect(getByText('Link an e-resource to get started')).toBeInTheDocument();
-   });
-
-   test('renders error message', () => {
-    const { getByText } = renderComponent;
-    expect(getByText('Please provide an e-resource or description to continue')).toBeInTheDocument();
-   });
+    test('renders error message', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Please provide an e-resource or description to continue')).toBeInTheDocument();
+    });
+  });
 });
-
