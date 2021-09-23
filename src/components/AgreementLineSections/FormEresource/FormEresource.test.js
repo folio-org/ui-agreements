f3015bcae12674ad99ff5b7cc01180dd40656fa7
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-23 09:38:57 +0100

    Added tests for empty eresource

--- src/components/AgreementLineSections/FormEresource/FormEresource.test.js
+++ src/components/AgreementLineSections/FormEresource/FormEresource.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
+import { render } from '@testing-library/react';
 import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
 import { Button } from '@folio/stripes-testing';
 import translationsProperties from '../../../../test/helpers';
-import { initialValues, handlers, data, data2 } from './testResources';
+import { initialValues, handlers, data, data2, emptyData } from './testResources';
 import FormEresource from './FormEresource';
 
 jest.mock('../EresourceSelector', () => () => <div>EresourceSelector</div>);
@@ -38,7 +39,7 @@ describe('FormEresource', () => {
     beforeEach(() => {
       renderComponent = renderWithIntl(
         <TestForm onSubmit={onSubmit}>
-          <FormEresource agreementLineSource={data.agreementLineSource} line={data.line} lineId={data.lineId} setFieldData={data.setFieldData} values={data.values} />
+          <FormEresource agreementLineSource={data.agreementLineSource} basket={data.basket} line={data.line} values={data.values} />
         </TestForm>,
         translationsProperties
       );
@@ -59,15 +60,31 @@ describe('FormEresource', () => {
     beforeEach(() => {
       renderComponent = renderWithIntl(
         <TestForm onSubmit={onSubmit}>
-          <FormEresource agreementLineSource={data2.agreementLineSource} line={data2.line} lineId={data2.lineId} setFieldData={data2.setFieldData} values={data2.values} />
+          <FormEresource agreementLineSource={data2.agreementLineSource} basket={data2.basket} line={data2.line} values={data2.values} />
         </TestForm>,
         translationsProperties
       );
     });
 
-    test('renders the EresourceSelector component', () => {
+    test('renders the BasketSelector component', () => {
       const { getByText } = renderComponent;
-      expect(getByText('EresourceSelector')).toBeInTheDocument();
+      expect(getByText('BasketSelector')).toBeInTheDocument();
+    });
+  });
+
+  describe('renders agreementLineSource with no data', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <TestForm onSubmit={onSubmit}>
+          <FormEresource basket={emptyData.basket} line={emptyData.line} values={emptyData.values} />
+        </TestForm>,
+        translationsProperties
+      );
+    });
+
+    test('renders null', () => {
+      const { queryByText } = renderComponent;
+      expect(queryByText('FormEresource')).toBeNull();
     });
   });
 });
