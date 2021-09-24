38c03349f289a8ba6d3e8539a35d0487ef06482d
Monireh <monireh.rasouli@k-int.co.uk>
2021-09-24 13:30:12 +0100

    updated props, renamed items to agreementLines, added test for empty fireld message and  IfEResourceEnabled component

--- src/components/AgreementLinesFieldArray/AgreementLinesFieldArray.test.js
+++ src/components/AgreementLinesFieldArray/AgreementLinesFieldArray.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
 import { FieldArray } from 'react-final-form-arrays';
+import userEvent from '@testing-library/user-event';
 import AgreementLinesFieldArray from './AgreementLinesFieldArray';
 
 import translationsProperties from '../../../test/helpers';
@@ -13,51 +14,181 @@ const onSubmit = jest.fn();
 
 const data = {
   'agreementLines': [{
-    'id': 'd97db008-c00e-442d-9aca-e58f2f25486e',
-    'activeTo': '2021-09-30',
-    'tags': '[]',
-    'owner': '{agreementStatus: {…}, alternateNames: Array(0), ca…}',
-    'resource': '{_object: {…}, class: "org.olf.kb.PackageContentIte…}',
-    'activeFrom': '2021-09-01',
-    'poLines': '[]',
-    'suppressFromDiscovery': false,
-    'note': 'note',
-    'customCoverage': false,
-    'explanation': 'Agreement includes this item from a package specifically',
-    'startDate': '2021-09-01',
-    'endDate': '2021-09-30',
-    'contentUpdated': null,
-    'haveAccess': true
+      'id': '648f362b-9a71-4eb6-8fe8-9ed20dffb0dc',
+      'dateCreated': '2021-09-23T13:13:43Z',
+      'activeTo': '2021-09-04',
+      'tags': '[]',
+      'lastUpdated': '2021-09-23T13:13:43Z',
+      'owner': {
+        'id': 'af691ea8-4f2a-41f1-907c-1674e0377d90',
+        'cancellationDeadline': '2021-09-28',
+        'dateCreated': '2021-09-23T08:02:15Z',
+        'isPerpetual': {
+          'id': '2c91809c7c1054b5017c105cc2120031',
+          'value': 'yes',
+          'label': 'Yes'
+        },
+        'name': 'MR agr test',
+        'orgs': [],
+        'externalLicenseDocs': [],
+        'outwardRelationships': [],
+        'customProperties': {},
+        'contacts': [],
+        'tags': [],
+        'lastUpdated': '2021-09-23T13:13:43Z',
+        'inwardRelationships': [],
+        'renewalPriority': {
+          'id': '2c91809c7c1054b5017c105cc2380039',
+          'value': 'for_review',
+          'label': 'For review'
+        },
+        'endDate': '2021-09-30',
+        'startDate': '2021-09-01',
+        'linkedLicenses': [],
+        'docs': [],
+        'periods': [
+          '{cancellationDeadline: "2021-09-28", endDate: "2021…}'
+        ],
+        'usageDataProviders': [],
+        'agreementStatus': {
+          'id': '2c91809c7c1054b5017c105cc27f0041',
+          'value': 'active',
+          'label': 'Active'
+        },
+        'supplementaryDocs': [],
+        'description': 'This is description',
+        'items': [
+          '{id: "9e46275f-2b11-4235-963e-a63ee04b4241"}',
+          '{id: "648f362b-9a71-4eb6-8fe8-9ed20dffb0dc"}'
+        ],
+        'alternateNames': []
+      },
+      'resource': {
+        'id': 'fe84a421-ab24-4fd0-8a98-b65664ec0126',
+        'class': 'org.olf.kb.Pkg',
+        'name': 'Brill Test',
+        'suppressFromDiscovery': false,
+        'tags': [],
+        'customCoverage': false,
+        '_object': {
+          'id': 'fe84a421-ab24-4fd0-8a98-b65664ec0126',
+          'dateCreated': '2021-09-23T02:37:26Z',
+          'tags': '[]',
+          'lastUpdated': '2021-09-23T02:37:26Z',
+          'vendor': '{id: "a71f4c10-e238-4d07-8ce1-1a69ae00570b", name: …}',
+          'coverage': '[]',
+          'source': 'GOKb',
+          'remoteKb': '{activationEnabled: false, active: true, cursor: "2…}',
+          'name': 'Brill Test',
+          'suppressFromDiscovery': false,
+          'reference': 'Brill_Test_2',
+          'resourceCount': 0,
+          'class': 'org.olf.kb.Pkg'
+        }
+      },
+      'activeFrom': '2021-09-01',
+      'poLines': [{
+        'id': 'e5b1cf47-2548-4913-819a-681720ea9206',
+        'poLineId': '556abc25-ebbf-3fb2-b478-1bfaff0af4dc',
+        'owner': '{id: "648f362b-9a71-4eb6-8fe8-9ed20dffb0dc"}'
+      }],
+      'suppressFromDiscovery': false,
+      'note': 'Agreement line note',
+      'customCoverage': false,
+      'explanation': 'Agreement includes a package containing this item',
+      'startDate': '2021-09-01',
+      'endDate': '2021-09-04',
+      'contentUpdated': null,
+      'haveAccess': false
+    },
+    {
+      'id': '9e46275f-2b11-4235-963e-a63ee04b4241',
+      'type': 'external',
+      'description': 'agreement line description',
+      'authority': 'EKB-PACKAGE',
+      'reference': '936-3581',
+      'explanation': null,
+      'startDate': '2021-09-01',
+      'endDate': '2021-09-30',
+      'activeFrom': '2021-09-01',
+      'activeTo': '2021-09-30',
+      'contentUpdated': null,
+      'haveAccess': true,
+      'suppressFromDiscovery': true,
+      'note': 'agreement line note',
+      'owner': '{agreementStatus: {…}, alternateNames: Array(0), ca…}',
+      'poLines': [{
+        'id': '971c9ba0-1cca-4238-978e-4d719af3ccff',
+        'poLineId': '556abc25-ebbf-3fb2-b478-1bfaff0af4dc',
+        'owner': '{id: "9e46275f-2b11-4235-963e-a63ee04b4241"}'
+      }],
+      'customCoverage': false,
+      'reference_object': {
+        'label': 'i-law.com',
+        'type': 'Package',
+        'provider': 'Informa Law & Finance',
+        'titleCount': 181,
+        'selectedCount': 181,
+        'contentType': 'Aggregated Full Text',
+        'providerName': 'Informa Law & Finance',
+        'isSelected': true
+      }
+    }
+  ],
+  'orderLines': [{
+    'id': '556abc25-ebbf-3fb2-b478-1bfaff0af4dc',
+    'edition': 'First edition',
+    'checkinItems': false,
+    'agreementId': '09c6ed1b-3984-4d9a-8f9b-e1200b68b61c',
+    'acquisitionMethod': 'Purchase',
+    'alerts': '[]',
+    'cancellationRestriction': false,
+    'cancellationRestrictionNote': '',
+    'claims': '[{…}]',
+    'collection': false,
+    'contributors': '[]',
+    'cost': '{additionalCost: 0, currency: "USD", discount: 0, d…}',
+    'description': '',
+    'details': '{productIds: Array(1), receivingNote: "", subscript…}',
+    'donor': '',
+    'fundDistribution': '[{…}]',
+    'isPackage': false,
+    'locations': '[{…}]',
+    'orderFormat': 'Other',
+    'paymentStatus': 'Pending',
+    'physical': '{materialSupplier: "e0fb5df2-cdf1-11e8-a8d5-f2801f1…}',
+    'poLineDescription': '',
+    'poLineNumber': '81-1',
+    'publicationDate': '1915',
+    'publisher': 'American Bar Association',
+    'purchaseOrderId': 'c27e60f9-6361-44c1-976e-0c4821a33a7d',
+    'receiptStatus': 'Pending',
+    'reportingCodes': '[]',
+    'requester': '',
+    'rush': false,
+    'selector': '',
+    'source': 'User',
+    'tags': '{tagList: Array(1)}',
+    'titleOrPackage': 'ABA Journal',
+    'vendorDetail': '{instructions: "", noteFromVendor: "", referenceNum…}',
+    'metadata': '{createdDate: "2021-09-23T01:53:26.249+00:00", upda…}'
   }],
-  'agreementLinesToAdd': '[]',
-  'agreementStatusValues': '[{…}, {…}, {…}, {…}, {…}]',
-  'reasonForClosureValues': '[{…}, {…}, {…}, {…}]',
-  'amendmentStatusValues': '[{…}, {…}, {…}, {…}]',
-  'basket': [{
-    'class': 'org.olf.kb.PackageContentItem',
-    'id': 'c4a72ccf-8be1-453a-9147-ab77d078bb56',
-    'name': "'200 Years of Ricardian Trade Theory' on Platform 'SpringerLink' in Package Springer eBooks: Economics and Finance 2017",
-    '_object': '{addedTimestamp: 1632275874997, class: "org.olf.kb.…}'
-  }],
-  'contactRoleValues': '[{…}, {…}, {…}, {…}]',
-  'documentCategories': '[{…}, {…}, {…}]',
-  'externalAgreementLine': '[]',
-  'isPerpetualValues': '[{…}, {…}]',
-  'licenseLinkStatusValues': '[{…}, {…}, {…}]',
-  'orderLines': '[]',
-  'orgRoleValues': '[{…}]',
-  'renewalPriorityValues': '[{…}, {…}, {…}]',
-  'supplementaryProperties': '[]',
-  'users': '[]'
 };
 
-const items = [{
-  'id': 'd97db008-c00e-442d-9aca-e58f2f25486e',
-  'poLines': [],
+const agreementLines = [{
+  'id': 'c439980b-5ed4-4c2d-ae2b-bb72fbea76a9',
+  'poLines': [{
+    'id': 'f802d6fa-cfc4-4931-9b27-cba443999a72',
+    'poLineId': '556abc25-ebbf-3fb2-b478-1bfaff0af4dc',
+    'owner': {
+      'id': 'c439980b-5ed4-4c2d-ae2b-bb72fbea76a9'
+    }
+  }],
   'activeFrom': '2021-09-01',
   'activeTo': '2021-09-30',
-  'note': 'note'
+  'note': 'agreement line note'
 }];
+
 
 describe('AgreementLinesFieldArray', () => {
   describe('with empty initial values', () => {
@@ -70,15 +201,42 @@ describe('AgreementLinesFieldArray', () => {
         >
           <FieldArray
             component={AgreementLinesFieldArray}
-            name="items"
+            name="agreementLines"
           />
         </TestForm>, translationsProperties
       );
     });
 
+    test('renders the IfEResourcesEnabled component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('IfEResourcesEnabled')).toBeInTheDocument();
+    });
+
+    it('renders empty field message', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('No agreement lines for this agreement')).toBeInTheDocument();
+    });
+
     it('renders empty field', () => {
       const { queryAllByTestId } = renderComponent;
-      expect(queryAllByTestId(/greementLinesFieldArray\[.*\]/).length).toEqual(0);
+      expect(queryAllByTestId(/agreementLinesFieldArray\[.*\]/).length).toEqual(0);
+    });
+
+    it('clicking the add button renders the agreementLine field', () => {
+      const { getByText, getByRole } = renderComponent;
+      userEvent.click(getByRole('button', { name: /Add agreement line/i }));
+      expect(getByText('AgreementLineField')).toBeInTheDocument();
+    });
+
+    it('adding/removing fields using the add/remove works as expected', () => {
+      const { getByRole, queryAllByTestId } = renderComponent;
+      expect(getByRole('button', { name: /Add agreement line/i })).toBeInTheDocument();
+      userEvent.click(getByRole('button', { name: /Add agreement line/i }));
+      expect(queryAllByTestId(/agreementLinesFieldArray\[.*\]/i).length).toEqual(1);
+      userEvent.click(getByRole('button', { name: /Add agreement line/i }));
+      expect(queryAllByTestId(/agreementLinesFieldArray\[.*\]/i).length).toEqual(2);
+      userEvent.click(getByRole('button', { name: /Remove agreement line 2/i }));
+      expect(queryAllByTestId(/agreementLinesFieldArray\[.*\]/i).length).toEqual(1);
     });
   });
 
@@ -87,14 +245,14 @@ describe('AgreementLinesFieldArray', () => {
     beforeEach(async () => {
       renderComponent = renderWithIntl(
         <TestForm
-          initialValues={{ items }}
+          initialValues={{ agreementLines }}
           onSubmit={onSubmit}
         >
           <FieldArray
             component={AgreementLinesFieldArray}
             data={data}
-            items={items}
-            name="items"
+            items={agreementLines}
+            name="agreementLines"
           />
         </TestForm>,
         translationsProperties
@@ -102,9 +260,9 @@ describe('AgreementLinesFieldArray', () => {
     });
 
     test('renders the IfEResourcesEnabled component', () => {
-        const { getByText } = renderComponent;
-        expect(getByText('IfEResourcesEnabled')).toBeInTheDocument();
-      });
+      const { getByText } = renderComponent;
+      expect(getByText('IfEResourcesEnabled')).toBeInTheDocument();
+    });
 
     test('renders the AgreementLineField component', () => {
       const { getByText } = renderComponent;
