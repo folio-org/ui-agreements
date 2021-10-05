bb1fcd0019be1eb48e62216cf4a4bce739856c2e
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-10-05 11:52:25 +0100

    Add test coverage for platformSection, PlatformInfo

---
+++ src/components/PlatformSections/PlatformInfo/testResources.js
@@ -0,0 +1,28 @@
+const data = {
+    'id': 'platformInfo',
+    'platform': {
+      'id': 'ab3680c0-dc61-479d-9697-5b11eed4334f',
+      'dateCreated': '2021-10-05T01:58:57Z',
+      'lastUpdated': '2021-10-05T01:58:57Z',
+      'name': 'Mathematical Sciences Publishers',
+      'locators': [
+        {
+          'id': 'ec691378-6ce1-4f58-afa2-c7349d582701',
+          'domainName': 'msp.org'
+        }
+      ]
+    },
+    'handlers': {
+      'onClose': () => {},
+      'onEdit':  () => {},
+      'onViewUrlCustomizer':  () => {},
+      'onClickProxyServerAction':  () => {},
+    },
+    'stringTemplates': {
+      'urlProxiers': '[]',
+      'urlCustomisers': '[]'
+    },
+    'proxyServers': []
+  };
+
+  export default data;
