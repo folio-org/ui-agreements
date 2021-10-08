3bd59efebc2430e748e8d41205fcafed25bdd696
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-10-08 16:09:07 +0100

    ERM 1401 added tests for PlatformSection, PlatformInfo component (#896)
    * Add test coverage for platformSection, PlatformInfo
    
    * button
    
    * ERM-1401 Added tests for PlatformSections, PlatformInfo component
    
    * Indent
    
    * added valid platform code
    
    Co-authored-by: aditya matukumalli <aditya.matukumalli@gmail.com>

---
+++ src/components/PlatformSections/PlatformInfo/PlatformInfo.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { KeyValue } from '@folio/stripes-testing';
+import data from './testResources';
+import translationsProperties from '../../../../test/helpers';
+import PlatformInfo from './PlatformInfo';
+
+describe('PlatformInfo', () => {
+  beforeEach(() => {
+    renderWithIntl(
+      <PlatformInfo platform={data.platform} />,
+      translationsProperties
+    );
+  });
+
+  test('renders the expected locators key value', async () => {
+    await KeyValue('Locators').has({ value: 'msp.org' });
+  });
+
+  test('renders the expected local platform code key value', async () => {
+    await KeyValue('Local platform code').has({ value: '2222' });
+  });
+});
+
