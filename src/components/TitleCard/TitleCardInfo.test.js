4685cefe257384efce4650298549fed0917402ae
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2021-10-18 16:45:20 +0200

    ERM-1290 Add test coverage for ui-agreements TitleCard
    * add data-testid to TitleCard
    * add tests for TitleCard, TitleCardInfo, MonographResourceInfo, SerialResourceInfo components

---
+++ src/components/TitleCard/TitleCardInfo.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { StaticRouter as Router } from 'react-router-dom';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import translationsProperties from '../../../test/helpers';
+import TitleCardInfo from './TitleCardInfo';
+import { monographTitle, serialTitleWithRelatedTitles } from './testResources';
+
+jest.mock('./MonographResourceInfo', () => () => <div>MonographResourceInfo</div>);
+jest.mock('./SerialResourceInfo', () => () => <div>SerialResourceInfo</div>);
+jest.mock('../RelatedTitleInfo', () => () => <div>RelatedTitleInfo</div>);
+
+let renderComponent;
+describe('TitleCardInfo', () => {
+    describe('with serial resource with related titles', () => {
+        beforeEach(() => {
+            renderComponent = renderWithIntl(
+              <Router>
+                <TitleCardInfo
+                  title={serialTitleWithRelatedTitles}
+                />
+              </Router>,
+            translationsProperties
+            );
+        });
+
+        test('renders the SerialResourceInfo', () => {
+            const { getByText } = renderComponent;
+            expect(getByText('SerialResourceInfo')).toBeInTheDocument();
+        });
+
+        test('renders the RelatedTitleInfo', () => {
+          const { getByText } = renderComponent;
+          expect(getByText('RelatedTitleInfo')).toBeInTheDocument();
+        });
+    });
+
+    describe('with monograph resource', () => {
+      beforeEach(() => {
+          renderComponent = renderWithIntl(
+            <Router>
+              <TitleCardInfo
+                title={monographTitle}
+              />
+            </Router>,
+          translationsProperties
+          );
+      });
+
+      test('renders the MonographResourceInfo', () => {
+          const { getByText } = renderComponent;
+          expect(getByText('MonographResourceInfo')).toBeInTheDocument();
+      });
+  });
+});
