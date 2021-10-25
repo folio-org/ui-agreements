c9a977ce4b5801c64e7901e8c3a4c68f5321970d
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-10-25 11:12:29 +0100

    Removed data-testid from Agreement file

--- src/routes/AgreementViewRoute/AgreementViewRoute.test.js
+++ src/routes/AgreementViewRoute/AgreementViewRoute.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
