69d71cedb9e0d795897dcd494835e7143f0d8e49
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-11-01 15:25:36 +0000

    updated testResource file, mocked filterHandlers, state

--- src/components/EResourceFilters/EResourceFilters.test.js
+++ src/components/EResourceFilters/EResourceFilters.test.js
@@ -3,11 +3,20 @@ import '@folio/stripes-erm-components/test/jest/__mock__';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
 import { MemoryRouter } from 'react-router-dom';
 import { screen } from '@testing-library/react';
-import { Accordion, Checkbox, MultiSelect } from '@folio/stripes-testing';
+import { Accordion, Checkbox } from '@folio/stripes-testing';
 import translationsProperties from '../../../test/helpers';
-import { checkedActivedFilters, checkedData, activeFilters, data, filterHandlers } from './testResources';
+import { activeFilters, data } from './testResources';
 import EResourceFilters from './EResourceFilters';
 
+const stateMock = jest.fn();
+
+  const filterHandlers = {
+    state : stateMock,
+    checkbox: () => {},
+    clear: () => {},
+    clearGroup: () => {},
+    reset: () => {},
+};
 
 describe('EResourceFilters', () => {
   describe('EResourceFilters with unchecked filters', () => {
@@ -79,61 +88,25 @@ describe('EResourceFilters', () => {
       it('should display the correct Tags filter name', () => {
         expect(screen.getByText('Tags')).toBeVisible();
       });
-    });
 
-    describe('EResourceFilters with checked filters', () => {
-      beforeEach(() => {
-         renderWithIntl(
-           <MemoryRouter>
-             <EResourceFilters
-               activeFilters={checkedActivedFilters}
-               data={checkedData}
-               filterHandlers={filterHandlers}
-             />
-           </MemoryRouter>,
-            translationsProperties
-          );
-        });
-
-        test('renders Type Monograph checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-type-monograph' }).is({ checked: true });
-        });
-
-        test('renders Type Serial checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-type-serial' }).is({ checked: true });
-        });
-
-        test('renders Publication type journal Checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-publicationType-journal' }).is({ checked: true });
-        });
-
-        test('renders Publication type book Checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-publicationType-book' }).is({ checked: true });
-        });
-
-        test('renders Publication type database Checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-publicationType-database' }).is({ checked: true });
-        });
-
-        test('renders Is package yes Checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-class-package' }).is({ checked: true });
-        });
-
-        test('renders Is package no Checkbox as checked', async () => {
-          await Checkbox({ id: 'clickable-filter-class-nopackage' }).is({ checked: true });
-        });
-
-        it('should display filter by tags accordion', () => {
-          expect(screen.getByText('Tags')).toBeInTheDocument();
-        });
-
-        // test('renders the Tags multiSelection', async () => {
-        //   await MultiSelect('Tags').exists();
-        // });
-
-        // test('renders the Tags multiSelection', async () => {
-        //   await MultiSelect('Tags').select(['catalogingrecords', 'important', 'urgent']);
-        // });
+      it('should display filter by tags accordion', () => {
+        expect(screen.getByText('Tags')).toBeInTheDocument();
       });
+
+      it('should display filter by tags accordion', () => {
+        expect(screen.getByText('External data source')).toBeInTheDocument();
+      });
+
+      test('renders the checkboxes  ', async () => {
+        await Checkbox({ id: 'clickable-filter-type-monograph' }).click();
+        await Checkbox({ id: 'clickable-filter-type-monograph' }).click();
+        await Checkbox({ id: 'clickable-filter-publicationType-journal' }).click();
+        await Checkbox({ id: 'clickable-filter-publicationType-book' }).click();
+        await Checkbox({ id: 'clickable-filter-publicationType-database' }).click();
+        await Checkbox({ id: 'clickable-filter-class-package' }).click();
+        await Checkbox({ id: 'clickable-filter-class-nopackage' }).click();
+        expect(stateMock).toHaveBeenCalled();
+      });
+    });
   });
 
