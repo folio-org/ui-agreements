26cea246a366a1b84a32b2d06ed8591c93c307fc
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-11-05 17:57:23 +0000

    Added test for GeneralSetting component

--- src/settings/GeneralSettings.test.js
+++ src/settings/GeneralSettings.test.js
@@ -2,11 +2,8 @@ import React from 'react';
 import '@folio/stripes-erm-components/test/jest/__mock__';
 import { MemoryRouter } from 'react-router-dom';
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
-import { screen } from '@testing-library/react';
 import translationsProperties from '../../test/helpers';
 import GeneralSettings from './GeneralSettings';
-
-jest.mock('./GeneralSettingsForm', () => () => <div>GeneralSettingsForm</div>);
 
 describe('GeneralSettings', () => {
     describe('rendering the GeneralSettings component', () => {
@@ -20,13 +17,7 @@ describe('GeneralSettings', () => {
         );
       });
 
-      test('renders GeneralSettingsForm component', () => {
-        const { getByText } = renderComponent;
-        expect(getByText('GeneralSettingsForm')).toBeInTheDocument();
-      });
-
       test('renders ConfigManager component ', () => {
-        screen.debug();
         const { getByText } = renderComponent;
         expect(getByText('ConfigManager')).toBeInTheDocument();
       });
