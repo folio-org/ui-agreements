3b439287feae7a08be6b8407b6b2d78af84a9a2b
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-11-17 18:35:06 +0000

    Add tests for POLineField component

---
+++ src/components/POLinesFieldArray/POLineField/POLineField.test.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { StaticRouter as Router } from 'react-router-dom';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import translationsProperties from '../../../../test/helpers';
+import { input, id, meta, onPOLineSelected, poLine, data } from './testResources';
+import POLineField from './POLineField';
+
+jest.mock('../../POLineCard', () => () => <div>POLineCard</div>);
+
+let renderComponent;
+describe('POLineField', () => {
+  describe('POLineField with linked PO Line ', () => {
+      beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <Router>
+          <POLineField
+            id={id}
+            input={input}
+            meta={meta}
+            onPOLineSelected={onPOLineSelected}
+            poLine={poLine}
+          />
+        </Router>,
+        translationsProperties
+      );
+    });
+
+    test('renders the POLineCard component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('POLineCard')).toBeInTheDocument();
+    });
+  });
+
+  describe('POLineField with no PO Line', () => {
+      beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <Router>
+          <POLineField
+            {...data}
+          />
+        </Router>,
+        translationsProperties
+      );
+    });
+
+    test('displays the PO LineCard title', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('PO line')).toBeInTheDocument();
+    });
+
+    test('displays the No "find-po-line" plugin is installed layout', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('No "find-po-line" plugin is installed')).toBeInTheDocument();
+    });
+
+    test('displays the No PO line linked layout', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('No PO line linked')).toBeInTheDocument();
+    });
+
+    test('displays Link a PO line to get started layout', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Link a PO line to get started')).toBeInTheDocument();
+    });
+  });
+});
