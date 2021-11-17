73d8d085ba45ac2804d655b4beed4d6f9caa055e
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2021-11-17 10:45:17 -0500

    Add rtl tests for Package component, refs ERM-1672

---
+++ src/components/views/Package/Package.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import translationsProperties from '../../../../test/helpers';
+
+import Package from './Package';
+import { data, handlers } from './testResources';
+
+jest.mock('../../EResourceSections/Agreements', () => () => <div>Agreements</div>);
+jest.mock('../../EResourceSections/PackageContents', () => () => <div>PackageContents</div>);
+jest.mock('../../EResourceSections/PackageInfo', () => () => <div>PackageInfo</div>);
+
+describe('Package', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <Package
+          data={data}
+          handlers={handlers}
+        />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  it('renders the PackageInfo component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('PackageInfo')).toBeInTheDocument();
+  });
+
+  it('renders the Agreements component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Agreements')).toBeInTheDocument();
+  });
+
+  it('renders the PackageContents component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('PackageContents')).toBeInTheDocument();
+  });
+
+  it('renders the NotesSmartAccordion component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('NotesSmartAccordion')).toBeInTheDocument();
+  });
+});
