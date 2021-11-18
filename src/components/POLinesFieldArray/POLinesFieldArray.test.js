a79497515ef7b30314d30007aa06584066ce49a5
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-11-18 16:53:08 +0000

    ERM-1400 Add tests for POLinesFieldArray component

---
+++ src/components/POLinesFieldArray/POLinesFieldArray.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { StaticRouter as Router } from 'react-router-dom';
+import { Button } from '@folio/stripes-testing';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { data, noPolineData } from './testResources';
+import translationsProperties from '../../../test/helpers';
+import POLinesFieldArray from './POLinesFieldArray';
+
+jest.mock('@folio/stripes-erm-components', () => ({
+    ...jest.requireActual('@folio/stripes-erm-components'),
+    EditCard: () => <div>EditCard</div>
+ }));
+
+ const onAddField = jest.fn();
+
+let renderComponent;
+describe('POLinesFieldArray', () => {
+  describe('POLinesFieldArray with linked PO Line ', () => {
+      beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <Router>
+          <POLinesFieldArray {...data} />
+        </Router>,
+        translationsProperties
+      );
+    });
+
+    test('renders the EditCard component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('EditCard')).toBeInTheDocument();
+    });
+
+    test('renders the Add PO line button', async () => {
+      await Button('Add PO line').exists();
+    });
+  });
+
+  describe('POLinesFieldArray with no linked PO lines ', () => {
+    beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <Router>
+        <POLinesFieldArray {...noPolineData} />,
+      </Router>,
+      translationsProperties
+    );
+  });
+
+  test('renders No PO lines message', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('No PO lines for this agreement line')).toBeInTheDocument();
+  });
+
+  test('renders the Add PO line button', async () => {
+    await Button('Add PO line').exists();
+  });
+
+  describe('clicking add button', () => {
+    beforeEach(async () => { await Button('Add PO line').click(); });
+
+    test('should invoke the onFilterPackageContents callback', () => {
+      expect(onAddField).not.toHaveBeenCalled();
+    });
+  });
+ });
+});
---
+++ src/components/POLinesFieldArray/testResources.js
@@ -0,0 +1,114 @@
+const data = {
+    'fields': {
+        'name': 'poLines',
+        'forEach': () => {},
+        'length': 1,
+        'map': () => {},
+        'setFieldData': () => {},
+        'insert': () => {},
+        'concat': () => {},
+        'move': () => {},
+        'pop': () => {},
+        'push': () => {},
+        'remove': () => {},
+        'removeBatch': () => {},
+        'shift': () => {},
+        'swap': () => {},
+        'unshift': () => {},
+        'update': () => {},
+        'value': [{
+            '_delete': false
+        }]
+    },
+    'meta': {
+        'active': false,
+        'data': '{}',
+        'dirty': true,
+        'dirtySinceLastSubmit': false,
+        'error': [{
+            'poLineId': '<Memo />'
+        }],
+        'invalid': true,
+        'modified': false,
+        'modifiedSinceLastSubmit': false,
+        'pristine': false,
+        'submitFailed': false,
+        'submitSucceeded': false,
+        'submitting': false,
+        'touched': false,
+        'valid': false,
+        'validating': false,
+        'visited': false,
+        '__versions': {
+            'final-form': '4.20.4',
+            'react-final-form': '6.5.7',
+            'react-final-form-arrays': '3.1.2'
+        }
+    },
+    'agreementLineIndex': 0,
+    'poLines': [],
+    'items': [{
+        '_delete': false
+    }],
+    'name': 'poLines',
+    'onAddField': () => {},
+    'onDeleteField': () => {},
+    'onMarkForDeletion': () => {},
+    'onPrependField': () => {},
+    'onReplaceField': () => {},
+    'onUpdateField': () => {}
+};
+
+const noPolineData = {
+    'fields': {
+        'name': 'poLines',
+        'forEach': () => {},
+        'length': 0,
+        'map': () => {},
+        'setFieldData': () => {},
+        'insert': () => {},
+        'concat': () => {},
+        'move': () => {},
+        'pop': () => {},
+        'push': () => {},
+        'remove': () => {},
+        'removeBatch': () => {},
+        'shift': () => {},
+        'swap': () => {},
+        'unshift': () => {},
+        'update': () => {}
+    },
+    'meta': {
+        'active': false,
+        'data': {},
+        'dirty': false,
+        'dirtySinceLastSubmit': false,
+        'invalid': false,
+        'modified': false,
+        'modifiedSinceLastSubmit': false,
+        'pristine': true,
+        'submitFailed': false,
+        'submitSucceeded': false,
+        'submitting': false,
+        'touched': false,
+        'valid': true,
+        'validating': false,
+        'visited': false,
+        '__versions': '{final-form: "4.20.4", react-final-form: "6.5.7", r…}'
+    },
+    'agreementLineIndex': 0,
+    'poLines': [],
+    'items': [],
+    'name': 'poLines',
+    'onAddField': () => {},
+    'onDeleteField': () => {},
+    'onMarkForDeletion': () => {},
+    'onPrependField': () => {},
+    'onReplaceField': () => {},
+    'onUpdateField': () => {},
+};
+
+export {
+    data,
+    noPolineData
+};
