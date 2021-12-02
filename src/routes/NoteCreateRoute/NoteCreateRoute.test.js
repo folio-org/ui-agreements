f77f4ba9788ed86526edeed46f5f1b7ecaa92a64
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-12-02 10:04:47 +0000

    Add tests for NoteCreateRoute component, refs ERM-1493

---
+++ src/routes/NoteCreateRoute/NoteCreateRoute.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import translationsProperties from '../../../test/helpers';
+import NoteCreateRoute from './NoteCreateRoute';
+
+const history = {
+      'length': 42,
+      'action': 'POP',
+      'location': {
+        'pathname': '/erm/notes/create',
+        'search': '',
+        'hash': '',
+        'state': {
+          'entityName': 'MR agreement',
+          'entityType': 'agreement',
+          'entityId': 'c4ed2be0-380a-421a-9364-0f411e9c897b',
+          'referredRecordData': '{}'
+        },
+        'key': '64w68l'
+      },
+      'createHref': 'ƒ createHref() {}',
+      'push': 'ƒ push() {}',
+      'replace': 'ƒ replace() {}',
+      'go': 'ƒ go() {}',
+      'goBack': 'ƒ goBack() {}',
+      'goForward': 'ƒ goForward() {}',
+      'block': 'ƒ block() {}',
+      'listen': 'ƒ listen() {}'
+    };
+
+    const location = {
+      'pathname': '/erm/notes/create',
+      'search': '',
+      'hash': '',
+      'state': {
+        'entityName': 'MR agreement',
+        'entityType': 'agreement',
+        'entityId': 'c4ed2be0-380a-421a-9364-0f411e9c897b',
+        'referredRecordData': {}
+      },
+      'key': '64w68l'
+    };
+
+describe('NoteCreateRoute', () => {
+  describe('renders the NoteCreateRoute', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <NoteCreateRoute history={history} location={location} />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the NoteCreatePage component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('NoteCreatePage')).toBeInTheDocument();
+    });
+  });
+});
