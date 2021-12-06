e0d9559a84ee413b80a2e7019f719418f6ce75ba
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-12-06 21:29:32 +0000

    Add tests for PlatformsRoute component

---
+++ src/routes/PlatformsRoute/PlatformsRoute.test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import {
+    history,
+    location,
+    match,
+    platforms,
+    mutator
+} from './testResources';
+import translationsProperties from '../../../test/helpers';
+import PlatformsRoute from './PlatformsRoute';
+
+
+const props = {
+    history: { history },
+    location: { location },
+    match: { match },
+    mutator: { mutator },
+    resources: { platforms }
+  };
+
+describe('PlatformsRoute', () => {
+  describe('rendering the route with permissions', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <PlatformsRoute {...props} />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the platforms component', () => {
+      const { getByTestId } = renderComponent;
+      expect(getByTestId('platforms')).toBeInTheDocument();
+    });
+
+    describe('re-rendering the route', () => { // makes sure that we hit the componentDidUpdate block
+      beforeEach(() => {
+        renderWithIntl(
+          <MemoryRouter>
+            <PlatformsRoute {...props} />
+          </MemoryRouter>,
+          translationsProperties,
+          renderComponent.rerender
+        );
+      });
+
+      test('renders the platforms component', () => {
+        const { getByTestId } = renderComponent;
+        expect(getByTestId('platforms')).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('rendering with no permissions', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <PlatformsRoute
+            {...props}
+            stripes={{ hasPerm: () => false }}
+          />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('displays the permission error', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Sorry - your permissions do not allow access to this page.')).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/routes/PlatformsRoute/testResources.js
@@ -0,0 +1,466 @@
+const history = {
+    'length': 21,
+    'action': 'PUSH',
+    'location': '{hash: "", key: "pn5ey9", pathname: "/erm/platforms…}',
+    'createHref': () => {},
+    'push': () => {},
+    'replace': () => {},
+    'go': () => {},
+    'goBack': () => {},
+    'goForward': () => {},
+    'block': () => {},
+    'listen': () => {},
+};
+
+const location = {
+    'pathname': '/erm/platforms/606ef7de-6db5-498a-9ca5-7bc99ff62136/urlcustomizer/433f88e4-55df-4c65-81a8-cb4681115824',
+    'search': '?sort=name',
+    'hash': '',
+    'key': 'pn5ey9'
+};
+
+const match = {
+    'path': '/erm/platforms/:id?',
+    'url': '/erm/platforms/606ef7de-6db5-498a-9ca5-7bc99ff62136',
+    'isExact': false,
+    'params': '{id: "606ef7de-6db5-498a-9ca5-7bc99ff62136"}'
+};
+
+const platforms = {
+    'hasLoaded': true,
+    'isPending': false,
+    'failed': false,
+    'records': [{
+            'id': 'bf42cc24-e9f1-4024-a2c3-45c1e16c46b8',
+            'dateCreated': '2021-12-06T02:56:36Z',
+            'lastUpdated': '2021-12-06T02:56:36Z',
+            'name': 'ACM Digital Library',
+            'locators': [{
+                'id': '15aa5571-de87-425d-b172-b79b1d308ceb',
+                'domainName': 'dl.acm.org'
+            }]
+        },
+        {
+            'id': 'a5a239e8-fa8b-45a9-89d2-a269676b3aff',
+            'dateCreated': '2021-12-06T02:18:13Z',
+            'lastUpdated': '2021-12-06T02:18:13Z',
+            'name': 'ACS Publications',
+            'locators': [{
+                'id': 'ab5a8233-6dcc-408e-8dd7-4f8da599e651',
+                'domainName': 'pubs.acs.org'
+            }]
+        },
+        {
+            'id': '0acf79cd-8778-43cb-9996-4c21f45d05fe',
+            'dateCreated': '2021-12-06T02:44:27Z',
+            'lastUpdated': '2021-12-06T02:44:27Z',
+            'name': 'AIP Scitation',
+            'locators': [{
+                'id': '65b52d6f-9dba-43a8-b48d-c28d532c6f2d',
+                'domainName': 'scitation.aip.org'
+            }]
+        },
+        {
+            'id': '8fac3908-3160-413d-aaad-fe51d44a7974',
+            'dateCreated': '2021-12-06T02:17:41Z',
+            'lastUpdated': '2021-12-06T02:17:41Z',
+            'name': 'ASCE Library',
+            'locators': [{
+                'id': 'e3012038-1ae8-4628-ad10-15f402fa3838',
+                'domainName': 'ascelibrary.org'
+            }]
+        },
+        {
+            'id': '405041c5-5153-4260-b54a-a0e857976d00',
+            'dateCreated': '2021-12-06T02:15:46Z',
+            'lastUpdated': '2021-12-06T02:15:46Z',
+            'name': 'AVS - American Vacuum Society',
+            'locators': [{
+                'id': '5ffce5c6-9bb4-4f2e-bfe3-108c9de3788f',
+                'domainName': 'avs.scitation.org'
+            }]
+        },
+        {
+            'id': '720a6265-7589-4618-9e23-b627105b625f',
+            'dateCreated': '2021-12-06T02:17:53Z',
+            'lastUpdated': '2021-12-06T02:17:53Z',
+            'name': 'BMJ Journals',
+            'locators': [{
+                'id': 'cf27fc5c-21f9-478f-838b-652a2140bf0b',
+                'domainName': 'journals.bmj.com'
+            }]
+        },
+        {
+            'id': 'add3e1d8-6bf7-40e6-af69-be02c3e59188',
+            'dateCreated': '2021-12-06T02:36:11Z',
+            'lastUpdated': '2021-12-06T02:36:11Z',
+            'name': 'Brill',
+            'locators': [{
+                'id': 'b462841b-cab2-4098-8b80-49092a0f1b29',
+                'domainName': 'booksandjournals.brillonline.com'
+            }]
+        },
+        {
+            'id': 'db3035e4-5152-42f8-ace1-e18061ede637',
+            'dateCreated': '2021-12-06T02:04:29Z',
+            'lastUpdated': '2021-12-06T02:04:29Z',
+            'name': 'Budrich Journals',
+            'locators': [{
+                'id': 'f0d6e374-ebbd-492c-8505-3fcef7f20899',
+                'domainName': 'budrich-journals.de'
+            }]
+        },
+        {
+            'id': 'ce51353a-58f4-4c45-8e47-16e0cbeafd95',
+            'dateCreated': '2021-12-06T02:39:33Z',
+            'lastUpdated': '2021-12-06T02:39:33Z',
+            'name': 'Cambridge University Press',
+            'locators': [{
+                'id': 'ab8af871-1d03-4a92-a5e1-6140cdc7d360',
+                'domainName': 'www.cambridge.org'
+            }]
+        },
+        {
+            'id': 'd32a3e79-03f7-4e79-8244-196809ce3df0',
+            'dateCreated': '2021-12-06T02:05:05Z',
+            'lastUpdated': '2021-12-06T02:05:05Z',
+            'name': 'Content Select',
+            'locators': [{
+                'id': 'd60fd47f-3e43-4c4e-9e19-fa488823d0bc',
+                'domainName': 'content-select.com'
+            }]
+        },
+        {
+            'id': '1ba1d4f1-d5ec-44e0-9c19-1e90ecfba414',
+            'dateCreated': '2021-12-06T02:04:56Z',
+            'lastUpdated': '2021-12-06T02:04:56Z',
+            'name': 'DeGruyter Online',
+            'locators': [{
+                'id': '097248d6-e757-43ab-b2b2-d24be12d48a7',
+                'domainName': 'www.degruyter.com'
+            }]
+        },
+        {
+            'id': '1e630541-d0dc-4b72-9d37-217e944d516e',
+            'dateCreated': '2021-12-06T02:04:35Z',
+            'lastUpdated': '2021-12-06T02:04:35Z',
+            'name': 'Duncker & Humblot eJournals',
+            'locators': [{
+                'id': 'afe85408-a5b2-4c2e-b1d5-036df176192f',
+                'domainName': 'elibrary.duncker-humblot.com'
+            }]
+        },
+        {
+            'id': '7ee4d67b-827a-4582-8d0b-f6c196e9163b',
+            'dateCreated': '2021-12-06T02:16:56Z',
+            'lastUpdated': '2021-12-06T02:16:56Z',
+            'name': 'EBSCOhost',
+            'locators': [{
+                'id': '0e66fc4d-ed30-4701-a3b3-791e65f3ef62',
+                'domainName': 'www.ebscohost.com'
+            }]
+        },
+        {
+            'id': '06d2e164-59aa-4708-b48d-dc6372f93297',
+            'dateCreated': '2021-12-06T01:57:18Z',
+            'lastUpdated': '2021-12-06T01:57:18Z',
+            'name': 'Elgaronline',
+            'locators': [{
+                'id': '5a44a12d-a314-4b58-ac0f-c42b54b0b80b',
+                'domainName': 'www.elgaronline.com'
+            }]
+        },
+        {
+            'id': '5e37dfd6-a5c0-44f2-9001-12f2a853d489',
+            'dateCreated': '2021-12-06T01:52:07Z',
+            'lastUpdated': '2021-12-06T01:52:07Z',
+            'name': 'Emerald Insight',
+            'locators': [{
+                'id': '39385228-654a-454d-b71d-66413b6ad876',
+                'domainName': 'www.emeraldinsight.com'
+            }]
+        },
+        {
+            'id': '425fee46-bf3f-42e1-9bba-bcb2d67ee042',
+            'dateCreated': '2021-12-06T02:51:34Z',
+            'lastUpdated': '2021-12-06T02:51:34Z',
+            'name': 'Hanser eLibrary',
+            'locators': [{
+                'id': 'bb846f5e-8e80-453b-a85f-ac386cb5eef5',
+                'domainName': 'www.hanser-elibrary.com'
+            }]
+        },
+        {
+            'id': '87979354-a45b-4acf-b1c8-4e6f87a68a40',
+            'dateCreated': '2021-12-06T02:18:03Z',
+            'lastUpdated': '2021-12-06T02:18:03Z',
+            'name': 'Hogrefe eContent',
+            'locators': [{
+                'id': 'bb0cd86c-c8d0-4748-a07d-85f5448abf6e',
+                'domainName': 'econtent.hogrefe.com'
+            }]
+        },
+        {
+            'id': '9159da2c-b862-4f2c-aa7f-1750454b404b',
+            'dateCreated': '2021-12-06T02:44:23Z',
+            'lastUpdated': '2021-12-06T02:44:23Z',
+            'name': 'IEEE Xplore',
+            'locators': [{
+                'id': '1ff920cd-43b3-4103-adc3-be0abf29d370',
+                'domainName': 'ieeexplore.ieee.org'
+            }]
+        },
+        {
+            'id': 'd645935c-fcf2-41d7-8427-a24d6404590a',
+            'dateCreated': '2021-12-06T02:48:40Z',
+            'lastUpdated': '2021-12-06T02:48:40Z',
+            'name': 'IET Digital Library',
+            'locators': [{
+                'id': '4b1888bb-fa84-4e7d-bdaf-20e4e2a98ac1',
+                'domainName': 'digital-library.theiet.org'
+            }]
+        },
+        {
+            'id': '9235b1aa-4b75-4b7d-aa43-e1876505a036',
+            'dateCreated': '2021-12-06T02:18:23Z',
+            'lastUpdated': '2021-12-06T02:18:23Z',
+            'name': 'JSTOR',
+            'locators': [{
+                'id': '7f67840e-2dfc-4a3e-88bc-bdce380a1fac',
+                'domainName': 'www.jstor.org'
+            }]
+        },
+        {
+            'id': '606ef7de-6db5-498a-9ca5-7bc99ff62136',
+            'dateCreated': '2021-12-06T02:15:47Z',
+            'lastUpdated': '2021-12-06T02:15:47Z',
+            'name': 'Karger',
+            'locators': [{
+                'id': '84b64f59-a075-47b9-a240-b599eb67e01c',
+                'domainName': 'www.karger.com'
+            }]
+        },
+        {
+            'id': '1dae0388-ec01-4c10-b3b6-f3ba00a7dc84',
+            'dateCreated': '2021-12-06T02:00:03Z',
+            'lastUpdated': '2021-12-06T02:00:03Z',
+            'name': 'Mary Ann Liebert, Inc. Publishers',
+            'locators': [{
+                'id': 'eeebd68d-13b2-4fde-bbc5-7edd10d73cbf',
+                'domainName': 'www.liebertpub.com'
+            }]
+        },
+        {
+            'id': 'f735c28c-f467-4eec-b021-d2582420bab0',
+            'dateCreated': '2021-12-06T02:04:14Z',
+            'lastUpdated': '2021-12-06T02:04:14Z',
+            'name': 'Mathematical Sciences Publishers',
+            'locators': [{
+                'id': '57d69f36-8dac-4dde-b7aa-05d00ed73c12',
+                'domainName': 'msp.org'
+            }]
+        },
+        {
+            'id': '569d7129-9c81-4089-b604-dc880685d637',
+            'dateCreated': '2021-12-06T02:03:37Z',
+            'lastUpdated': '2021-12-06T02:03:37Z',
+            'name': 'OECD iLibrary',
+            'locators': [{
+                'id': 'c643304c-0710-499d-a7c9-2cc08f75c584',
+                'domainName': 'www.oecd-ilibrary.org'
+            }]
+        },
+        {
+            'id': 'ed37e6a3-7aaf-48cf-9bd6-d8dd7d57c213',
+            'dateCreated': '2021-12-06T02:02:45Z',
+            'lastUpdated': '2021-12-06T02:02:45Z',
+            'name': 'Oxford Academic Journals',
+            'locators': [{
+                'id': 'b51976a7-41e3-4dc3-ab0f-cae239ae629e',
+                'domainName': 'academic.oup.com'
+            }]
+        },
+        {
+            'id': 'ce6de686-2ad9-4030-ace2-b892438ca22c',
+            'dateCreated': '2021-12-06T02:03:34Z',
+            'lastUpdated': '2021-12-06T02:03:34Z',
+            'name': 'Oxford Medicine Online',
+            'locators': [{
+                'id': '4476e73e-8e99-4a53-9ea1-385e11a1734a',
+                'domainName': 'oxfordmedicine.com'
+            }]
+        },
+        {
+            'id': 'bc74a44e-15ee-4bd4-a9cf-9bd6c4e56411',
+            'dateCreated': '2021-12-06T02:02:45Z',
+            'lastUpdated': '2021-12-06T02:02:45Z',
+            'name': 'Perinorm',
+            'locators': [{
+                'id': 'd31f9cdb-19d4-4a06-8385-4d9d19f7aa76',
+                'domainName': 'www.perinorm.com'
+            }]
+        },
+        {
+            'id': '44fb44ee-37ca-4ccf-a7df-039a205330ba',
+            'dateCreated': '2021-12-06T02:04:23Z',
+            'lastUpdated': '2021-12-06T02:04:23Z',
+            'name': 'Project Euclid',
+            'locators': [{
+                'id': 'ba8b31fc-b790-4d25-aca4-391c0e3414fb',
+                'domainName': 'projecteuclid.org'
+            }]
+        },
+        {
+            'id': '6e0b3010-d139-4038-8ec6-433ed5d294df',
+            'dateCreated': '2021-12-06T02:16:55Z',
+            'lastUpdated': '2021-12-06T02:16:55Z',
+            'name': 'Psychiatryonline',
+            'locators': [{
+                'id': 'c585b127-6471-491f-9e47-02028666d569',
+                'domainName': 'journals.psychiatryonline.org'
+            }]
+        },
+        {
+            'id': '7c039165-7efd-428b-9428-2386594c2041',
+            'dateCreated': '2021-12-06T02:04:15Z',
+            'lastUpdated': '2021-12-06T02:04:15Z',
+            'name': 'Sage',
+            'locators': [{
+                'id': '2a7df833-b687-42bf-9035-ec74c492711c',
+                'domainName': 'journals.sagepub.com'
+            }]
+        },
+        {
+            'id': '47bbd026-6a2c-47e1-922b-2689b4a67b53',
+            'dateCreated': '2021-12-06T01:57:05Z',
+            'lastUpdated': '2021-12-06T01:57:05Z',
+            'name': 'SpringerLink',
+            'locators': [{
+                'id': 'd74a1c34-76fe-4842-9be0-bfba830882be',
+                'domainName': 'link.springer.com'
+            }]
+        },
+        {
+            'id': '6c741965-edfc-4a2f-bed3-ff7c1f7369ce',
+            'dateCreated': '2021-12-06T01:52:19Z',
+            'lastUpdated': '2021-12-06T01:52:19Z',
+            'name': 'Taylor & Francis Online',
+            'locators': [{
+                'id': 'b79dc273-e1bc-4025-9431-5e25ea504d45',
+                'domainName': 'www.tandfonline.com'
+            }]
+        },
+        {
+            'id': '9c00dacc-ba54-42b1-bcb5-e1825a0c3832',
+            'dateCreated': '2021-12-06T02:39:26Z',
+            'lastUpdated': '2021-12-06T02:39:26Z',
+            'name': 'Testanbieter alte GUI',
+            'locators': [{
+                'id': '53eee74a-e490-4c07-9913-7361ba09692e',
+                'domainName': 'example.testanbieter.de'
+            }]
+        },
+        {
+            'id': 'ace98489-983d-438b-ae81-222ed40ef455',
+            'dateCreated': '2021-12-06T02:16:36Z',
+            'lastUpdated': '2021-12-06T02:16:36Z',
+            'name': 'Wiley Online Library',
+            'locators': [{
+                'id': '2d42833f-4c40-4c7a-b2c0-4089d17280bf',
+                'domainName': 'onlinelibrary.wiley.com'
+            }]
+        },
+        {
+            'id': '20746fa8-52e1-4b8c-aa97-c2aac8291d0d',
+            'dateCreated': '2021-12-06T02:40:12Z',
+            'lastUpdated': '2021-12-06T02:40:12Z',
+            'name': 'aiaa.org',
+            'locators': [{
+                'id': '4564b0ea-5cc9-4a4e-afcb-2756a6fa3ff9',
+                'domainName': 'www.aiaa.org'
+            }]
+        },
+        {
+            'id': '489f1f42-aa9e-4c8f-bf92-8c61285f2c41',
+            'dateCreated': '2021-12-06T02:17:41Z',
+            'lastUpdated': '2021-12-06T02:17:41Z',
+            'name': 'dlib.eastview.com',
+            'locators': [{
+                'id': 'bf76232f-860f-4212-a9c4-58e5852979a3',
+                'domainName': 'dlib.eastview.com'
+            }]
+        },
+        {
+            'id': '04aa1870-5289-41b2-8f3e-2332c6b58ba4',
+            'dateCreated': '2021-12-06T03:21:00Z',
+            'lastUpdated': '2021-12-06T03:21:00Z',
+            'name': 'epubs SIAM',
+            'locators': [{
+                'id': 'ebc92282-f2c9-4db7-8416-324987931f5a',
+                'domainName': 'epubs.siam.org'
+            }]
+        },
+        {
+            'id': '221e7f7d-d94b-4a78-b073-4dabf6dd7bf7',
+            'dateCreated': '2021-12-06T02:00:22Z',
+            'lastUpdated': '2021-12-06T02:00:22Z',
+            'name': 'igi-global.com',
+            'locators': [{
+                'id': '5c032430-ce71-4024-9ca1-784041fced84',
+                'domainName': 'www.igi-global.com'
+            }]
+        },
+        {
+            'id': 'ac028756-8d70-43c1-9f60-6292fee01ebc',
+            'dateCreated': '2021-12-06T02:38:46Z',
+            'lastUpdated': '2021-12-06T02:38:46Z',
+            'name': 'nationallizenzen.zbw.eu',
+            'locators': [{
+                'id': '68c928e8-8a5f-432c-9d1c-77e958d3cd50',
+                'domainName': 'nationallizenzen.zbw.eu'
+            }]
+        },
+        {
+            'id': '1607458a-d4e9-495c-bd2c-39595bced67c',
+            'dateCreated': '2021-12-06T02:04:39Z',
+            'lastUpdated': '2021-12-06T02:04:39Z',
+            'name': 'nature.com',
+            'locators': [{
+                'id': '902b2e44-c0d3-47a5-8e4a-1e1ce1daaf3f',
+                'domainName': 'www.nature.com'
+            }]
+        }
+    ],
+    'successfulMutations': [],
+    'failedMutations': [],
+    'pendingMutations': [],
+    'loadedAt': 'Mon Dec 06 2021 09:57:46 GMT+0000 (Greenwich Mean Time)',
+    'url': 'https://folio-snapshot-okapi.dev.folio.org/erm/platforms?perPage=100&sort=name%3Basc&stats=true',
+    'headers': 'Headers(undefined) {"content-type" => "application/json;charset=UTF-8"}',
+    'httpStatus': 200,
+    'other': {
+        'pageSize': 100,
+        'page': 1,
+        'totalPages': 1,
+        'meta': '{}',
+        'totalRecords': 40,
+        'total': 40
+    },
+    'resource': 'platforms',
+    'module': '@folio/agreements',
+    'throwErrors': true
+};
+
+const mutator = {
+    'platforms': '{DELETE: ƒ DELETE() {}, POST: ƒ POST() {}, PUT: ƒ P…}',
+    'query': '{replace: ƒ replace() {}, update: ƒ update() {}}',
+    'resultCount': '{replace: ƒ replace() {}, update: ƒ update() {}}'
+};
+
+export {
+    history,
+    location,
+    match,
+    platforms,
+    mutator
+};
