4c95b9ae80c83aee726112723118178d09025b3f
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-02-25 15:00:35 +0000

    test: NoValue change
    Test tweak to reflect change in NoValue

--- src/components/EResourceSections/PackageInfo/PackageInfo.test.js
+++ src/components/EResourceSections/PackageInfo/PackageInfo.test.js
@@ -720,15 +720,15 @@ describe('PackageInfo', () => {
     });
 
     test('renders the expcected provider', async () => {
-      await KeyValue('Provider').has({ value: '-' });
+      await KeyValue('Provider').has({ value: 'No value set-' });
     });
 
     test('renders the expcected source', async () => {
-      await KeyValue('Source').has({ value: '-' });
+      await KeyValue('Source').has({ value: 'No value set--' });
     });
 
     test('renders the expcected reference', async () => {
-      await KeyValue('Reference').has({ value: '-' });
+      await KeyValue('Reference').has({ value: 'No value set--' });
     });
   });
 });
