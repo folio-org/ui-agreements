f3b4dd9fb3629175c55d78dc200fffcf41cf97c2
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-05-10 14:51:35 +0100

    Extend tests for SourceTitleIdentifierField and DestinationTitleIdentifierField

--- src/components/IdentifierReassignmentForm/SourceTitleIdentifierField/SourceTitleIdentifierField.test.js
+++ src/components/IdentifierReassignmentForm/SourceTitleIdentifierField/SourceTitleIdentifierField.test.js
@@ -36,6 +36,21 @@ describe('SourceTitleIdentifierField', () => {
     expect(getByTestId('sourceTitleCard')).toBeInTheDocument();
   });
 
+  test('renders the Title text', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Title')).toBeInTheDocument();
+  });
+
+  test('renders the expected text when no title is selected', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('No source title selected')).toBeInTheDocument();
+  });
+
+  test('renders the expected text to select source title', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Select the source title for the identifier(s)')).toBeInTheDocument();
+  });
+
   test('renders the submit button', async () => {
     await Button('Submit').exists();
   });
