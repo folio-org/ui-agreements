59df103d0edd4f4514baa5f3a2d5f32f3395625d
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-06-21 16:43:38 +0100

    refactor: useAgreementsRefdata
    Refactored useAgreementsRefdata to make slightly more useful down the road

--- src/hooks/useAgreementsRefdata.js
+++ src/hooks/useAgreementsRefdata.js
@@ -1,12 +1,43 @@
-import { refdataOptions, useRefdata } from '@k-int/stripes-kint-components';
+import orderBy from 'lodash/orderBy';
+import { useRefdata, refdataOptions } from '@k-int/stripes-kint-components';
+
 import { REFDATA_ENDPOINT } from '../constants/endpoints';
 
-const useAgreementsRefdata = ({ desc, options = {} }) => {
-  return useRefdata({
+const sortRefdata = (refdataData) => {
+  const sortedRefData = [...(refdataData ?? [])];
+  if (Array.isArray(refdataData)) {
+    for (let index = 0; index < refdataData.length; index++) {
+      // Order the refdata values in ascending order i.e Author, DFG, Library
+      sortedRefData[index].values = orderBy(
+        refdataData[index].values,
+        'value',
+        'asc'
+      );
+    }
+  }
+
+  return sortedRefData;
+};
+
+const useAgreementsRefdata = ({ desc, options = {}, returnQueryObject = false } = {}) => {
+  const refdata = useRefdata({
     desc,
     endpoint: REFDATA_ENDPOINT,
-    options: { ...refdataOptions, sort: [{ path: 'desc' }], ...options }
+    options: { ...refdataOptions, sort: [{ path: 'desc' }], ...options },
+    returnQueryObject
   });
+
+  if (returnQueryObject) {
+    const { data: refdataData, ...rest } = refdata;
+    const sortedData = sortRefdata(refdataData);
+
+    return ({
+      data: sortedData,
+      ...rest
+    });
+  }
+
+  return sortRefdata(refdata);
 };
 
 export default useAgreementsRefdata;
