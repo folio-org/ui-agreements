f22e283a323b44c2ebe070b34db6ce1f606d7da9
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-06-21 13:33:40 +0100

    refactor: Refactored pick list value settings to use EditableRefdataList
    Refactored picklist settings to shiny new component in the hopes it would also fix a caching issue, but that does not seem to be the case
    
    ERM-2214, ERM-2215

--- src/hooks/useAgreementsRefdata.js
+++ src/hooks/useAgreementsRefdata.js
@@ -1,12 +1,31 @@
-import { refdataOptions, useRefdata } from '@k-int/stripes-kint-components';
+import orderBy from 'lodash/orderBy';
+import { useRefdata, refdataOptions } from '@k-int/stripes-kint-components';
+
 import { REFDATA_ENDPOINT } from '../constants/endpoints';
 
-const useAgreementsRefdata = ({ desc, options = {} }) => {
-  return useRefdata({
+const useAgreementsRefdata = ({ desc, options = {} } = {}) => {
+  const refdata = useRefdata({
     desc,
     endpoint: REFDATA_ENDPOINT,
-    options: { ...refdataOptions, sort: [{ path: 'desc' }], ...options }
+    options: { ...refdataOptions, sort: [{ path: 'desc' }], ...options },
   });
+
+  const sortedRefData = [...refdata];
+
+  // This may want to be refactored later without the reliance on orderBy
+  // If the refdata reteched is in an array, sort the values
+  if (Array.isArray(refdata)) {
+    for (let index = 0; index < refdata.length; index++) {
+      // Order the refdata values in ascending order i.e Author, DFG, Library
+      sortedRefData[index].values = orderBy(
+        refdata[index].values,
+        'value',
+        'asc'
+      );
+    }
+  }
+
+  return sortedRefData;
 };
 
 export default useAgreementsRefdata;
