f0311aef78a06517529d50831de9afd9841edac1
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-07-12 11:58:48 +0100

    Remove redundant duplicate utility functions in ui-agreements

--- src/components/utilities/getResourceIdentifier.js
+++
@@ -1,7 +0,0 @@
-export default (eresource = {}, type) => {
-  const identifiers = eresource?.identifiers || [];
-  // find first identifiers value of namespace 'type'
-  const value = identifiers.find(i => i.identifier?.ns.value === type)?.identifier?.value;
-
-  return value;
-};
