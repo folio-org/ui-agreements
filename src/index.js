7897631ccbb5dcb0689ad0266d07fca8f42bfabc
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-09-14 15:26:48 +0100

    refactor: Index.js
    Refactor index.js to functional
    
    refs ERM-2330

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Switch } from 'react-router-dom';
@@ -51,42 +51,24 @@ import OpenBasketButton from './components/OpenBasketButton';
 
 import Settings from './settings';
 
-class App extends React.Component {
-  static eventHandler(event, _s, data) {
-    if (event === 'LOAD_STRIPES_REGISTRY') {
-      // Data should contain Registry singleton:
-      setUpRegistry(data);
-    }
+const App = (props) => {
+  // Destructure important props
+  const {
+    actAs,
+    history,
+    location: { pathname },
+    match: { path },
+    stripes
+  } = props;
 
-    return null;
-  }
+  const [showKeyboardShortcutsModal, setShowKeyboardShortcutsModal] = useState(false);
 
-  static propTypes = {
-    history: PropTypes.object,
-    location: PropTypes.shape({
-      pathname: PropTypes.string
-    }),
-    match: PropTypes.object.isRequired,
-    actAs: PropTypes.string.isRequired,
-    stripes: PropTypes.object.isRequired,
-  }
-
-  state = {
-    showKeyboardShortcutsModal: false,
-  };
-
-  changeKeyboardShortcutsModal = (modalState) => {
-    this.setState({ showKeyboardShortcutsModal: modalState });
-  };
-
-  shortcutModalToggle(handleToggle) {
+  const shortcutModalToggle = (handleToggle) => {
     handleToggle();
-    this.changeKeyboardShortcutsModal(true);
-  }
+    setShowKeyboardShortcutsModal(true);
+  };
 
-  searchInput = () => {
-    const { pathname } = this.props.location;
-
+  const searchInput = () => {
     if (pathname.search('/erm/agreements') === 0) {
       return 'input-agreement-search';
     } else if (pathname.search('/erm/eresources') === 0) {
@@ -98,118 +80,134 @@ class App extends React.Component {
     } else {
       return undefined;
     }
-  }
+  };
 
-  focusSearchField = () => {
-    const { history, stripes } = this.props;
-    const el = document.getElementById(this.searchInput());
+  const focusSearchField = () => {
+    const el = document.getElementById(searchInput());
     if (el) {
       el.focus();
     } else {
       history.push(stripes.home);
     }
-  }
+  };
 
-  shortcuts = [
+  const shortcuts = [
     {
       name: 'search',
-      handler: this.focusSearchField
+      handler: focusSearchField
     },
     {
       name: 'openShortcutModal',
-      handler: this.changeKeyboardShortcutsModal
+      handler: setShowKeyboardShortcutsModal
     },
   ];
 
-  render() {
-    const { actAs, match: { path } } = this.props;
-
-    if (actAs === 'settings') {
-      return <Settings {...this.props} />;
-    }
-
-    return (
-      <>
-        <CommandList commands={defaultKeyboardShortcuts}>
-          <HasCommand
-            commands={this.shortcuts}
-            isWithinScope={checkScope}
-            scope={document.body}
-          >
-            <AppContextMenu>
-              {(handleToggle) => (
-                <NavList>
-                  <NavListSection>
-                    <NavListItem
-                      id="keyboard-shortcuts-item"
-                      onClick={() => { this.shortcutModalToggle(handleToggle); }}
-                    >
-                      <FormattedMessage id="ui-agreements.appMenu.keyboardShortcuts" />
-                    </NavListItem>
-                  </NavListSection>
-                </NavList>
-              )}
-            </AppContextMenu>
-            <div className={css.container}>
-              <IfEResourcesEnabled>
-                <Layout className={`${css.header} display-flex justify-end full padding-top-gutter padding-start-gutter padding-end-gutter`}>
-                  <OpenBasketButton />
-                </Layout>
-              </IfEResourcesEnabled>
-              <div className={css.body}>
-                <Switch>
-                  <Route component={AgreementCreateRoute} path={`${path}/agreements/create`} />
-                  <Route component={AgreementEditRoute} path={`${path}/agreements/:id/edit`} />
-                  <Route component={AgreementLineCreateRoute} path={`${path}/agreements/:agreementId/line/create`} />
-                  <Route component={AgreementLineEditRoute} path={`${path}/agreements/:agreementId/line/:lineId/edit`} />
-                  <Route component={AgreementsRoute} path={`${path}/agreements/:id?`}>
-                    <Switch>
-                      <Route component={AgreementLineViewRoute} path={`${path}/agreements/:agreementId/line/:lineId`} />
-                      <Route component={AgreementViewRoute} path={`${path}/agreements/:id`} />
-                    </Switch>
-                  </Route>
-
-                  <Route component={AgreementLineEditRoute} path={`${path}/agreementLines/:lineId/agreement/:agreementId/edit`} />
-                  <Route component={AgreementLinesRoute} path={`${path}/agreementLines/:id?`}>
-                    <Route component={AgreementLineViewRoute} path={`${path}/agreementLines/:lineId/agreement/:agreementId`} />
-                  </Route>
-
-                  <Route component={EResourceEditRoute} path={`${path}/eresources/:id/edit`} />
-                  <Route component={EResourcesRoute} path={`${path}/eresources/:id?`}>
-                    <Route component={EResourceViewRoute} path={`${path}/eresources/:id`} />
-                  </Route>
-
-                  <Route component={NoteCreateRoute} path={`${path}/notes/create`} />
-                  <Route component={NoteEditRoute} path={`${path}/notes/:id/edit`} />
-                  <Route component={NoteViewRoute} path={`${path}/notes/:id`} />
-
-                  <Route component={PlatformEditRoute} path={`${path}/platforms/:id/edit`} />
-                  <Route component={UrlCustomizerCreateRoute} path={`${path}/platforms/:platformId/urlcustomizer/create`} />
-                  <Route component={UrlCustomizerEditRoute} path={`${path}/platforms/:platformId/urlcustomizer/:templateId/edit`} />
-                  <Route component={PlatformsRoute} path={`${path}/platforms/:id?`}>
-                    <Switch>
-                      <Route component={UrlCustomizerViewRoute} path={`${path}/platforms/:platformId/urlcustomizer/:templateId`} />
-                      <Route component={PlatformViewRoute} path={`${path}/platforms/:id`} />
-                    </Switch>
-                  </Route>
-
-                  <Route component={BasketRoute} path={`${path}/basket`} />
-                </Switch>
-              </div>
-            </div>
-          </HasCommand>
-        </CommandList>
-        {this.state.showKeyboardShortcutsModal && (
-          <KeyboardShortcutsModal
-            allCommands={defaultKeyboardShortcuts}
-            onClose={() => { this.changeKeyboardShortcutsModal(false); }}
-            open
-          />
-        )}
-      </>
-    );
+  if (actAs === 'settings') {
+    return <Settings {...props} />;
   }
-}
+
+  return (
+    <>
+      <CommandList commands={defaultKeyboardShortcuts}>
+        <HasCommand
+          commands={shortcuts}
+          isWithinScope={checkScope}
+          scope={document.body}
+        >
+          <AppContextMenu>
+            {(handleToggle) => (
+              <NavList>
+                <NavListSection>
+                  <NavListItem
+                    id="keyboard-shortcuts-item"
+                    onClick={() => { shortcutModalToggle(handleToggle); }}
+                  >
+                    <FormattedMessage id="ui-agreements.appMenu.keyboardShortcuts" />
+                  </NavListItem>
+                </NavListSection>
+              </NavList>
+            )}
+          </AppContextMenu>
+          <div className={css.container}>
+            <IfEResourcesEnabled>
+              <Layout className={`${css.header} display-flex full padding-top-gutter padding-start-gutter padding-end-gutter`}>
+                <div /> {/* Empty start item so we can get centre/end aligned */}
+                <div />
+                <OpenBasketButton />
+              </Layout>
+            </IfEResourcesEnabled>
+            <div className={css.body}>
+              <Switch>
+                <Route component={AgreementCreateRoute} path={`${path}/agreements/create`} />
+                <Route component={AgreementEditRoute} path={`${path}/agreements/:id/edit`} />
+                <Route component={AgreementLineCreateRoute} path={`${path}/agreements/:agreementId/line/create`} />
+                <Route component={AgreementLineEditRoute} path={`${path}/agreements/:agreementId/line/:lineId/edit`} />
+                <Route component={AgreementsRoute} path={`${path}/agreements/:id?`}>
+                  <Switch>
+                    <Route component={AgreementLineViewRoute} path={`${path}/agreements/:agreementId/line/:lineId`} />
+                    <Route component={AgreementViewRoute} path={`${path}/agreements/:id`} />
+                  </Switch>
+                </Route>
+
+                <Route component={AgreementLineEditRoute} path={`${path}/agreementLines/:lineId/agreement/:agreementId/edit`} />
+                <Route component={AgreementLinesRoute} path={`${path}/agreementLines/:id?`}>
+                  <Route component={AgreementLineViewRoute} path={`${path}/agreementLines/:lineId/agreement/:agreementId`} />
+                </Route>
+
+                <Route component={EResourceEditRoute} path={`${path}/eresources/:id/edit`} />
+                <Route component={EResourcesRoute} path={`${path}/eresources/:id?`}>
+                  <Route component={EResourceViewRoute} path={`${path}/eresources/:id`} />
+                </Route>
+
+                <Route component={NoteCreateRoute} path={`${path}/notes/create`} />
+                <Route component={NoteEditRoute} path={`${path}/notes/:id/edit`} />
+                <Route component={NoteViewRoute} path={`${path}/notes/:id`} />
+
+                <Route component={PlatformEditRoute} path={`${path}/platforms/:id/edit`} />
+                <Route component={UrlCustomizerCreateRoute} path={`${path}/platforms/:platformId/urlcustomizer/create`} />
+                <Route component={UrlCustomizerEditRoute} path={`${path}/platforms/:platformId/urlcustomizer/:templateId/edit`} />
+                <Route component={PlatformsRoute} path={`${path}/platforms/:id?`}>
+                  <Switch>
+                    <Route component={UrlCustomizerViewRoute} path={`${path}/platforms/:platformId/urlcustomizer/:templateId`} />
+                    <Route component={PlatformViewRoute} path={`${path}/platforms/:id`} />
+                  </Switch>
+                </Route>
+
+                <Route component={BasketRoute} path={`${path}/basket`} />
+              </Switch>
+            </div>
+          </div>
+        </HasCommand>
+      </CommandList>
+      {showKeyboardShortcutsModal && (
+        <KeyboardShortcutsModal
+          allCommands={defaultKeyboardShortcuts}
+          onClose={() => { setShowKeyboardShortcutsModal(false); }}
+          open
+        />
+      )}
+    </>
+  );
+};
+
+App.eventHandler = (event, _s, data) => {
+  if (event === 'LOAD_STRIPES_REGISTRY') {
+    // Data should contain Registry singleton:
+    setUpRegistry(data);
+  }
+
+  return null;
+};
+
+App.propTypes = {
+  history: PropTypes.object,
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }),
+  match: PropTypes.object.isRequired,
+  actAs: PropTypes.string.isRequired,
+  stripes: PropTypes.object.isRequired,
+}; 
 
 export default App;
 export { default as Agreements } from './components/views/Agreements';
