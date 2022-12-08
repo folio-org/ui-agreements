8f4ae504ecfc8839f3ec94748075473f3e9da900
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-12-08 16:19:15 +0000

    feat: useBasket
    Added useBasketHook to capture basket interactions, powered by useBasketStore, a Zustand store under the hood
    
    ERM-2183

---
+++ src/hooks/useBasketStore.js
@@ -0,0 +1,33 @@
+import create from 'zustand';
+// We can easily persist this in future, just by uncommenting the code below
+/*
+import { persist } from 'zustand/middleware';
+*/
+
+const useBasketStore = create(
+  /* persist( */
+  (set, get) => ({
+    basketStore: {},
+    addToBasket: (item) => set((state) => {
+      if (state.basketStore[item.id] || !item.id) {
+        return state;
+      }
+
+      return { ...state, basketStore: { ...state.basketStore, [item.id]: item } };
+    }),
+    removeFromBasket: (item) => set((state) => {
+      if (!state.basketStore[item.id] || !item.id) {
+        return state;
+      }
+
+      const { [item.id]: _itemToRemove, ...newBasket } = state.basketStore;
+      return { ...state, basketStore: newBasket };
+    }),
+  })/* ,
+    {
+      name: '@folio/agreements:basket'
+    }
+  ) */
+);
+
+export default useBasketStore;
