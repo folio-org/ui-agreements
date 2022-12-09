9c37586363140c36a1cc11dfd744f58cdab5a749
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-12-09 13:11:52 +0000

    feat: useBasketStore
    BasketStore now persists between refreshes
    
    ERM-2183

--- src/hooks/useBasketStore.js
+++ src/hooks/useBasketStore.js
@@ -1,33 +1,30 @@
 import create from 'zustand';
-// We can easily persist this in future, just by uncommenting the code below
-/*
 import { persist } from 'zustand/middleware';
-*/
 
 const useBasketStore = create(
-  /* persist( */
-  (set) => ({
-    basketStore: {},
-    addToBasket: (item) => set((state) => {
-      if (state.basketStore[item.id] || !item.id) {
-        return state;
-      }
+  persist(
+    (set) => ({
+      basketStore: {},
+      addToBasket: (item) => set((state) => {
+        if (state.basketStore[item.id] || !item.id) {
+          return state;
+        }
 
-      return { ...state, basketStore: { ...state.basketStore, [item.id]: item } };
-    }),
-    removeFromBasket: (item) => set((state) => {
-      if (!state.basketStore[item.id] || !item.id) {
-        return state;
-      }
+        return { ...state, basketStore: { ...state.basketStore, [item.id]: item } };
+      }),
+      removeFromBasket: (item) => set((state) => {
+        if (!state.basketStore[item.id] || !item.id) {
+          return state;
+        }
 
-      const { [item.id]: _itemToRemove, ...newBasket } = state.basketStore;
-      return { ...state, basketStore: newBasket };
+        const { [item.id]: _itemToRemove, ...newBasket } = state.basketStore;
+        return { ...state, basketStore: newBasket };
+      }),
     }),
-  })/* ,
     {
       name: '@folio/agreements:basket'
     }
-  ) */
+  )
 );
 
 export default useBasketStore;
