16981f8eb0aaa237a76ed4b5ba412c6a72eef066
MonireRasouli <monireh.rasouli@k-int.co.uk>
2023-03-01 11:17:26 +0000

    erm-6212, on saving the agreement the content type is removed from the list of content types

--- src/components/AgreementSections/ContentTypesFieldArray/ContentTypesFieldArray.js
+++ src/components/AgreementSections/ContentTypesFieldArray/ContentTypesFieldArray.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState, useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
 import { getRefdataValuesByDesc, requiredValidator } from '@folio/stripes-erm-components';
@@ -13,6 +14,25 @@ import {
 import { useKiwtFieldArray } from '@k-int/stripes-kint-components';
 import { useAgreementsRefdata } from '../../../hooks';
 
+// custom hook that holds any required value (props/state) from  the previous render cycle via a ref.
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+}
+
+// Utility function to check if two arrays of scalars contain the same items (Order does not count)
+const arraysAreEqual = (a, b) => {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  return a.every(element => {
+    return b.includes(element);
+  });
+};
 
 const [
   AGREEMENT_CONTENT_TYPE
@@ -21,7 +41,7 @@ const [
 ];
 
 const ContentTypesFieldArray = ({
-  fields: { name }
+  fields: { name },
 }) => {
   const refdata = useAgreementsRefdata({
     desc: [
@@ -29,37 +49,71 @@ const ContentTypesFieldArray = ({
     ]
   });
   const contentTypeValues = getRefdataValuesByDesc(refdata, AGREEMENT_CONTENT_TYPE);
+  const agreementContentType = contentTypeValues.map(ct => ({ value: ct.value, label: ct.label }));
   const { items, onAddField, onDeleteField } = useKiwtFieldArray(name);
+  const [contentTypeInUse, setContentTypeInUse] = useState([]);
+  const contentTypeRefs = useRef([]);
+  contentTypeRefs.current = [];
 
-  const renderContentTypes = () => {
-    return items.map((act, index) => (
-      <div
-        key={act + index}
-        data-testid={`contentTypesFieldArray[${index}]`}
-      >
-        <Row>
-          <Col xs={11}>
-            <Field
-              component={Select}
-              dataOptions={contentTypeValues}
-              id="content-type-field-array"
-              name={`${name}[${index}].contentType.value`}
-              placeholder=" "
-              validate={requiredValidator}
-            />
-          </Col>
-          <Col xs={1}>
-            <IconButton
-              ariaLabel={`remove-content-types[${index}]-${act.name}`}
-              data-test-delete-field-button
-              icon="trash"
-              onClick={() => onDeleteField(index, act)}
-            />
-          </Col>
-        </Row>
-      </div>
-    ));
+  const itemsLength = items.length;
+  const previousCount = usePrevious(itemsLength);
+
+  useEffect(() => {
+    const newContentTypeInUse = items.map(i => i?.contentType?.value).filter(x => !!x);
+    if (!arraysAreEqual(contentTypeInUse, newContentTypeInUse)) {
+      setContentTypeInUse(newContentTypeInUse);
+    }
+  }, [items, contentTypeInUse]);
+
+  useEffect(() => {
+    // the second conditional is checking if the current field to be focused is the default content type field.
+    if (contentTypeRefs.current.length > 1 || (contentTypeRefs.current.length > 0 && previousCount - itemsLength === 1)) {
+      contentTypeRefs.current[itemsLength - 1].focus();
+    }
+  }, [previousCount, itemsLength]);
+
+  // callback ref that adds the individual ref to a field into the contentTypeRefs array
+  const addToRefs = (node) => {
+    if (node && !contentTypeRefs.current.includes(node)) {
+      contentTypeRefs.current.push(node);
+    }
   };
+
+  const renderContentTypes = () => (
+    items.map((act, index) => {
+      const dataOptions = agreementContentType.filter(ct => !contentTypeInUse.includes(ct.value) || ct.value === act.contentType?.value);
+      return (
+        <div
+          key={act + index}
+          data-testid={`contentTypesFieldArray[${index}]`}
+        >
+          <Row>
+            <Col xs={11}>
+              <Field
+                component={Select}
+                dataOptions={dataOptions}
+                id="content-type-field-array"
+                index={index}
+                inputRef={addToRefs}
+                name={`${name}[${index}].contentType.value`}
+                placeholder=" "
+                validate={requiredValidator}
+              />
+            </Col>
+            <Col xs={1}>
+              <IconButton
+                ariaLabel={`remove-content-types[${index}]-${act.name}`}
+                data-test-delete-field-button
+                icon="trash"
+                onClick={() => onDeleteField(index, act)}
+              />
+            </Col>
+          </Row>
+        </div>
+      );
+    })
+  );
+
   return (
     <div>
       <Headline>
