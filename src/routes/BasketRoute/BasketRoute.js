01288fc448f128a95c83e805cd8277543cec7c07
Jack Golding <jack.golding@k-int.com>
2023-05-23 10:26:53 +0100

    feat: Add to existing agreement
    Agreement lines are now added based on package to the selected agreement

--- src/routes/BasketRoute/BasketRoute.js
+++ src/routes/BasketRoute/BasketRoute.js
@@ -1,40 +1,67 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import { useMutation } from 'react-query';
+import { useLocation } from 'react-router-dom';
 
 import { useBatchedFetch } from '@folio/stripes-erm-components';
+import { useOkapiKy } from '@folio/stripes/core';
 
 import { useBasket } from '../../hooks';
-import { AGREEMENTS_ENDPOINT } from '../../constants/endpoints';
+import {
+  AGREEMENTS_ENDPOINT,
+  AGREEMENT_ENDPOINT,
+} from '../../constants/endpoints';
 
 import View from '../../components/views/Basket';
 import { urls } from '../../components/utilities';
 
-const BasketRoute = ({
-  history,
-}) => {
+const BasketRoute = ({ history }) => {
   const { basket, removeFromBasket } = useBasket();
+  const location = useLocation();
+  const ky = useOkapiKy();
 
   // AGREEMENTS BATCHED FETCH
-  const {
-    results: openAgreements,
-  } = useBatchedFetch({
-    batchParams:  {
+  const { results: openAgreements } = useBatchedFetch({
+    batchParams: {
       filters: [
         {
           path: 'agreementStatus.value',
-          values: ['active', 'draft', 'in_negotiation', 'requested']
-        }
+          values: ['active', 'draft', 'in_negotiation', 'requested'],
+        },
       ],
-      sort: [
-        { path: 'name' },
-      ],
+      sort: [{ path: 'name' }],
     },
     nsArray: ['ERM', 'Agreements', AGREEMENTS_ENDPOINT, 'BasketRoute'],
-    path: AGREEMENTS_ENDPOINT
+    path: AGREEMENTS_ENDPOINT,
   });
 
+  const { mutateAsync: putAgreement } = useMutation(
+    [
+      AGREEMENTS_ENDPOINT,
+      'ui-agreements',
+      'AgreementEditRoute',
+      'editAgreement',
+    ],
+    ({ payload, agreementId }) => ky
+      .put(AGREEMENT_ENDPOINT(agreementId), {
+        json: payload,
+      })
+      .json()
+      .then(() => {
+        history.push(`${urls.agreementView(agreementId)}${location.search}`);
+      })
+  );
+
   const handleAddToExistingAgreement = (addFromBasket, agreementId) => {
-    history.push(`${urls.agreementEdit(agreementId)}?addFromBasket=${addFromBasket}`);
+    const submitValues = {
+      payload: {
+        items: addFromBasket
+          .split(',')
+          .map((index) => ({ resource: basket[parseInt(index, 10)] }))
+          .filter((line) => line.resource),
+      },
+      agreementId,
+    };
+    putAgreement(submitValues);
   };
 
   const handleAddToNewAgreement = (addFromBasket) => {
