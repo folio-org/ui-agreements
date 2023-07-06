a7c1ff09cc8e6df94b1ab07cd54835a4922a90a0
Jack Golding <jack.golding@k-int.com>
2023-07-06 11:32:33 +0100

    refactor: Functional component
    Refactore PCIInfo from class to functional component

--- src/components/EResourceSections/PCIInfo/PCIInfo.js
+++ src/components/EResourceSections/PCIInfo/PCIInfo.js
@@ -15,18 +15,22 @@ import {
 import { resourceClasses } from '../../../constants';
 import AddToBasketButton from '../../AddToBasketButton';
 
-export default class PCIInfo extends React.Component {
-  static propTypes = {
-    pci: PropTypes.shape({
-      accessEnd: PropTypes.string,
-      accessStart: PropTypes.string,
-      dateCreated: PropTypes.string,
-      lastUpdated: PropTypes.string,
-      name: PropTypes.string,
-    }).isRequired,
-  }
+const propTypes = {
+  pci: PropTypes.shape({
+    id: PropTypes.string,
+    accessEnd: PropTypes.string,
+    accessStart: PropTypes.string,
+    dateCreated: PropTypes.string,
+    lastUpdated: PropTypes.string,
+    name: PropTypes.string,
+    pti: PropTypes.shape({
+      url: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
-  renderAddTitleToBasketButton = (pci) => {
+const PCIInfo = ({ pci }) => {
+  const renderAddTitleToBasketButton = () => {
     const { name: packageName } = pci;
     const entitlementOption = {
       class: resourceClasses.PCI,
@@ -36,76 +40,94 @@ export default class PCIInfo extends React.Component {
     };
     return (
       <AddToBasketButton
-        addButtonTooltipText={<FormattedMessage id="ui-agreements.eresources.addTitleButtonTooltip" values={{ packageName }} />}
-        addLabel={<FormattedMessage id="ui-agreements.eresources.addTitleToBasket" />}
+        addButtonTooltipText={
+          <FormattedMessage
+            id="ui-agreements.eresources.addTitleButtonTooltip"
+            values={{ packageName }}
+          />
+        }
+        addLabel={
+          <FormattedMessage id="ui-agreements.eresources.addTitleToBasket" />
+        }
         item={entitlementOption}
-        removeButtonTooltipText={<FormattedMessage id="ui-agreements.eresources.removeTitleButtonTooltip" values={{ packageName }} />}
+        removeButtonTooltipText={
+          <FormattedMessage
+            id="ui-agreements.eresources.removeTitleButtonTooltip"
+            values={{ packageName }}
+          />
+        }
       />
     );
-  }
+  };
 
-  renderUrl = (pci) => {
+  const renderUrl = () => {
     const url = pci?.pti?.url;
     return url ? (
-      <a
-        href={url}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
+      <a href={url} rel="noopener noreferrer" target="_blank">
         {url}
       </a>
-    ) : <NoValue />;
-  }
-
-  render() {
-    const { pci } = this.props;
-    return (
-      <div id="pci-info">
-        <Row>
-          <Col xs={12}>
-            <Headline
-              size="xx-large"
-              tag="h2"
-            >
-              {pci.name}
-            </Headline>
-          </Col>
-        </Row>
-        <MetaSection
-          contentId="pciInfoRecordMetaContent"
-          createdDate={pci.dateCreated}
-          hideSource
-          id="pciInfoRecordMeta"
-          lastUpdatedDate={pci.lastUpdated}
-        />
-        <Layout className="display-flex justified">
-          <Headline size="large" tag="h3">
-            <FormattedMessage id="ui-agreements.eresources.titleAvailability" />
-          </Headline>
-          <Layout>
-            {this.renderAddTitleToBasketButton(pci)}
-          </Layout>
-        </Layout>
-        <Row>
-          <Col xs={12}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.eresources.titleOnPlatformURL" />}>
-              {this.renderUrl(pci)}
-            </KeyValue>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={3}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.eresources.accessibleFrom" />}>
-              {pci?.accessStart ?? <NoValue />}
-            </KeyValue>
-          </Col>
-          <Col xs={3}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.eresources.accessibleUntil" />}>
-              {pci?.accessEnd ?? <NoValue />}
-            </KeyValue>
-          </Col>
-        </Row>
-      </div>
+    ) : (
+      <NoValue />
     );
-  }
-}
+  };
+
+  return (
+    <div id="pci-info">
+      <Row>
+        <Col xs={12}>
+          <Headline size="xx-large" tag="h2">
+            {pci.name}
+          </Headline>
+        </Col>
+      </Row>
+      <MetaSection
+        contentId="pciInfoRecordMetaContent"
+        createdDate={pci.dateCreated}
+        hideSource
+        id="pciInfoRecordMeta"
+        lastUpdatedDate={pci.lastUpdated}
+      />
+      <Layout className="display-flex justified">
+        <Headline size="large" tag="h3">
+          <FormattedMessage id="ui-agreements.eresources.titleAvailability" />
+        </Headline>
+        <Layout>{renderAddTitleToBasketButton(pci)}</Layout>
+      </Layout>
+      <Row>
+        <Col xs={12}>
+          <KeyValue
+            label={
+              <FormattedMessage id="ui-agreements.eresources.titleOnPlatformURL" />
+            }
+          >
+            {renderUrl(pci)}
+          </KeyValue>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <KeyValue
+            label={
+              <FormattedMessage id="ui-agreements.eresources.accessibleFrom" />
+            }
+          >
+            {pci?.accessStart ?? <NoValue />}
+          </KeyValue>
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={
+              <FormattedMessage id="ui-agreements.eresources.accessibleUntil" />
+            }
+          >
+            {pci?.accessEnd ?? <NoValue />}
+          </KeyValue>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+PCIInfo.propTypes = propTypes;
+
+export default PCIInfo;
