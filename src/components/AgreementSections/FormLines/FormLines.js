43b165365450abd7a38fd9bde95ad87d2c6987a3
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-08-03 17:11:44 +0200

    ERM-2979 Add agreement line summary information and navigation link to Agreement edit view
    * convert FormLines to functional component

--- src/components/AgreementSections/FormLines/FormLines.js
+++ src/components/AgreementSections/FormLines/FormLines.js
@@ -5,61 +5,57 @@ import { Link } from 'react-router-dom';
 import { Accordion, Badge, Row } from '@folio/stripes/components';
 import { urls } from '../../utilities';
 
-export default class FormLines extends React.Component {
-  static propTypes = {
-    agreementId: PropTypes.string,
-    agreementLineCount: PropTypes.number,
-    onToggle: PropTypes.func,
-    open: PropTypes.bool,
-  };
-
-  renderBadge = () => {
-    const { agreementLineCount } = this.props;
+const FormLines = ({ agreementId, agreementLineCount, onToggle, open }) => {
+  const renderBadge = () => {
     const count = agreementLineCount ?? 0;
     return <Badge>{count}</Badge>;
-  }
+  };
 
-  render() {
-    const { agreementId, agreementLineCount, onToggle, open } = this.props;
+  const containerStyle = { marginLeft: '1rem' };
+  const rowStyle = { marginBottom: '1rem' };
 
-    const containerStyle = { marginLeft: '1rem' };
-    const rowStyle = { marginBottom: '1rem' };
+  return (
+    <Accordion
+      displayWhenClosed={renderBadge()}
+      displayWhenOpen={renderBadge()}
+      id="formLines"
+      label={<FormattedMessage id="ui-agreements.agreementLines" />}
+      onToggle={onToggle}
+      open={open}
+    >
+      <div style={containerStyle}>
+        {agreementId && agreementLineCount > 0 ? (
+          <>
+            <Row style={rowStyle}>
+              <FormattedMessage id="ui-agreements.agreements.agreementLineCount" values={{ count: agreementLineCount }} />
+            </Row>
+            <Row style={rowStyle}>
+              <Link
+                data-test-agreement-lines-link
+                target="_blank"
+                to={`${urls.agreementLines()}?filters=agreement.${agreementId}`}
+              >
+                <strong>
+                  <FormattedMessage id="ui-agreements.agreementLines.viewInSearch" />
+                  {' '}
+                  <FormattedMessage id="ui-agreements.open.linkInNewTab" />
+                </strong>
+              </Link>
+            </Row>
+          </>
+        ) : (
+          <FormattedMessage id="ui-agreements.agreementLines.noLines" />
+        )}
+      </div>
+    </Accordion>
+  );
+};
 
-    return (
-      <Accordion
-        displayWhenClosed={this.renderBadge()}
-        displayWhenOpen={this.renderBadge()}
-        id="formLines"
-        label={<FormattedMessage id="ui-agreements.agreementLines" />}
-        onToggle={onToggle}
-        open={open}
-      >
-        <div style={containerStyle}>
-          {(agreementId && agreementLineCount > 0) ?
-            <>
-              <Row style={rowStyle}>
-                <FormattedMessage id="ui-agreements.agreements.agreementLineCount" values={{ count: agreementLineCount }} />
-              </Row>
-              <Row style={rowStyle}>
-                <Link
-                  data-test-agreement-lines-link
-                  target="_blank"
-                  to={`${urls.agreementLines()}?filters=agreement.${agreementId}`}
-                >
-                  <strong>
-                    <FormattedMessage id="ui-agreements.agreementLines.viewInSearch" />
-                    {' '}
-                    <FormattedMessage id="ui-agreements.open.linkInNewTab" />
-                  </strong>
-                </Link>
-              </Row>
-            </>
-            :
-            <FormattedMessage id="ui-agreements.agreementLines.noLines" />
-          }
-        </div>
-      </Accordion>
+FormLines.propTypes = {
+  agreementId: PropTypes.string,
+  agreementLineCount: PropTypes.number,
+  onToggle: PropTypes.func,
+  open: PropTypes.bool,
+};
 
-    );
-  }
-}
+export default FormLines;
