d052ce71d38181cfb0b05b21c8883116d2bed7ac
Claudia Malzer <42971992+CalamityC@users.noreply.github.com>
2023-09-25 15:10:24 +0200

    ERM-3038 Creating adding an eHolding resource to a new agreement fails (#1248)
    * ERM-3038 Creating adding an eHolding resource to a new agreement fails
    
    * add workaround to AgreementCreateRoute
    
    * ERM-3038 Creating adding an eHolding resource to a new agreement fails
    
    * add comments
    
    * refactor: Form wrangling
    
    Added item to initial form values instead of glueing it in on submit
    
    ERM-3038
    
    * chore: Linting
    
    ---------
    
    Co-authored-by: Ethan Freestone <ethan.freestone@k-int.co.uk>

--- src/components/views/Packages/Packages.js
+++ src/components/views/Packages/Packages.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { useLocalStorage, writeStorage } from '@rehooks/local-storage';
