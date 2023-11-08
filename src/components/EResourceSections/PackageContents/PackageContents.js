6b5c64bef53a093f6d6d40e65ada0abaab358027
MonireRasouli <monireh.rasouli@k-int.co.uk>
2023-11-08 23:31:54 +0000

    ERM-3092, refactored to functional component

--- src/components/EResourceSections/PackageContents/PackageContents.js
+++ src/components/EResourceSections/PackageContents/PackageContents.js
@@ -19,136 +19,132 @@ import Coverage from '../../Coverage';
 import EResourceLink from '../../EResourceLink';
 import { resultCount } from '../../../constants';
 
-export default class PackageContents extends React.Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      areContentsLoading: PropTypes.bool,
-      packageContents: PropTypes.arrayOf(PropTypes.object),
-      packageContentsCount: PropTypes.number,
-      packageContentsFilter: PropTypes.string,
-      searchString: PropTypes.string
-    }),
-    id: PropTypes.string,
-    onFilterPackageContents: PropTypes.func.isRequired,
-    onNeedMorePackageContents: PropTypes.func.isRequired,
+const propTypes = {
+  data: PropTypes.shape({
+    areContentsLoading: PropTypes.bool,
+    packageContents: PropTypes.arrayOf(PropTypes.object),
+    packageContentsCount: PropTypes.number,
+    packageContentsFilter: PropTypes.string,
+    searchString: PropTypes.string
+  }),
+  id: PropTypes.string,
+  onFilterPackageContents: PropTypes.func.isRequired,
+  onNeedMorePackageContents: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+const PackageContents = ({
+  data: { areContentsLoading, packageContents, packageContentsCount },
+  data,
+  id,
+  isLoading,
+  onNeedMorePackageContents,
+  onFilterPackageContents
+}) => {
+  const renderDate = date => (date ? <FormattedUTCDate value={date} /> : '');
+
+  const renderBadge = () => {
+    const count = packageContentsCount ?? 0;
+    return (count !== undefined && !isLoading) ? <Badge>{count}</Badge> : <Spinner />;
   };
 
-  columnMapping = {
-    name: <FormattedMessage id="ui-agreements.eresources.name" />,
-    platform: <FormattedMessage id="ui-agreements.eresources.platform" />,
-    coverage: <FormattedMessage id="ui-agreements.eresources.coverage" />,
-    accessStart: <FormattedMessage id="ui-agreements.eresources.accessStart" />,
-    accessEnd: <FormattedMessage id="ui-agreements.eresources.accessEnd" />,
-  };
-
-  columnWidths = {
-    name: { min: 200, max: 400 },
-    coverage: { min: 250, max: 320 },
-  }
-
-  formatter = {
-    name: pci => {
-      return (
-        <AppIcon
-          app="agreements"
-          iconAlignment="baseline"
-          iconKey="pci"
-          size="small"
-        >
-          <EResourceLink eresource={pci.pti.titleInstance} searchString={this.props.data?.searchString} />
-        </AppIcon>
-      );
-    },
-    platform: pci => {
-      const pti = pci.pti ?? {};
-      const { name, platform, url } = pti;
-
-      return (
-        <TitleOnPlatformLink
-          id={pci.id}
-          name={name}
-          platform={platform?.name}
-          url={url}
-        />
-      );
-    },
-    coverage: pci => <Coverage pci={pci} />,
-    accessStart: pci => this.renderDate(pci.accessStart),
-    accessEnd: pci => this.renderDate(pci.accessEnd),
-  };
-
-  visibleColumns = ['name', 'platform', 'coverage', 'accessStart', 'accessEnd'];
-
-  renderList = (packageContents, packageContentsCount) => {
+  const renderList = () => {
     return (
       <MultiColumnList
-        columnMapping={this.columnMapping}
-        columnWidths={this.columnWidths}
+        columnMapping={{
+          name: <FormattedMessage id="ui-agreements.eresources.name" />,
+          platform: <FormattedMessage id="ui-agreements.eresources.platform" />,
+          coverage: <FormattedMessage id="ui-agreements.eresources.coverage" />,
+          accessStart: <FormattedMessage id="ui-agreements.eresources.accessStart" />,
+          accessEnd: <FormattedMessage id="ui-agreements.eresources.accessEnd" />,
+        }}
+        columnWidths={{
+          name: { min: 200, max: 400 },
+          coverage: { min: 250, max: 320 },
+        }}
         contentData={packageContents}
-        formatter={this.formatter}
+        formatter={{
+          name: pci => {
+            return (
+              <AppIcon
+                app="agreements"
+                iconAlignment="baseline"
+                iconKey="pci"
+                size="small"
+              >
+                <EResourceLink eresource={pci.pti.titleInstance} searchString={data?.searchString} />
+              </AppIcon>
+            );
+          },
+          platform: pci => {
+            const pti = pci.pti ?? {};
+            const { name, platform, url } = pti;
+
+            return (
+              <TitleOnPlatformLink
+                id={pci.id}
+                name={name}
+                platform={platform?.name}
+                url={url}
+              />
+            );
+          },
+          coverage: pci => <Coverage pci={pci} />,
+          accessStart: pci => renderDate(pci.accessStart),
+          accessEnd: pci => renderDate(pci.accessEnd),
+        }}
         id="package-contents-list"
         interactive={false}
-        onNeedMoreData={this.props.onNeedMorePackageContents}
+        onNeedMoreData={onNeedMorePackageContents}
         pageAmount={resultCount.RESULT_COUNT_INCREMENT}
         pagingType="click"
         totalCount={packageContentsCount}
-        visibleColumns={this.visibleColumns}
+        visibleColumns={['name', 'platform', 'coverage', 'accessStart', 'accessEnd']}
       />
     );
   };
 
-  renderDate = date => (date ? <FormattedUTCDate value={date} /> : '');
-
-  renderBadge = () => {
-    const { packageContentsCount: count, areContentsLoading: isLoading } = this.props.data;
-    return (count !== undefined && !isLoading) ? <Badge>{count}</Badge> : <Spinner />;
-  };
-
-  renderFilterButton = filter => (
+  const renderFilterButton = (filter) => (
     <Button
       buttonStyle={
-        this.props.data.packageContentsFilter === filter ? 'primary' : 'default'
+        data.packageContentsFilter === filter ? 'primary' : 'default'
       }
       id={`clickable-pci-${filter}`}
-      onClick={() => this.props.onFilterPackageContents(filter)}
+      onClick={() => onFilterPackageContents(filter)}
     >
       <FormattedMessage id={`ui-agreements.content.${filter}`} />
     </Button>
   );
 
-  renderFilterButtons = () => (
+  const renderFilterButtons = () => (
     <Layout className="textCentered">
       <ButtonGroup>
-        {this.renderFilterButton('current')}
-        {this.renderFilterButton('future')}
-        {this.renderFilterButton('dropped')}
-        {this.renderFilterButton('all')}
+        {renderFilterButton('current')}
+        {renderFilterButton('future')}
+        {renderFilterButton('dropped')}
+        {renderFilterButton('all')}
       </ButtonGroup>
     </Layout>
   );
 
-  render() {
-    const {
-      data: { areContentsLoading, packageContents, packageContentsCount },
-      id,
-    } = this.props;
+  return (
+    <Accordion
+      displayWhenClosed={renderBadge()}
+      displayWhenOpen={renderBadge()}
+      id={id}
+      label={
+        <FormattedMessage id="ui-agreements.eresources.packageResources" />
+      }
+    >
+      {renderFilterButtons()}
+      {(packageContents && !areContentsLoading) ? (
+        renderList(packageContents, packageContentsCount)
+      ) : (
+        <Spinner />
+      )}
+    </Accordion>
+  );
+};
 
-    return (
-      <Accordion
-        displayWhenClosed={this.renderBadge()}
-        displayWhenOpen={this.renderBadge()}
-        id={id}
-        label={
-          <FormattedMessage id="ui-agreements.eresources.packageResources" />
-        }
-      >
-        {this.renderFilterButtons()}
-        {(packageContents && !areContentsLoading) ? (
-          this.renderList(packageContents, packageContentsCount)
-        ) : (
-          <Spinner />
-        )}
-      </Accordion>
-    );
-  }
-}
+PackageContents.propTypes = propTypes;
+export default PackageContents;
