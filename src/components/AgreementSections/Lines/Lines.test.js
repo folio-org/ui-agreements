1b0d0667e32bd7fa4b0674655e23ac6d57a8bed2
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-11-04 16:48:07 +0000

    test: Lines routing test
    Added an example test which uses MemoryRouter to allow for testing of "to" buttons

--- src/components/AgreementSections/Lines/Lines.test.js
+++ src/components/AgreementSections/Lines/Lines.test.js
@@ -1,19 +1,39 @@
 
 import { waitFor } from '@folio/jest-config-stripes/testing-library/react';
-import { Accordion, Button, MultiColumnList, renderWithIntl } from '@folio/stripes-erm-testing';
-import { MemoryRouter } from 'react-router-dom';
+import { Accordion, Button, renderWithIntl } from '@folio/stripes-erm-testing';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 import translationsProperties from '../../../../test/helpers';
 import Lines from './Lines';
 import { agreement, handlers } from './testResources';
 
+jest.mock('../CoveredEResourcesList', () => () => (
+  <div>CoveredEResourcesList</div>
+));
+
+jest.mock('../LinesList', () => () => (
+  <div>LinesList</div>
+));
+
 describe('Lines', () => {
+  let renderComponent;
   beforeEach(() => {
-    renderWithIntl(
+    renderComponent = renderWithIntl(
+      /* EXAMPLE testing "to" redirects using memory router */
       <MemoryRouter>
-        <Lines
-          agreement={agreement}
-          handlers={handlers}
-        />
+        <Switch>
+          <Route path="/erm/agreements/:agreementId/line/create">
+            Create agreement line
+          </Route>
+          <Route path="/erm/agreementLines">
+            View agreement lines
+          </Route>
+          <Route path="/">
+            <Lines
+              agreement={agreement}
+              handlers={handlers}
+            />
+          </Route>
+        </Switch>
       </MemoryRouter>,
       translationsProperties
     );
@@ -23,10 +43,27 @@ describe('Lines', () => {
     await Accordion('Agreement lines').exists();
   });
 
-  describe('Actions menu', () => {
+  test('renders LinesList component', async () => {
+    const { getByText } = renderComponent;
+    await waitFor(() => {
+      expect(getByText('LinesList')).toBeInTheDocument();
+    });
+  });
+
+  test('renders CoveredEResourcesList component', async () => {
+    const { getByText } = renderComponent;
+    await waitFor(() => {
+      expect(getByText('CoveredEResourcesList')).toBeInTheDocument();
+    });
+  });
+
+  test('actions menu exists', async () => {
+    await Button('Actions').exists();
+  });
+
+  describe('opening actions menu', () => {
     beforeEach(async () => {
       await waitFor(async () => {
-        await Button('Actions').exists();
         await Button('Actions').click();
       });
     });
@@ -42,13 +79,43 @@ describe('Lines', () => {
         await Button('View in agreement lines search').exists();
       });
     });
-  });
 
-  test('renders Lines list MCL', async () => {
-    await MultiColumnList('agreement-lines').exists();
-  });
+    test('renders ColumnManagerMenu component', async () => {
+      const { getByText } = renderComponent;
+      await waitFor(() => {
+        expect(getByText('ColumnManagerMenu')).toBeInTheDocument();
+      });
+    });
 
-  test('renders covered e-resources MCL', async () => {
-    await MultiColumnList('eresources-covered').exists();
+
+    describe('clicking new agreement line button', () => {
+      beforeEach(async () => {
+        await waitFor(async () => {
+          await Button('New agreement line').click();
+        });
+      });
+
+      test('redirected to agreement line screen', async () => {
+        const { getByText } = renderComponent;
+        await waitFor(async () => {
+          expect(getByText('Create agreement line')).toBeInTheDocument();
+        });
+      });
+    });
+
+    describe('clicking view in agreement lines search button', () => {
+      beforeEach(async () => {
+        await waitFor(async () => {
+          await Button('View in agreement lines search').click();
+        });
+      });
+
+      test('redirected to agreement line screen', async () => {
+        const { getByText } = renderComponent;
+        await waitFor(async () => {
+          expect(getByText('View agreement lines')).toBeInTheDocument();
+        });
+      });
+    });
   });
 });
