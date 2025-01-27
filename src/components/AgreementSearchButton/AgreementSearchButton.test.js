9951b3eec1754399f7fa79fe45e997519f172083
MonireRasouli <monireh.rasouli@k-int.co.uk>
2025-01-27 21:27:47 +0000

    Extend test for AgreementSearchButton

--- src/components/AgreementSearchButton/AgreementSearchButton.test.js
+++ src/components/AgreementSearchButton/AgreementSearchButton.test.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import { useRef as mockUseRef } from 'react';
 import { MemoryRouter } from 'react-router-dom';
+
+import { Pluggable } from '@folio/stripes/core';
 import {
   renderWithIntl,
 } from '@folio/stripes-erm-testing';
@@ -7,21 +9,47 @@ import {
 import AgreementSearchButton from './AgreementSearchButton';
 import translationsProperties from '../../../test/helpers';
 
+const mockOnClick = jest.fn();
+const mockOnAgreementSelected = jest.fn();
+
+const MockPluggable = ({ children, renderTrigger }) => {
+  const buttonRef = mockUseRef();
+  return (
+    <>
+      {renderTrigger({
+        buttonRef,
+        onClick: mockOnClick,
+        id: 'test-id'
+      })}
+      {children}
+    </>
+  );
+};
+Pluggable.mockImplementation(MockPluggable);
 describe('AgreementSearchButton', () => {
   let renderComponent;
   beforeEach(() => {
     renderComponent = renderWithIntl(
       <MemoryRouter>
         <AgreementSearchButton
-          disabled={false}
+          buttonProps={{ id: 'test-button' }}
+          disabled
           name="agreement"
+          onAgreementSelected={mockOnAgreementSelected}
         />
       </MemoryRouter>,
       translationsProperties
     );
   });
 
-  it('renders the expected text', () => {
+  it('renders the button as disabled when the disabled prop is true', () => {
+    const { getByRole } = renderComponent;
+    const button = getByRole('button', { name: /select agreement/i });
+
+    expect(button).toBeDisabled();
+  });
+
+  it('renders the fallback message when no plugin is available', () => {
     const { getByText } = renderComponent;
     expect(getByText('No agreement plugin')).toBeInTheDocument();
   });
