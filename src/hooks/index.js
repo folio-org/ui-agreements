264a818cfdcbbdacda636ad591dc646afebe630b
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2025-05-14 15:30:19 +0100

    ERM-3704, Hide tags icon when tags are disabled (#1415)

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
 export { default as useAcqMethods } from './useAcqMethods';
 export { default as useAgreementsContexts } from './useAgreementsContexts';
-export { default as useAgreementsHelperApp } from './useAgreementsHelperApp';
 export { default as useAgreementsSettings } from './useAgreementsSettings';
 export { default as useChunkedOrderLines } from './useChunkedOrderLines';
 export { default as useSuppressFromDiscovery } from './useSuppressFromDiscovery';
