c8a3bf78f953dd67aa065a4f27a40801e223ee47
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-05-21 14:40:28 +0100

    refactor: Nested hook code
    Refactor to slightly clean up nested logic to shut sonarlint up

--- src/hooks/useAgreementsDisplaySettings/useAgreementsDisplaySettings.js
+++ src/hooks/useAgreementsDisplaySettings/useAgreementsDisplaySettings.js
@@ -41,6 +41,23 @@ const useAgreementsDisplaySettings = ({
   };
 
   const parsedSettings = useMemo(() => parseAgreementDisplaySettings(rawSettings) ?? {}, [rawSettings]);
+
+  // A helper function to take a list of updates, and combine them with oldSettings
+  // to come up with expected return shape from future get. We will use this to
+  // directly update the query cache and have an immediate renderable view of what
+  // the values _should_ be while they refetch in the background
+  const optimisticUpdateNewSettings = useCallback((updates, oldSettings) => {
+    const newSettings = [...(oldSettings || [])];
+    updates.forEach(update => {
+      const index = newSettings.findIndex(s => s.id === update.id);
+      if (index > -1) {
+        newSettings[index] = update;
+      }
+    });
+
+    return newSettings;
+  }, []);
+
   const onSubmit = useCallback((values) => {
     const updates = [];
 
@@ -70,26 +87,24 @@ const useAgreementsDisplaySettings = ({
       return Promise.all(updates.map(update => handleSubmit(update)))
         .then(() => {
           // Optimistically update the cache
-          queryClient.setQueriesData(baseQueryKey, oldSettings => {
-            // Create a new array to avoid direct mutation
-            const newSettings = [...(oldSettings || [])];
-            updates.forEach(update => {
-              const index = newSettings.findIndex(s => s.id === update.id);
-              if (index > -1) {
-                newSettings[index] = update;
-              }
-            });
-
-            return newSettings;
-          });
+          queryClient.setQueriesData(baseQueryKey, oldSettings => optimisticUpdateNewSettings(updates, oldSettings));
         })
         .then(() => {
           queryClient.invalidateQueries(baseQueryKey);
         });
     }
 
+    // In case we somehow called this with no updates
+    // (Shouldn't be possible because of the disabled save button),
+    // return an empty promise
     return Promise.resolve(true);
-  }, [baseQueryKey, handleSubmit, queryClient, rawSettings]);
+  }, [
+    baseQueryKey,
+    handleSubmit,
+    optimisticUpdateNewSettings,
+    queryClient,
+    rawSettings
+  ]);
 
   return {
     parsedSettings,
