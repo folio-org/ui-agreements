cb4be44b59f7998b9f130da9ea0e8f206fc91ecc
Claudia Malzer <42971992+CalamityC@users.noreply.github.com>
2025-07-04 12:26:12 +0200

    ERM-3727 On saving Agreements display settings the user should get a toast message per changed setting (#1423)
    * ERM-3727 On saving Agreements display settings the user should get a toast message per changed setting
    
    * add new translations
    * change existing translations according to pattern
    * add callout messages
    
    * * fix test
    
    * * extend test
    
    * remove screen import from test
    
    * add timeout to toast messages
    
    * refactor: Minor tweaks to share callout logic and ensure consistent return for failed messages
    
    ---------
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
    Co-authored-by: Ethan Freestone <ethan.freestone@k-int.co.uk>

--- src/hooks/useAgreementsDisplaySettings/useAgreementsDisplaySettings.js
+++ src/hooks/useAgreementsDisplaySettings/useAgreementsDisplaySettings.js
@@ -1,6 +1,8 @@
 import { useCallback, useMemo } from 'react';
 import { useQueryClient } from 'react-query';
+import { useIntl } from 'react-intl';
 import get from 'lodash/get';
+import { useCallout } from '@folio/stripes/core';
 
 import { useSettingSection } from '@k-int/stripes-kint-components';
 
@@ -12,6 +14,8 @@ import getAgreementsSettingsField from './getAgreementsSettingsField';
 const useAgreementsDisplaySettings = ({
   namespaceAppend = []
 } = {}) => {
+  const callout = useCallout();
+  const intl = useIntl();
   const queryClient = useQueryClient();
   const baseQueryKey = useMemo(() => [
     'ERM',
@@ -46,9 +50,9 @@ const useAgreementsDisplaySettings = ({
   // to come up with expected return shape from future get. We will use this to
   // directly update the query cache and have an immediate renderable view of what
   // the values _should_ be while they refetch in the background
-  const optimisticUpdateNewSettings = useCallback((updates, oldSettings) => {
+  const updateSettingQueryCache = useCallback((updates, oldSettings) => {
     const newSettings = [...(oldSettings || [])];
-    updates.forEach(update => {
+    updates.forEach(({ update }) => {
       const index = newSettings.findIndex(s => s.id === update.id);
       if (index > -1) {
         newSettings[index] = update;
@@ -84,27 +88,72 @@ const useAgreementsDisplaySettings = ({
     });
 
     if (updates.length > 0) {
-      return Promise.all(updates.map(update => handleSubmit(update)))
+      const successful = [];
+      const failed = [];
+
+      // type MUST be 'success' or 'error'
+      const sendSettingsCallout = (type) => {
+        callout.sendCallout({
+          type,
+          timeout: 10000,
+          message: (
+            <ul>
+              {(type === 'success' ? successful : failed).map(({ update, error: _error }) => {
+                // error will obviously be null for successful updates.
+                // We can choose to surface the errors to the user should we wish
+                const translationId = getAgreementsSettingsField(update.key);
+                const settingLabel = intl.formatMessage({
+                  id: `ui-agreements.settings.${translationId}`,
+                  fallbackMessage: update.key
+                });
+
+                return (
+                  <li key={update.id || update.key}>
+                    {intl.formatMessage(
+                      { id: `ui-agreements.settings.update.${type}` },
+                      { settingLabel, newValue: update.value }
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          )
+        });
+      };
+
+      const updatePromises = updates.map(update => handleSubmit(update)
         .then(() => {
-          // Optimistically update the cache
-          queryClient.setQueriesData(baseQueryKey, oldSettings => optimisticUpdateNewSettings(updates, oldSettings));
+          // Ensure same shape for these objects in failure AND success
+          successful.push({ update });
         })
+        .catch((error) => {
+          failed.push({ update, error });
+        }));
+
+      return Promise.all(updatePromises)
         .then(() => {
+          if (successful.length > 0) {
+            sendSettingsCallout('success');
+            // Optimistically update the cache
+            queryClient.setQueriesData(baseQueryKey, oldSettings => updateSettingQueryCache(successful, oldSettings));
+          }
+
+          if (failed.length > 0) {
+            sendSettingsCallout('error');
+            // Pessimistically update the cache
+            queryClient.setQueriesData(baseQueryKey, oldSettings => updateSettingQueryCache(failed, oldSettings));
+          }
+
+          // Refetch in the background to ensure we're up to date with the DB
           queryClient.invalidateQueries(baseQueryKey);
+          return true;
         });
     }
-
     // In case we somehow called this with no updates
     // (Shouldn't be possible because of the disabled save button),
     // return an empty promise
     return Promise.resolve(true);
-  }, [
-    baseQueryKey,
-    handleSubmit,
-    optimisticUpdateNewSettings,
-    queryClient,
-    rawSettings
-  ]);
+  }, [rawSettings, callout, intl, handleSubmit, queryClient, baseQueryKey, updateSettingQueryCache]);
 
   return {
     parsedSettings,
