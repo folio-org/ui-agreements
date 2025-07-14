1e01f874ddf44b7ffae9ce5f0bde1f995d53502e
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-07-14 16:57:54 +0200

    ERM-3755 Build search results transformation
    * add otherids congig entry
    * add special formatter for publicationDates
    * add new translations
    * change GOKB translations to uppercase only
    * add pane title and icon

--- src/routes/GokbRoute/GokbRoute.js
+++ src/routes/GokbRoute/GokbRoute.js
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
 import kyImport from 'ky';
-
 import { JSONPath } from 'jsonpath-plus';
-
 import handlebars from 'handlebars';
 
+import { AppIcon } from '@folio/stripes/core';
+import { FormattedUTCDate, Icon } from '@folio/stripes/components';
 import { ColumnManagerMenu, useColumnManager } from '@folio/stripes/smart-components';
 
 import { SASQRoute } from '@k-int/stripes-kint-components';
@@ -46,8 +46,6 @@ const GokbRoute = ({ location }) => {
     label: <FormattedMessage id={`ui-agreements.gokb.${col.name}`} />
   }));
 
-  console.log('resultColumns', resultColumns);
-
   // EXAMPLE: Using JSONPath to format the results
   // const formatter = {
   //   name: (resource) => JSONPath({ path: '$.name', json: resource }),
@@ -69,30 +67,58 @@ const GokbRoute = ({ location }) => {
         col.name,
         (resource) => {
           const result = JSONPath({ path: col.value.expression, json: resource });
-          // console.log(`formatter[${col.name}]`, result);
           return col.joinStrategy === 'Comma'
             ? (result || []).filter(Boolean).join(', ')
             : result;
         }
       ])
-
   );
+
+  const specialFormatter = {
+    publicationDates: (resource) => {
+      const { dateFirstOnline, dateFirstInPrint, publishedFrom, publishedTo } = resource;
+
+      return (
+        <div>
+          {dateFirstOnline && (
+            <div>
+              <FormattedMessage id="ui-agreements.gokb.publicationDates.firstOnline" />:{' '}
+              <FormattedUTCDate value={dateFirstOnline} />
+            </div>
+          )}
+          {dateFirstInPrint && (
+            <div>
+              <FormattedMessage id="ui-agreements.gokb.publicationDates.firstInPrint" />:{' '}
+              <FormattedUTCDate value={dateFirstInPrint} />
+            </div>
+          )}
+          {(publishedFrom || publishedTo) && (
+            <div>
+              <FormattedMessage id="ui-agreements.gokb.publicationDates.publishedFromTo" />:{' '}
+              <span>
+                {publishedFrom ? <FormattedUTCDate value={publishedFrom} /> : '*'}{' '}
+                <Icon icon="arrow-right" size="small" />{' '}
+                {publishedTo ? <FormattedUTCDate value={publishedTo} /> : '*'}
+              </span>
+            </div>
+          )}
+        </div>
+      );
+    },
+  };
 
   const formatter = {
     ...stringFormatter,
-    ...arrayFormatter
+    ...arrayFormatter,
+    ...specialFormatter
   };
-
-  console.log('formatter', formatter);
 
   const columnMapping = resultColumns?.length
     ? Object.fromEntries(resultColumns.map(col => [col.propertyPath, col.label]))
     : {};
 
-  console.log('columnMapping', columnMapping);
   const { visibleColumns, toggleColumn } = useColumnManager('gokb-search-list-column-manager', columnMapping);
 
-  console.log('visibleColumns', visibleColumns);
   const renderActionMenu = () => {
     return (
       <ColumnManagerMenu
@@ -124,10 +150,12 @@ const GokbRoute = ({ location }) => {
       }}
       mainPaneProps={{
         actionMenu: renderActionMenu,
+        appIcon: <AppIcon app="agreements" iconKey="package" size="small" />,
         id: 'gokb-search-main-pane',
-        paneTitle: <FormattedMessage id="ui-agreements.gokb" />,
+        paneTitle: <FormattedMessage id="ui-agreements.gokb.packages" />,
       }}
       mclProps={{
+        columnWidths: { publicationDates: 300 },
         formatter,
         visibleColumns
       }}
@@ -137,6 +165,7 @@ const GokbRoute = ({ location }) => {
       }}
       queryParameterGenerator={generateQuery}
       resultColumns={resultColumns}
+      sasqProps={{ initialSortState: { sort: 'name' } }}
       searchFieldAriaLabel="input-gokb-search"
     />
   );
