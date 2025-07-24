b6a15eeba4df18f487585fd5b9f28b3c8d4c1b9a
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-07-24 14:44:33 +0200

    remove unused import in testfile

--- src/routes/utilities/getResultsDisplayConfig.test.js
+++ src/routes/utilities/getResultsDisplayConfig.test.js
@@ -1,5 +1,4 @@
 import { JSONPath } from 'jsonpath-plus';
-import handlebars from 'handlebars';
 import getResultsDisplayConfig from './getResultsDisplayConfig';
 
 jest.mock('jsonpath-plus', () => ({
