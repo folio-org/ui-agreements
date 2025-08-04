e52b391e7a0325d6fe2e8d63eeb8ba23c14c6fbc
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2025-08-04 09:39:58 +0100

    ERM-3754, Build search component transformation (#1430)
    * ERM-3754, Build search component transformation
    
    * tweak
    
    * tweak
    
    * removed title Remove default 'Title' from componentType in query generation
    
    * Refactor query generation to use searchString for input
    
    * added util searchTransformation file and translations
    
    * Refactor search transformation logic and update translations
    
    * tweak
    
    * remove unwanted columns
    
    * added searchKey to key prop on SASQRoute
    
    * fix translation, name and parameter for altname
    
    * add tests for the utility functions
    
    * Removed potentially problematic parameters
    
    * create a search component builder utility
    
    * add test for searchComponentBuilder
    
    * refactor: Changes
    
    * chore: Linting
    
    * fix: State now correctly updates
    
    * fix: Query handling
    
    ---------
    
    Co-authored-by: Jack Golding <94838251+Jack-Golding@users.noreply.github.com>
    Co-authored-by: Jack Golding <jack.golding@k-int.com>

--- src/routes/GokbRoute/GokbRoute.js
+++ src/routes/GokbRoute/GokbRoute.js
@@ -4,37 +4,53 @@ import kyImport from 'ky';
 
 import { JSONPath } from 'jsonpath-plus';
 
-import handlebars from 'handlebars';
-
 import { SASQRoute } from '@k-int/stripes-kint-components';
+import config from '../../../docs/gokb-search-v1';
+
+import { searchConfigTypeHandler } from '../utilities/adjustments/searchConfigConstructor';
 
 const GokbRoute = ({ location }) => {
+  // Build search configuration from the config file
+  const searchConfig = config.configuration.results.fetch.search;
+
+  const { searchParameterParse, HeaderComponent } = searchConfigTypeHandler({
+    type: searchConfig.type,
+    searchConfig,
+  });
+
   const fetchParameters = {
-    endpoint: 'https://gokbt.gbv.de/gokb/rest/titles',
-    SASQ_MAP: {
-      searchKey: 'uuid',
-    },
+    endpoint: 'https://gokbt.gbv.de/gokb/api/find',
+    SASQMap: {},
   };
 
+  // Function to generate the GOKb query string based on the current state
+  // Not very happy with this at the moment,its a bit more a bespoke piece of work and doesnt adjust to the searchConfig
+  // Something to revisit in the future, once we have all the query parts in place
   const generateQuery = (params, query) => {
-    const offset = (params.page - 1) * params.perPage;
+    // Additionally this offset handlinig work needs to be dynamically handled based on search config
+    const perPage = params?.perPage || 25;
+    const offset = (params.page - 1) * perPage;
 
-    // EXAMPLE: Using handlebars to generate the query string
-    // Namely name will be the field configured by the results.fetch.search key and its "handlebars template type"
-    // max & offset are configured by the pagination parameters
-    const template = handlebars.compile(
-      '?name={{input}}&max={{perPage}}&offset={{offset}}&es=true'
-    );
+    const queryParts = [];
 
-    return template({
-      input: query?.query || '',
-      perPage: params?.perPage,
-      offset,
-    });
+    if (query?.query) {
+      const { key: searchKey, string: searchString } = searchParameterParse(
+        query?.query
+      );
+      if (searchString) {
+        queryParts.push(searchString);
+        fetchParameters.SASQMap = {
+          ...fetchParameters.SASQMap,
+          searchKey,
+        };
+      }
+    }
+
+    queryParts.push(`max=${perPage}`);
+    queryParts.push(`offset=${offset}`);
+    return `?${queryParts.join('&')}`;
   };
 
-  // When building the SASQ from the config file, using the results.display values
-  // should construct a formatter and resultColumns object
   const resultColumns = [
     {
       propertyPath: 'name',
@@ -42,7 +58,6 @@ const GokbRoute = ({ location }) => {
     },
   ];
 
-  // EXAMPLE: Using JSONPath to format the results
   const formatter = {
     name: (resource) => JSONPath({ path: '$.name', json: resource }),
   };
@@ -50,17 +65,19 @@ const GokbRoute = ({ location }) => {
   return (
     <SASQRoute
       fetchParameters={fetchParameters}
+      FilterPaneHeaderComponent={HeaderComponent}
       filterPaneProps={{
         id: 'gokb-search-main-filter-pane',
       }}
       id="gokb-search"
-      lookupQueryPromise={({ _ky, queryParams, endpoint }) => kyImport.get(`${endpoint}${queryParams}`).json()
-      }
+      lookupQueryPromise={({ _ky, queryParams, endpoint }) => {
+        return kyImport.get(`${endpoint}${queryParams}`).json();
+      }}
       lookupResponseTransform={(data) => {
         const transformedData = {
           ...data,
-          totalRecords: data._pagination.total,
-          results: data?.data,
+          totalRecords: data.count,
+          results: data.records,
         };
         return transformedData;
       }}
---
+++ src/routes/utilities/adjustments/searchConfigConstructor.js
@@ -0,0 +1,77 @@
+import { FormattedMessage } from 'react-intl';
+import { Select } from '@folio/stripes/components';
+import handlebars from 'handlebars';
+import { useState } from 'react';
+
+const handleBarsTransform = (searchConfig, query) => {
+  const template = handlebars.compile(searchConfig.templateString);
+  return template({ query });
+};
+
+// Since this is reusing handlebars logic, should 1000% be moved elsewhere
+const searchConfigOptionsHandler = (type, searchConfig, query) => {
+  switch (type) {
+    case 'Handlebars':
+      return handleBarsTransform(searchConfig, query);
+    default:
+      return null;
+  }
+};
+
+const QueryDropdown = ({ options }) => {
+  const searchLookup = options.reduce(
+    (acc, curr) => ({
+      ...acc,
+      [curr.name]: { ...curr },
+    }),
+    {}
+  );
+
+  const searchOptions = options.map((option) => {
+    return {
+      label: (
+        <FormattedMessage
+          id={`ui-agreements.gokbSearch.searchIndex.${option.name}`}
+        />
+      ),
+      value: option.name,
+    };
+  });
+
+  const defaultValue = options.find((opt) => opt.default === true)?.name;
+  const [searchKey, setSearchKey] = useState(defaultValue);
+
+  // This should also potentially be returning an sasqRenderProps object
+  return {
+    searchParameterParse: (query) => {
+      const searchQueryString = searchConfigOptionsHandler(
+        searchLookup[searchKey]?.type,
+        searchLookup[searchKey],
+        query
+      );
+
+      return { key: searchKey, string: searchQueryString };
+    },
+    HeaderComponent: () => (
+      <Select
+        dataOptions={searchOptions}
+        id="search-dropdown"
+        onChange={(e) => {
+          setSearchKey(searchLookup[e.target.value]?.name);
+        }}
+        value={searchKey || defaultValue}
+      />
+    ),
+  };
+};
+
+export const searchConfigTypeHandler = ({ type, searchConfig }) => {
+  switch (type) {
+    case 'queryDropdown':
+      return QueryDropdown({
+        options: searchConfig.options,
+      });
+    default:
+      return null;
+  }
+};
