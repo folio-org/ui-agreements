ca543f968add59ccf1267314fe8e880bf8412a05
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-08-07 17:09:12 +0100

    refactor: Remove now-unnecessary select prop from useClaimPolicies, as endpoint returns have been tweaked

--- src/components/AccessControl/hooks/useClaimPolicies.js
+++ src/components/AccessControl/hooks/useClaimPolicies.js
@@ -1,7 +1,6 @@
 import { useMemo } from 'react';
 
 import { useQuery } from 'react-query';
-import omit from 'lodash/omit';
 
 import { useOkapiKy } from '@folio/stripes/core';
 
@@ -14,22 +13,7 @@ const useClaimPolicies = ({
   const { data: { claimPolicies = [] } = {} } = useQuery(
     ['AccessControl', 'claimPolicies', endpoint],
     () => ky.get(`${endpoint}/claimPolicies`).json(),
-    {
-      select: (policyData) => { // This will be UNNECESSARY after snapshot rebuilds
-        if (policyData.claimPolicyIds) {
-          return {
-            ...omit(
-              policyData,
-              'claimPolicyIds'
-            ),
-            claimPolicies: policyData.claimPolicyIds
-          };
-        }
-
-        return policyData;
-      },
-      ...queryOptions,
-    }
+    queryOptions
   );
 
   // Also give an option flattening to list of policies with type injected... in the same shape as the policy claim list
