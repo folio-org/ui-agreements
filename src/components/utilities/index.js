8b5a3fdee430b7d824b07f5a4c6affa0e7230090
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-08-20 12:25:43 +0100

    chore: Flat out remove unused links that used to point to MOD_SETTINGS
    Also remove optionalOkpaiInterface dependency on configuration

--- src/components/utilities/index.js
+++ src/components/utilities/index.js
@@ -4,7 +4,6 @@ export { default as getConflictWarnings } from './getConflictWarnings';
 export { default as formatNoteReferrerEntityData } from './formatNoteReferrerEntityData';
 export { default as isDetached } from './isDetached';
 export { default as isExternal } from './isExternal';
-export { default as withSuppressFromDiscovery } from './withSuppressFromDiscovery';
 export { default as urls } from './urls';
 export { default as validators } from './validators';
 export { default as parseMclPageSize } from './parseMclPageSize';
