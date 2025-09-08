54a76c4b3051d8021ee3bc38f5d772017b824060
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-09-08 14:34:46 +0100

    refactor: BasketList
    Refactor BasketList to functional component, and ready it for basket resource type-specific logic

--- src/components/BasketList/BasketList.js
+++ src/components/BasketList/BasketList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { uniqueId } from 'lodash';
 import { FormattedMessage } from 'react-intl';
@@ -13,102 +13,131 @@ import {
 import { EResourceType } from '@folio/stripes-erm-components';
 import Coverage from '../Coverage';
 import EResourceLink from '../EResourceLink';
+import { resourceClasses } from '../../constants';
 
-class BasketList extends React.Component {
-  static propTypes = {
-    basket: PropTypes.arrayOf(PropTypes.object),
-    onRemoveItem: PropTypes.func,
-    onToggleAll: PropTypes.func,
-    onToggleItem: PropTypes.func,
-    selectedItems: PropTypes.object,
-  }
+const BasketList = ({
+  basket,
+  onRemoveItem,
+  onToggleAll,
+  onToggleItem,
+  selectedItems
+}) => {
+  const getTextName = useCallback((basketItem) => {
+    if (
+      basketItem.class &&
+      basketItem.class === resourceClasses.PACKAGE
+    ) {
+      return basketItem._object.name ?? basketItem.name;
+    }
 
-  getName = (eresource) => {
-    const pti = eresource?._object?.pti ?? eresource?.pti;
-    const name = pti?.titleInstance?.name ?? eresource.name;
+    if (
+      basketItem.class &&
+      basketItem.class === resourceClasses.PCI
+    ) {
+      return (basketItem?._object?.pti ?? basketItem?.pti)?.titleInstance?.name ?? basketItem.name;
+    }
 
-    return name;
-  }
+    return basketItem.name;
+  }, []);
 
-  render() {
-    const { basket, onRemoveItem, selectedItems } = this.props;
+  const renderName = useCallback((basketItem) => {
+    if (
+      basketItem.class &&
+      (
+        basketItem.class === resourceClasses.PACKAGE ||
+        basketItem.class === resourceClasses.PCI
+      )
+    ) {
+      return <EResourceLink eresource={basketItem} />;
+    }
 
-    return (
-      <MultiColumnList
-        columnMapping={{
-          selected: (
-            <Checkbox
-              checked={Object.values(selectedItems).includes(false) !== true}
-              name="selected-all"
-              onChange={this.props.onToggleAll}
-            />
-          ),
-          name: <FormattedMessage id="ui-agreements.eresources.name" />,
-          publicationType: <FormattedMessage id="ui-agreements.eresources.publicationType" />,
-          package: <FormattedMessage id="ui-agreements.eresources.parentPackage" />,
-          platform: <FormattedMessage id="ui-agreements.eresources.platform" />,
-          coverage: <FormattedMessage id="ui-agreements.eresources.coverage" />,
-          action: <FormattedMessage id="ui-agreements.action" />,
-        }}
-        columnWidths={{
-          coverage: { min: 250, max: 320 },
-          name: 300,
-          package: { max: 400 },
-        }}
-        contentData={basket}
-        formatter={{
-          selected: resource => (
-            <Checkbox
-              checked={!!(selectedItems[resource.id])}
-              name={`selected-${resource.id}`}
-              onChange={() => this.props.onToggleItem(resource)}
-            />
-          ),
-          name: resource => <EResourceLink eresource={resource} />,
-          publicationType: resource => <EResourceType resource={resource} />,
-          package: resource => {
-            const pkg = resource?._object?.pkg;
-            if (!pkg) return null;
+    // If in doubt, fallback to basketItem name
+    return basketItem.name;
+  }, []);
 
-            return <EResourceLink eresource={pkg} />;
-          },
-          platform: resource => (
-            resource?._object?.pti?.platform?.name ||
-            resource?._object?.nominalPlatform?.name || null
-          ),
-          // The resource below fits the same shape as the eresources in an agreement line, so we pass them in the eResource prop.
-          coverage: resource => <Coverage eResource={resource} />,
-          action: resource => (
-            <Tooltip
-              id={uniqueId('removeBasketItemBtn')}
-              text={<FormattedMessage id="ui-agreements.basket.removeFromBasket" values={{ publicationType: <EResourceType resource={resource} />, name: this.getName(resource) }} />}
-            >
-              {({ ariaIds, ref }) => (
-                <IconButton
-                  ref={ref}
-                  aria-labelledby={ariaIds.text}
-                  icon="trash"
-                  onClick={() => onRemoveItem(resource)}
-                />
-              )}
-            </Tooltip>
-          )
-        }}
-        interactive={false}
-        maxHeight={400}
-        rowUpdater={resource => selectedItems[resource.id]}
-        visibleColumns={[
-          'selected',
-          'name',
-          'publicationType',
-          'package',
-          'platform',
-          'coverage',
-          'action'
-        ]}
-      />
-    );
-  }
-}
+  return (
+    <MultiColumnList
+      columnMapping={{
+        selected: (
+          <Checkbox
+            checked={Object.values(selectedItems).includes(false) !== true}
+            name="selected-all"
+            onChange={onToggleAll}
+          />
+        ),
+        name: <FormattedMessage id="ui-agreements.eresources.name" />,
+        publicationType: <FormattedMessage id="ui-agreements.eresources.publicationType" />,
+        package: <FormattedMessage id="ui-agreements.eresources.parentPackage" />,
+        platform: <FormattedMessage id="ui-agreements.eresources.platform" />,
+        coverage: <FormattedMessage id="ui-agreements.eresources.coverage" />,
+        action: <FormattedMessage id="ui-agreements.action" />,
+      }}
+      columnWidths={{
+        coverage: { min: 250, max: 320 },
+        name: 300,
+        package: { max: 400 },
+      }}
+      contentData={basket}
+      formatter={{
+        selected: resource => (
+          <Checkbox
+            checked={!!(selectedItems[resource.id])}
+            name={`selected-${resource.id}`}
+            onChange={() => onToggleItem(resource)}
+          />
+        ),
+        name: renderName,
+        publicationType: resource => <EResourceType resource={resource} />,
+        package: resource => {
+          const pkg = resource?._object?.pkg;
+          if (!pkg) return null;
+
+          return <EResourceLink eresource={pkg} />;
+        },
+        platform: resource => (
+          resource?._object?.pti?.platform?.name ||
+          resource?._object?.nominalPlatform?.name || null
+        ),
+        // The resource below fits the same shape as the eresources in an agreement line, so we pass them in the eResource prop.
+        coverage: resource => <Coverage eResource={resource} />,
+        action: resource => (
+          <Tooltip
+            id={uniqueId('removeBasketItemBtn')}
+            text={<FormattedMessage id="ui-agreements.basket.removeFromBasket" values={{ publicationType: <EResourceType resource={resource} />, name: getTextName(resource) }} />}
+          >
+            {({ ariaIds, ref }) => (
+              <IconButton
+                ref={ref}
+                aria-labelledby={ariaIds.text}
+                icon="trash"
+                onClick={() => onRemoveItem(resource)}
+              />
+            )}
+          </Tooltip>
+        )
+      }}
+      interactive={false}
+      maxHeight={400}
+      rowUpdater={resource => selectedItems[resource.id]}
+      visibleColumns={[
+        'selected',
+        'name',
+        'publicationType',
+        'package',
+        'platform',
+        'coverage',
+        'action'
+      ]}
+    />
+  );
+};
+
+BasketList.propTypes = {
+  basket: PropTypes.arrayOf(PropTypes.object),
+  onRemoveItem: PropTypes.func,
+  onToggleAll: PropTypes.func,
+  onToggleItem: PropTypes.func,
+  selectedItems: PropTypes.object,
+};
 
 export default BasketList;
