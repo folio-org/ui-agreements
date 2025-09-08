3972f457872de26cb475a1e11874e9039b9a98b7
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-09-08 12:30:09 +0100

    feat: Add ability to render Tooltip around IconDropdown menu

--- src/components/IconDropdown/IconDropdown.js
+++ src/components/IconDropdown/IconDropdown.js
@@ -1,11 +1,21 @@
 import noop from 'lodash/noop';
-import { Button, Dropdown, DropdownMenu, Icon, IconButton } from '@folio/stripes/components';
+import {
+  Button,
+  Dropdown,
+  DropdownMenu,
+  Icon,
+  IconButton, Tooltip,
+} from '@folio/stripes/components';
 
 // IconSelect isn't quite right since this isn't a select its an action menu... no central component for this rn
 // FIXME we should add this to kint components or erm components probably
 const IconDropdown = ({
   icon = 'ellipsis',
-  options = []
+  options = [],
+  triggerProps: {
+    tooltipProps = {},
+    ...triggerProps
+  } = {}
 }) => {
   return (
     <Dropdown
@@ -26,18 +36,34 @@ const IconDropdown = ({
         );
       }}
       renderTrigger={({ onToggle, triggerRef, keyHandler, ariaProps, getTriggerProps }) => {
-        return (
+        const renderIconButtonWithRef = (theRef, iconButtonProps = {}) => (
           <IconButton
-            ref={triggerRef}
-            icon="ellipsis"
+            ref={theRef}
+            icon={icon}
             marginBottom0
             onClick={onToggle}
             onKeyDown={keyHandler}
             type="button"
             {...getTriggerProps()}
             {...ariaProps}
+            {...triggerProps}
+            {...iconButtonProps}
           />
         );
+
+        if (Object.keys(tooltipProps).length > 0) {
+          return (
+            <Tooltip
+              triggerRef={triggerRef}
+              {...tooltipProps}
+            >
+              {({ ref, ariaIds }) => (
+                renderIconButtonWithRef(ref, { ariaLabelledBy: ariaIds.text })
+              )}
+            </Tooltip>
+          );
+        }
+        return renderIconButtonWithRef(triggerRef);
       }}
     />
   );
