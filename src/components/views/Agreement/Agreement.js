1e489ae6a189b3435f79821e9155f2e0b0ac0e34
MonireRasouli <monireh.rasouli@k-int.co.uk>
2025-09-09 15:42:52 +0100

    ERM-3781, Use acquisition units to modify Actions menus in agreements view

--- src/components/views/Agreement/Agreement.js
+++ src/components/views/Agreement/Agreement.js
@@ -143,12 +143,12 @@ const Agreement = ({
   const getActionMenu = ({ onToggle }) => {
     const buttons = [];
 
-    if (stripes.hasPerm('ui-agreements.agreements.edit') && canEdit !== false) {
+    if (canEdit !== false) {
       buttons.push(
         <Button
           key="clickable-dropdown-edit-agreement"
           buttonStyle="dropdownItem"
-          disabled={canEditLoading}
+          disabled={!stripes.hasPerm('ui-agreements.agreements.edit') || canEditLoading}
           id="clickable-dropdown-edit-agreement"
           onClick={handlers.onEdit}
         >
@@ -192,12 +192,12 @@ const Agreement = ({
       );
     }
 
-    if (stripes.hasPerm('ui-agreements.agreements.delete') && canDelete !== false) {
+    if (canDelete !== false) {
       buttons.push(
         <Button
           key="clickable-dropdown-delete-agreement"
           buttonStyle="dropdownItem"
-          disabled={canDeleteLoading}
+          disabled={!stripes.hasPerm('ui-agreements.agreements.delete') || canDeleteLoading}
           id="clickable-dropdown-delete-agreement"
           onClick={() => {
             setShowDeleteConfirmationModal(true);
