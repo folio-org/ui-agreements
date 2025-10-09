68144341dc516dd4bec485c32b801096544f2253
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-10-09 21:10:55 +0200

    * fix pagination issue

--- src/registry/RemoteResourceAgreementsList/RemoteResourceAgreementsList.js
+++ src/registry/RemoteResourceAgreementsList/RemoteResourceAgreementsList.js
@@ -3,9 +3,17 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { useOkapiKy } from '@folio/stripes/core';
 
+import { generateKiwtQueryParams, usePrevNextPagination } from '@k-int/stripes-kint-components';
 import EntitlementAgreementsList from '../../components/EntitlementsAgreementsList';
 
-import { ERESOURCE_ENTITLEMENTS_ENDPOINT, TITLES_ELECTRONIC_ENDPOINT } from '../../constants';
+import {
+  ERESOURCE_ENTITLEMENTS_ENDPOINT,
+  TITLES_ELECTRONIC_ENDPOINT,
+  ENTITLEMENT_AGREEMENTS_LIST_PAGINATION_ID
+} from '../../constants';
+
+import { useAgreementsDisplaySettings } from '../../hooks';
+import { parseMclPageSize } from '../../components/utilities';
 
 const propTypes = {
   remoteId: PropTypes.string.isRequired,
@@ -17,11 +25,8 @@ const RemoteResourceAgreementsList = ({
   setBadgeCount,
 }) => {
   const ky = useOkapiKy();
-  let isEmptyMessage;
 
-  const {
-    data: eresourceId,
-  } = useQuery(
+  const { data: eresourceId } = useQuery(
     ['REMOTEKB', 'fetchLocalTitleId', remoteId],
     () => ky.get(`${TITLES_ELECTRONIC_ENDPOINT}?match=identifiers.identifier.value&term=${remoteId}`).json(),
     {
@@ -33,15 +38,29 @@ const RemoteResourceAgreementsList = ({
     }
   );
 
-  const {
-    data: entitlements = [],
-  } = useQuery(
-    ['REMOTEKB', 'fetchEntitlementsForTitle', eresourceId],
-    () => ky.get(ERESOURCE_ENTITLEMENTS_ENDPOINT(eresourceId)).json(),
+  const settings = useAgreementsDisplaySettings({ namespaceAppend: ['entitlementAgreements'] });
+  const pageSize = parseMclPageSize(settings, 'entitlements');
+  const paginationId = `${ENTITLEMENT_AGREEMENTS_LIST_PAGINATION_ID}-${eresourceId || 'pending'}`;
+
+  const { currentPage } = usePrevNextPagination({
+    pageSize,
+    id: paginationId,
+    syncToLocation: false
+  });
+
+  const queryParams = generateKiwtQueryParams({ page: currentPage, perPage: pageSize }, {});
+
+  const { data: { results: entitlements = [], totalRecords: entitlementsCount = 0 } = {} } = useQuery(
+    ['REMOTEKB', 'fetchEntitlementsForTitle', eresourceId, queryParams],
+    () => {
+      const params = [...queryParams];
+      return ky.get(`${ERESOURCE_ENTITLEMENTS_ENDPOINT(eresourceId)}?${params.join('&')}`).json();
+    },
     {
-      enabled: !!eresourceId,
+      enabled: !!eresourceId && !!currentPage && !!pageSize,
       onSuccess: (data) => {
-        setBadgeCount(Array.isArray(data) ? data.length : 0);
+        const count = Array.isArray(data) ? data.length : (data?.totalRecords ?? 0);
+        setBadgeCount(count);
       },
       onError: () => {
         setBadgeCount(0);
@@ -55,7 +74,7 @@ const RemoteResourceAgreementsList = ({
         entitlements={entitlements}
         eresourceId={eresourceId}
         id="remote-resource-agreements-list"
-        isEmptyMessage={isEmptyMessage}
+        totalCount={entitlementsCount}
         visibleColumns={['name', 'type', 'startDate', 'endDate', 'eresource', 'acqMethod', 'coverage', 'isCustomCoverage']}
       />
       : <FormattedMessage id="ui-agreements.remoteKb.remoteTitleNotFound" />
