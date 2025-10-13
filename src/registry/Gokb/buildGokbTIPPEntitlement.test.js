7537ef3af24640b291e032c4e628e396ef1a7b4c
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-10-13 15:27:50 +0200

    * fix  buildGokbTIPPEntitlement test

--- src/registry/Gokb/buildGokbTIPPEntitlement.test.js
+++ src/registry/Gokb/buildGokbTIPPEntitlement.test.js
@@ -1,5 +1,6 @@
 import omit from 'lodash/omit';
 import { gokbTipps } from '../../../test/jest/GOKB';
+import { GOKB_RESOURCE_AUTHORITY, BASKET_TYPE_GOKB_TITLE } from '../../constants';
 import buildGokbTIPPEntitlementOption from './buildGokbTIPPEntitlementOption';
 import { pkgs } from '../../../test/jest/eresources';
 
@@ -9,11 +10,11 @@ describe('buildGokbTIPPEntitlement', () => {
       tipp: gokbTipps[0],
       pkg: pkgs[0],
       label: 'with id',
-      expectedId: gokbTipps[0].id
+      expectedId: gokbTipps[0].tippTitleUuid
     },
     {
       tipp: omit(gokbTipps[0], 'id'),
-      expectedId: gokbTipps[0].uuid,
+      expectedId: gokbTipps[0].tippTitleUuid,
       pkg: pkgs[0],
       label: 'without id'
     }
@@ -29,12 +30,18 @@ describe('buildGokbTIPPEntitlement', () => {
 
     test('outcome is as expected', () => {
       expect(outcome).toEqual({
-        _object: tipp,
         id: expectedId,
         name: tipp.name,
         pkg,
+        publicationType: { label: tipp?.titleType },
+        payload: {
+          type: 'external',
+          authority: GOKB_RESOURCE_AUTHORITY,
+          reference: `${tipp.tippPackageUuid}:${tipp.tippTitleUuid}`,
+          resourceName: tipp.name,
+        },
         tipp,
-        type: 'GOKB_TITLE'
+        type: BASKET_TYPE_GOKB_TITLE
       });
     });
   });
