7e06515b01f0325d5064cc4d03b7d721f97db14d
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-11-04 16:31:55 +0100

    * refactor Info.js

--- src/components/AgreementLineSections/Info/Info.js
+++ src/components/AgreementLineSections/Info/Info.js
@@ -48,213 +48,183 @@ const propTypes = {
   resource: PropTypes.object.isRequired,
 };
 
+const getTitlePlatformUrl = ({ resource, external }) => {
+  if (external) {
+    const url = resource.reference_object?.url;
+    return url ? (
+      <a href={url} rel="noopener noreferrer" target="_blank">{url}</a>
+    ) : <NoValue />;
+  }
+  const url = resource.pti?.url;
+  return url ? (
+    <a href={url} rel="noopener noreferrer" target="_blank">{url}</a>
+  ) : <NoValue />;
+};
+
+const renderTemplatedUrls = (resource) => {
+  const templated = resource?.pti?.templatedUrls ?? [];
+  if (templated.length <= 1) return null;
+
+  const sorted = orderBy(
+    templated.filter(tu => tu?.name !== 'defaultUrl'),
+    'name',
+    'asc'
+  );
+
+  return (
+    <KeyValue label={<FormattedMessage id="ui-agreements.eresources.proxiedAndCustomizedUrls" />}>
+      <MultiColumnList
+        columnMapping={{
+          name: <FormattedMessage id="ui-agreements.eresources.proxiesCustomizers" />,
+          url: <FormattedMessage id="ui-agreements.eresources.url" />,
+        }}
+        contentData={sorted}
+        id="templated-urls"
+        interactive={false}
+        visibleColumns={['name', 'url']}
+      />
+    </KeyValue>
+  );
+};
+
+const renderErrorCard = (resource) => (
+  <ErrorCard
+    cardStyle="positive"
+    error={{ number: resource.reference_object?.error, message: resource.reference_object?.message }}
+    headerStart={(
+      <AppIcon app="e-holdings" size="small">
+        <strong>
+          {(resource.reference && resource.authority)
+            ? `${resource.authority}: ${resource.reference}`
+            : (resource.authority ? resource.authority : resource.reference)}
+        </strong>
+      </AppIcon>
+    )}
+  />
+);
+
+const renderPackageSection = ({ resource, external }) => (
+  <>
+    <Headline size="large" tag="h3">
+      <FormattedMessage id="ui-agreements.eresources.packageDetails" />
+    </Headline>
+    {external ? <PackageCardExternal pkg={resource} /> : <PackageCard pkg={resource} />}
+  </>
+);
+
+const renderTitleSection = ({ resource, external, isGokb }) => (
+  <>
+    <Headline size="large" tag="h3">
+      <FormattedMessage id="ui-agreements.eresources.titleDetails" />
+    </Headline>
+
+    <KeyValue label={<FormattedMessage id="ui-agreements.eresources.titleOnPlatformURL" />}>
+      <div data-test-agreement-line-url>
+        {getTitlePlatformUrl({ resource, external })}
+      </div>
+    </KeyValue>
+
+    {renderTemplatedUrls(resource)}
+
+    {external ? <TitleCardExternal title={resource} /> : <TitleCard title={resource} />}
+
+    <Headline size="large" tag="h3">
+      <FormattedMessage id="ui-agreements.eresources.parentPackageDetails" />
+    </Headline>
+    {(external && !isGokb)
+      ? <PackageCardExternal packageData={resource.reference_object?.packageData ?? {}} />
+      : <PackageCard pkg={resource.pkg ?? {}} />}
+  </>
+);
+
 const Info = ({ isSuppressFromDiscoveryEnabled, line, resource: incomingResource }) => {
   const isGokb = line?.authority === GOKB_RESOURCE_AUTHORITY;
   const lookupReference = isGokb ? incomingResource?.reference : undefined;
   const localPkg = useLocalGokbPkg({ reference: lookupReference });
   const resource = isGokb ? { ...incomingResource, pkg: localPkg } : incomingResource;
 
-  const sortedTemplatedUrls = orderBy(
-    resource?.pti?.templatedUrls,
-    'name',
-    'asc'
-  ).filter((tu) => tu?.name !== 'defaultUrl');
+  const external = isExternal(line);
+  const hasRefErr = !!resource.reference_object?.error;
+  const isPkg = isPackage(resource);
 
   return (
-    <>
-      <div data-testid="lineInfo">
-        <Headline size="x-large" tag="h2">
-          <FormattedMessage id="ui-agreements.line.lineInformation" />
-        </Headline>
-        <MetaSection
-          contentId="agreementLineInfoRecordMetaContent"
-          createdDate={line.dateCreated}
-          hideSource
-          id="agreementLineInfoRecordMeta"
-          lastUpdatedDate={line.lastUpdated}
-        />
-        <KeyValue
-          label={<FormattedMessage id="ui-agreements.line.parentAgreement" />}
-        >
-          <div data-test-agreement-line-agreement>
-            {line.owner?.id ? (
-              <Link to={urls.agreementView(line.owner.id)}>
-                {line.owner.name}
-              </Link>
-            ) : (
-              <NoValue />
-            )}
-          </div>
-        </KeyValue>
-        <Row>
-          <Col md={3} xs={6}>
-            <KeyValue
-              label={
-                <FormattedMessage id="ui-agreements.eresources.activeFrom" />
-              }
-            >
-              <div data-test-agreement-line-active-from>
-                {line?.activeFrom ? (
-                  <FormattedUTCDate value={line?.activeFrom} />
-                ) : (
-                  <NoValue />
-                )}
-              </div>
-            </KeyValue>
-          </Col>
-          <Col md={3} xs={6}>
-            <KeyValue
-              label={
-                <FormattedMessage id="ui-agreements.eresources.activeTo" />
-              }
-            >
-              <div data-test-agreement-line-active-to>
-                {line?.activeTo ? (
-                  <FormattedUTCDate value={line?.activeTo} />
-                ) : (
-                  <NoValue />
-                )}
-              </div>
-            </KeyValue>
-          </Col>
-          {isSuppressFromDiscoveryEnabled('agreementLine') ? (
-            <Col md={3} xs={12}>
-              <KeyValue
-                label={
-                  <FormattedMessage id="ui-agreements.agreementLines.suppressFromDiscovery" />
-                }
-              >
-                <div data-test-agreement-line-suppress-from-discovery>
-                  <FormattedMessage
-                    id={`ui-agreements.${line.suppressFromDiscovery ? 'yes' : 'no'}`}
-                  />
-                </div>
-              </KeyValue>
-            </Col>
-          ) : null}
+    <div data-testid="lineInfo">
+      <Headline size="x-large" tag="h2">
+        <FormattedMessage id="ui-agreements.line.lineInformation" />
+      </Headline>
+
+      <MetaSection
+        contentId="agreementLineInfoRecordMetaContent"
+        createdDate={line.dateCreated}
+        hideSource
+        id="agreementLineInfoRecordMeta"
+        lastUpdatedDate={line.lastUpdated}
+      />
+
+      <KeyValue label={<FormattedMessage id="ui-agreements.line.parentAgreement" />}>
+        <div data-test-agreement-line-agreement>
+          {line.owner?.id ? (
+            <Link to={urls.agreementView(line.owner.id)}>
+              {line.owner.name}
+            </Link>
+          ) : <NoValue />}
+        </div>
+      </KeyValue>
+
+      <Row>
+        <Col md={3} xs={6}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.eresources.activeFrom" />}>
+            <div data-test-agreement-line-active-from>
+              {line?.activeFrom ? <FormattedUTCDate value={line?.activeFrom} /> : <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+
+        <Col md={3} xs={6}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.eresources.activeTo" />}>
+            <div data-test-agreement-line-active-to>
+              {line?.activeTo ? <FormattedUTCDate value={line?.activeTo} /> : <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+
+        {isSuppressFromDiscoveryEnabled('agreementLine') && (
           <Col md={3} xs={12}>
-            <KeyValue label={<FormattedMessage id="ui-agreements.note" />}>
-              <div data-test-agreement-line-note>
-                {line.note ?? <NoValue />}
+            <KeyValue label={<FormattedMessage id="ui-agreements.agreementLines.suppressFromDiscovery" />}>
+              <div data-test-agreement-line-suppress-from-discovery>
+                <FormattedMessage id={`ui-agreements.${line.suppressFromDiscovery ? 'yes' : 'no'}`} />
               </div>
             </KeyValue>
           </Col>
-        </Row>
-        <Row>
-          <Col md={3} xs={12}>
-            <KeyValue
-              label={<FormattedMessage id="ui-agreements.description" />}
-            >
-              <div data-test-agreement-line-description>
-                {line.description ?? <NoValue />}
-              </div>
-            </KeyValue>
-          </Col>
-        </Row>
-        {!isDetached(line) ? (
-          !resource.reference_object?.error ? (
-            isPackage(resource) ? (
-              <>
-                <Headline size="large" tag="h3">
-                  <FormattedMessage id="ui-agreements.eresources.packageDetails" />
-                </Headline>
-                {isExternal(line) ? (
-                  <PackageCardExternal pkg={resource} />
-                ) : (
-                  <PackageCard pkg={resource} />
-                )}
-              </>
-            ) : (
-              <>
-                <Headline size="large" tag="h3">
-                  <FormattedMessage id="ui-agreements.eresources.titleDetails" />
-                </Headline>
-                <KeyValue
-                  label={
-                    <FormattedMessage id="ui-agreements.eresources.titleOnPlatformURL" />
-                  }
-                >
-                  <div data-test-agreement-line-url>
-                    {isExternal(line) ? (
-                      resource.reference_object?.url ? (
-                        <a
-                          href={resource.reference_object.url}
-                          rel="noopener noreferrer"
-                          target="_blank"
-                        >
-                          {resource.reference_object?.url}
-                        </a>
-                      ) : (
-                        <NoValue />
-                      )
-                    ) : resource.pti?.url ? (
-                      <a
-                        href={resource.pti.url}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        {resource.pti?.url}
-                      </a>
-                    ) : (
-                      <NoValue />
-                    )}
-                  </div>
-                </KeyValue>
-                {resource?.pti?.templatedUrls.length > 1 && (
-                  <KeyValue
-                    label={
-                      <FormattedMessage id="ui-agreements.eresources.proxiedAndCustomizedUrls" />
-                    }
-                  >
-                    <MultiColumnList
-                      columnMapping={{
-                        name: (
-                          <FormattedMessage id="ui-agreements.eresources.proxiesCustomizers" />
-                        ),
-                        url: (
-                          <FormattedMessage id="ui-agreements.eresources.url" />
-                        ),
-                      }}
-                      contentData={sortedTemplatedUrls}
-                      id="templated-urls"
-                      interactive={false}
-                      visibleColumns={['name', 'url']}
-                    />
-                  </KeyValue>
-                )}
-                {isExternal(line) ? (
-                  <TitleCardExternal title={resource} />
-                ) : (
-                  <TitleCard title={resource} />
-                )}
-                <Headline size="large" tag="h3">
-                  <FormattedMessage id="ui-agreements.eresources.parentPackageDetails" />
-                </Headline>
-                {(isExternal(line) && !isGokb) ? (
-                  <PackageCardExternal
-                    packageData={resource.reference_object?.packageData ?? {}}
-                  />
-                ) : (
-                  <PackageCard pkg={resource.pkg ?? {}} />
-                )}
-              </>
-            )
-          ) : (
-            <ErrorCard
-              cardStyle="positive"
-              error={{ number: resource.reference_object?.error, message: resource.reference_object?.message }}
-              headerStart={(
-                <AppIcon app="e-holdings" size="small">
-                  <strong>
-                    {(resource.reference && resource.authority) ? `${resource.authority}: ${resource.reference}` : (resource.authority ? resource.authority : resource.reference)}
-                  </strong>
-                </AppIcon>
-              )}
-            />
-          )
-        ) : null}
-      </div>
-    </>
+        )}
+
+        <Col md={3} xs={12}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.note" />}>
+            <div data-test-agreement-line-note>
+              {line.note ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={3} xs={12}>
+          <KeyValue label={<FormattedMessage id="ui-agreements.description" />}>
+            <div data-test-agreement-line-description>
+              {line.description ?? <NoValue />}
+            </div>
+          </KeyValue>
+        </Col>
+      </Row>
+
+      {!isDetached(line) && (
+        hasRefErr
+          ? renderErrorCard(resource)
+          : (isPkg
+            ? renderPackageSection({ resource, external })
+            : renderTitleSection({ resource, external, isGokb }))
+      )}
+    </div>
   );
 };
 
