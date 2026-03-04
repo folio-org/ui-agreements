4050ad437a4a5c60203b7486eef605dd94c07fb3
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-03-04 12:30:42 +0100

    * extend test to cover title

--- src/components/EResourceSections/Identifiers/Identifiers.test.js
+++ src/components/EResourceSections/Identifiers/Identifiers.test.js
@@ -1,15 +1,44 @@
 import orderBy from 'lodash/orderBy';
 
-import { renderWithIntl, MultiColumnList, MultiColumnListCell, MultiColumnListHeader } from '@folio/stripes-erm-testing';
+import {
+  renderWithIntl,
+  MultiColumnList,
+  MultiColumnListCell,
+  MultiColumnListHeader
+} from '@folio/stripes-erm-testing';
 import { MemoryRouter } from 'react-router-dom';
 import translationsProperties from '../../../../test/helpers';
 import Identifiers from './Identifiers';
 
-import { pkgs } from '../../../../test/jest/eresources';
+import { pkgs, tis } from '../../../../test/jest/eresources';
 
-const eresource = pkgs.find(p => p.id === 'eab7a8ea-6665-4f06-a55a-73a5aad36215');
+const pkg = pkgs.find(p => p.id === 'eab7a8ea-6665-4f06-a55a-73a5aad36215');
+const title = tis.find(t => t.id === '38138ae2-9dd9-412a-a0eb-e8890c9d1274');
 
-describe('Identifiers', () => {
+const cases = [
+  {
+    name: 'package',
+    eresource: pkg,
+    typeLabels: {
+      gokb_id: 'GOKB ID',
+      gokb_uuid: 'GOKB UUID',
+    },
+  },
+  {
+    name: 'title',
+    eresource: title,
+    typeLabels: {
+      ezb: 'EZB',
+      gokb_uuid: 'GOKB UUID',
+      issn: 'ISSN',
+      zdb: 'ZDB',
+    },
+  },
+];
+
+describe.each(cases)('Identifiers ($name)', ({ eresource, typeLabels }) => {
+  const typeLabel = (item) => typeLabels[item.identifier.ns.value] || item.identifier.ns.value;
+
   beforeEach(() => {
     renderWithIntl(
       <MemoryRouter>
@@ -43,70 +72,80 @@ describe('Identifiers', () => {
     let sortedByIdentifierDesc;
 
     beforeEach(() => {
-      // We know what the type labels resolve to:
-      // gokb_id   -> "GOKB ID"
-      // gokb_uuid -> "GOKB UUID"
-      const typeLabel = (item) => (
-        item.identifier.ns.value === 'gokb_id' ? 'GOKB ID' : 'GOKB UUID'
-      );
+      const identifiers = eresource.identifiers;
 
-      sortedByTypeAsc = orderBy(eresource.identifiers, [typeLabel], ['asc']);
-      sortedByTypeDesc = orderBy(eresource.identifiers, [typeLabel], ['desc']);
+      sortedByTypeAsc = orderBy(identifiers, [typeLabel], ['asc']);
+      sortedByTypeDesc = orderBy(identifiers, [typeLabel], ['desc']);
 
-      sortedByIdentifierAsc = orderBy(eresource.identifiers, [i => i.identifier.value], ['asc']);
-      sortedByIdentifierDesc = orderBy(eresource.identifiers, [i => i.identifier.value], ['desc']);
+      sortedByIdentifierAsc = orderBy(identifiers, [i => i.identifier.value], ['asc']);
+      sortedByIdentifierDesc = orderBy(identifiers, [i => i.identifier.value], ['desc']);
     });
 
-    test('initial sort is ascending by type', async () => {
-      await MultiColumnListCell({ row: 0, columnIndex: 0 }).has({
-        content: (sortedByTypeAsc[0].identifier.ns.value === 'gokb_id' ? 'GOKB ID' : 'GOKB UUID')
-      });
+    test('initial sort is ascending by type (all rows)', async () => {
+      for (let row = 0; row < sortedByTypeAsc.length; row++) {
+        // eslint-disable-next-line no-await-in-loop
+        await MultiColumnListCell({ row, columnIndex: 0 }).has({
+          content: typeLabel(sortedByTypeAsc[row])
+        });
+      }
     });
 
     describe('clicking Resource identifier type header', () => {
       beforeEach(async () => {
-        await MultiColumnList().clickHeader('Resource identifier type');
+        await MultiColumnList('identifiers-list').clickHeader('Resource identifier type');
       });
 
-      test('sort toggles to descending by type', async () => {
-        await MultiColumnListCell({ row: 0, columnIndex: 0 }).has({
-          content: (sortedByTypeDesc[0].identifier.ns.value === 'gokb_id' ? 'GOKB ID' : 'GOKB UUID')
-        });
+      test('sort toggles to descending by type (all rows)', async () => {
+        for (let row = 0; row < sortedByTypeDesc.length; row++) {
+          // eslint-disable-next-line no-await-in-loop
+          await MultiColumnListCell({ row, columnIndex: 0 }).has({
+            content: typeLabel(sortedByTypeDesc[row])
+          });
+        }
       });
 
       describe('clicking Resource identifier type header again', () => {
         beforeEach(async () => {
-          await MultiColumnList().clickHeader('Resource identifier type');
+          await MultiColumnList('identifiers-list').clickHeader('Resource identifier type');
         });
 
-        test('sort toggles back to ascending by type', async () => {
-          await MultiColumnListCell({ row: 0, columnIndex: 0 }).has({
-            content: (sortedByTypeAsc[0].identifier.ns.value === 'gokb_id' ? 'GOKB ID' : 'GOKB UUID')
-          });
+        test('sort toggles back to ascending by type (all rows)', async () => {
+          for (let row = 0; row < sortedByTypeAsc.length; row++) {
+            // eslint-disable-next-line no-await-in-loop
+            await MultiColumnListCell({ row, columnIndex: 0 }).has({
+              content: typeLabel(sortedByTypeAsc[row])
+            });
+          }
         });
       });
     });
 
     describe('clicking Resource identifier header', () => {
       beforeEach(async () => {
-        await MultiColumnList().clickHeader('Resource identifier');
+        await MultiColumnList('identifiers-list').clickHeader('Resource identifier');
       });
 
-      test('sort is ascending by identifier', async () => {
-        await MultiColumnListCell({ row: 0, columnIndex: 1 }).has({
-          content: sortedByIdentifierAsc[0].identifier.value
-        });
+      test('sort is ascending by identifier (all rows)', async () => {
+        for (let row = 0; row < sortedByIdentifierAsc.length; row++) {
+          // eslint-disable-next-line no-await-in-loop
+          await MultiColumnListCell({ row, columnIndex: 1 }).has({
+            content: sortedByIdentifierAsc[row].identifier.value
+          });
+        }
       });
 
       describe('clicking Resource identifier header again', () => {
         beforeEach(async () => {
-          await MultiColumnList().clickHeader('Resource identifier');
+          await MultiColumnList('identifiers-list').clickHeader('Resource identifier');
         });
 
-        test('sort toggles to descending by identifier', async () => {
-          await MultiColumnListCell({ row: 0, columnIndex: 1 }).has({
-            content: sortedByIdentifierDesc[0].identifier.value
-          });
+        test('sort toggles to descending by identifier (all rows)', async () => {
+          for (let row = 0; row < sortedByIdentifierDesc.length; row++) {
+            // eslint-disable-next-line no-await-in-loop
+            await MultiColumnListCell({ row, columnIndex: 1 }).has({
+              content: sortedByIdentifierDesc[row].identifier.value
+            });
+          }
         });
       });
     });
