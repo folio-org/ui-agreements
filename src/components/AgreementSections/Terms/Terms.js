66966bf338514962e0e05d60e1eec55a5984ea8b
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-03-12 17:10:40 +0100

    ERM-3953 Implement a ' parallel batched fetch' approach for retrieving custom properties in Agreements and Licenses
    * convert Terms component from class to functional
    * use new useCustProps hook

--- src/components/AgreementSections/Terms/Terms.js
+++ src/components/AgreementSections/Terms/Terms.js
@@ -2,29 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Accordion } from '@folio/stripes/components';
-import { CustomPropertyCard } from '@k-int/stripes-kint-components';
+import { CustomPropertyCard, useCustProps } from '@k-int/stripes-kint-components';
 
-export default class Terms extends React.Component {
-  static propTypes = {
-    agreement: PropTypes.shape({
-      linkedLicenses: PropTypes.arrayOf(PropTypes.shape({
-        remoteId_object: PropTypes.object.isRequired,
-        status: PropTypes.shape({
-          value: PropTypes.string,
-        }).isRequired,
-      })),
-    }),
-    id: PropTypes.string,
-    terms: PropTypes.arrayOf(PropTypes.object),
-  }
+import { useHasLicensesInterface } from '../../../hooks';
+import { LICENSE_CUSTPROP_ENDPOINT } from '../../../constants';
 
-  static defaultProps = {
-    terms: [],
-  }
+const Terms = ({ agreement, id }) => {
+  const { hasLicensesInterface } = useHasLicensesInterface();
+  const { linkedLicenses = [] } = agreement || {};
 
-  renderTermsList = (controllingLicense) => {
-    const customProperties = controllingLicense.remoteId_object?.customProperties || [];
-    const terms = this.props.terms;
+  const { custprops: terms = [] } = useCustProps({
+    endpoint: LICENSE_CUSTPROP_ENDPOINT,
+    options: {
+      sort: [
+        { path: 'retired' },
+        { path: 'primary', direction: 'desc' },
+        { path: 'label' }
+      ]
+    },
+    queryParams: {
+      enabled: linkedLicenses?.length > 0 && hasLicensesInterface
+    }
+  });
+
+  const controllingLicense = linkedLicenses.find(l => l.status.value === 'controlling');
+
+  const renderTermsList = (license) => {
+    const customProperties = license.remoteId_object?.customProperties || [];
+
     return (
       <div>
         {terms.map((term, index) => (
@@ -38,26 +43,31 @@ export default class Terms extends React.Component {
         ))}
       </div>
     );
-  }
+  };
 
-  render() {
-    const {
-      agreement: { linkedLicenses = [] },
-      id,
-    } = this.props;
+  return (
+    <Accordion
+      id={id}
+      label={<FormattedMessage id="ui-agreements.agreements.licenseAndBusTerms" />}
+    >
+      {controllingLicense ?
+        renderTermsList(controllingLicense) :
+        <FormattedMessage id="ui-agreements.emptyAccordion.licenseAndBusinessTerms" />
+      }
+    </Accordion>
+  );
+};
 
-    const controllingLicense = linkedLicenses.find(l => l.status.value === 'controlling');
+Terms.propTypes = {
+  agreement: PropTypes.shape({
+    linkedLicenses: PropTypes.arrayOf(PropTypes.shape({
+      remoteId_object: PropTypes.object.isRequired,
+      status: PropTypes.shape({
+        value: PropTypes.string,
+      }).isRequired,
+    })),
+  }),
+  id: PropTypes.string,
+};
 
-    return (
-      <Accordion
-        id={id}
-        label={<FormattedMessage id="ui-agreements.agreements.licenseAndBusTerms" />}
-      >
-        {controllingLicense ?
-          this.renderTermsList(controllingLicense) :
-          <FormattedMessage id="ui-agreements.emptyAccordion.licenseAndBusinessTerms" />
-        }
-      </Accordion>
-    );
-  }
-}
+export default Terms;
