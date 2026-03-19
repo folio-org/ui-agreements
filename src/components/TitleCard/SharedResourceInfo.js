0ce8580cfc939cd90d9c96f88a18f33ab18fc8f7
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-03-19 15:12:03 +0100

    * remove unnecessary react jsx fragment

--- src/components/TitleCard/SharedResourceInfo.js
+++ src/components/TitleCard/SharedResourceInfo.js
@@ -30,32 +30,30 @@ const propTypes = {
 const SharedResourceInfo = ({
   titleInstance
 }) => (
-  <>
-    <Row>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.publicationType" />}>
-          <div data-test-title-instance-publication-type>
-            <EResourceType resource={titleInstance} />
-          </div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.materialType" />}>
-          <div data-test-title-instance-sub-type>{titleInstance.subType?.label ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifier" />}>
-          <div data-test-title-instance-work-identifier>{titleInstance.work?.sourceIdentifier?.identifier?.value ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifierType" />}>
-          <div data-test-title-instance-work-identifier>{titleInstance.work?.sourceIdentifier?.identifier?.ns?.value ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-    </Row>
-  </>
+  <Row>
+    <Col xs={3}>
+      <KeyValue label={<FormattedMessage id="ui-agreements.eresources.publicationType" />}>
+        <div data-test-title-instance-publication-type>
+          <EResourceType resource={titleInstance} />
+        </div>
+      </KeyValue>
+    </Col>
+    <Col xs={3}>
+      <KeyValue label={<FormattedMessage id="ui-agreements.eresources.materialType" />}>
+        <div data-test-title-instance-sub-type>{titleInstance.subType?.label ?? <NoValue />}</div>
+      </KeyValue>
+    </Col>
+    <Col xs={3}>
+      <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifier" />}>
+        <div data-test-title-instance-work-identifier>{titleInstance.work?.sourceIdentifier?.identifier?.value ?? <NoValue />}</div>
+      </KeyValue>
+    </Col>
+    <Col xs={3}>
+      <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifierType" />}>
+        <div data-test-title-instance-work-identifier>{titleInstance.work?.sourceIdentifier?.identifier?.ns?.value ?? <NoValue />}</div>
+      </KeyValue>
+    </Col>
+  </Row>
 );
 
 SharedResourceInfo.propTypes = propTypes;
