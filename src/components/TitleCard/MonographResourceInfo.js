5bcce7f63f23567ee11c45c4784cb4116461941b
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-03-19 14:43:25 +0100

    * fix 'duplication on new code' sonar cloud complaint

--- src/components/TitleCard/MonographResourceInfo.js
+++ src/components/TitleCard/MonographResourceInfo.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import {
@@ -9,6 +8,7 @@ import {
   Row,
 } from '@folio/stripes/components';
 import EResourceIdentifier from '../EResourceIdentifier';
+import SharedResourceInfo from './SharedResourceInfo';
 
 const propTypes = {
   titleInstance: PropTypes.shape({
@@ -35,28 +35,7 @@ const MonographResourceInfo = ({
   titleInstance
 }) => (
   <>
-    <Row>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.publicationType" />}>
-          <div data-test-title-instance-publication-type>{titleInstance.publicationType?.label ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.materialType" />}>
-          <div data-test-title-instance-sub-type>{titleInstance.subType?.label ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifier" />}>
-          <div data-test-title-instance-work-identifier>{titleInstance.work.sourceIdentifier.identifier.value ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifierType" />}>
-          <div data-test-title-instance-work-identifier>{titleInstance.work.sourceIdentifier.identifier.ns.value ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-    </Row>
+    <SharedResourceInfo titleInstance={titleInstance} />
     <Row>
       <Col xs={3}>
         <KeyValue label={<FormattedMessage id="ui-agreements.eresources.firstAuthor" />}>
--- src/components/TitleCard/SerialResourceInfo.js
+++ src/components/TitleCard/SerialResourceInfo.js
@@ -1,14 +1,8 @@
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
-import {
-  Col,
-  KeyValue,
-  NoValue,
-  Row,
-} from '@folio/stripes/components';
+import { Row } from '@folio/stripes/components';
 
-import { EResourceType } from '@folio/stripes-erm-components';
 import EResourceIdentifier from '../EResourceIdentifier';
+import SharedResourceInfo from './SharedResourceInfo';
 
 const propTypes = {
   titleInstance: PropTypes.shape({
@@ -22,30 +16,7 @@ const SerialResourceInfo = ({
   titleInstance
 }) => (
   <>
-    <Row>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.publicationType" />}>
-          <div data-test-title-instance-publication-type>
-            <EResourceType resource={titleInstance} />
-          </div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.materialType" />}>
-          <div data-test-title-instance-sub-type>{titleInstance.subType?.label ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifier" />}>
-          <div data-test-title-instance-work-identifier>{titleInstance.work.sourceIdentifier.identifier.value ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-      <Col xs={3}>
-        <KeyValue label={<FormattedMessage id="ui-agreements.eresources.workIdentifierType" />}>
-          <div data-test-title-instance-work-identifier>{titleInstance.work.sourceIdentifier.identifier.ns.value ?? <NoValue />}</div>
-        </KeyValue>
-      </Col>
-    </Row>
+    <SharedResourceInfo titleInstance={titleInstance} />
     <Row>
       <EResourceIdentifier titleInstance={titleInstance} type="ezb" />
       <EResourceIdentifier titleInstance={titleInstance} type="zdb" />
