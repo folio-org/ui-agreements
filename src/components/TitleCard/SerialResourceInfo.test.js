a8a6b4b43545572c6ecc804f0ac8549ffe935a6a
Claudia Malzer <42971992+CalamityC@users.noreply.github.com>
2026-03-23 18:22:53 +0100

    feat(UX): Add Work Source Identifier and Work Source Identifier namespace to Title view -- ERM-3973 (#1532)
    * ERM-3973 Add Work Source Identifier and Work Source Identifier namespace to Title view
    
    * add work identifier to title information
    
    * * extend tests
    
    * * remove console log
    
    * * fix 'duplication on new code' sonar cloud complaint
    
    * * add missing values tests
    
    * * remove unnecessary react jsx fragment
    
    * Resolve merge conflict in en.json
    
    ---------
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>

--- src/components/TitleCard/SerialResourceInfo.test.js
+++ src/components/TitleCard/SerialResourceInfo.test.js
@@ -4,7 +4,9 @@ import { KeyValue, renderWithIntl } from '@folio/stripes-erm-testing';
 
 import translationsProperties from '../../../test/helpers';
 import SerialResourceInfo from './SerialResourceInfo';
-import { serialTitleWithRelatedTitles } from './testResources';
+import { tis } from '../../../test/jest/eresources';
+
+const serialTitle = tis.find(ti => ti.id === '38138ae2-9dd9-412a-a0eb-e8890c9d1274');
 
 describe('SerialResourceInfo', () => {
   describe('with title resource', () => {
@@ -12,13 +14,13 @@ describe('SerialResourceInfo', () => {
       renderWithIntl(
         <Router>
           <SerialResourceInfo
-            titleInstance={serialTitleWithRelatedTitles.pti.titleInstance}
+            titleInstance={serialTitle}
           />
         </Router>,
         translationsProperties
       );
     });
-    ///
+
     test('renders the expected publicationType', async () => {
       await KeyValue('Publication type').has({ value: 'Journal' });
     });
@@ -27,16 +29,24 @@ describe('SerialResourceInfo', () => {
       await KeyValue('Material type').has({ value: 'Electronic' });
     });
 
+    test('renders the expected workIdentifier', async () => {
+      await KeyValue('Work identifier').has({ value: '00e46664-9d3b-458e-9fff-1f800fbb707e' });
+    });
+
+    test('renders the expected workIdentifier', async () => {
+      await KeyValue('Work identifier type').has({ value: 'gokb' });
+    });
+
     test('renders the expected EZB identifier', async () => {
-      await KeyValue('EZB').has({ value: '38504' });
+      await KeyValue('EZB').has({ value: '65256' });
     });
 
     test('renders the expected ZDB identifier', async () => {
-      await KeyValue('ZDB').has({ value: '2052442-0' });
+      await KeyValue('ZDB').has({ value: '2227388-8' });
     });
 
     test('renders the expected ISSN identifier', async () => {
-      await KeyValue('ISSN').has({ value: '1533-8606' });
+      await KeyValue('ISSN').has({ value: '1758-6569' });
     });
   });
 });
