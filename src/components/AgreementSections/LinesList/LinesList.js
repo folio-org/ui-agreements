2ee3ea2d44592e59dbb811d9c3abd9adb9840424
Yury Saukou <yury_saukou@epam.com>
2026-06-15 09:20:44 +0200

    resolve comment: return formatter back to the component's code

--- src/components/AgreementSections/LinesList/LinesList.js
+++ src/components/AgreementSections/LinesList/LinesList.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-
+import { IfPermission } from '@folio/stripes/core';
 import {
+  FormattedUTCDate,
   MultiColumnList,
+  NoValue,
   Spinner,
+  Tooltip
 } from '@folio/stripes/components';
 
+import { EResourceType } from '@folio/stripes-erm-components';
 import { usePrevNextPagination } from '@k-int/stripes-kint-components';
 
+import Coverage from '../../Coverage';
+import CustomCoverageIcon from '../../CustomCoverageIcon';
+import EResourceLink from '../../EResourceLink';
+import EResourceCount from '../../EResourceCount';
+import EResourceProvider from '../../EResourceProvider';
 import { useAgreementsDisplaySettings } from '../../../hooks';
+import { AGREEMENT_LINES_PAGINATION_ID, LINE_LISTING_COLUMN_MAPPING } from '../../../constants';
 import {
-  AGREEMENT_LINES_PAGINATION_ID,
-  LINE_LISTING_COLUMN_MAPPING,
-} from '../../../constants';
-import { parseMclPageSize } from '../../utilities';
-import getAgreementLinesListFormatter from './getAgreementLinesListFormatter';
+  getResourceFromEntitlement,
+  isDetached,
+  isExternal,
+  urls,
+  parseMclPageSize
+} from '../../utilities';
 
 const propTypes = {
   agreement: PropTypes.shape({
@@ -32,6 +44,20 @@ const propTypes = {
 const columnMapping = {
   ...LINE_LISTING_COLUMN_MAPPING,
   isCustomCoverage: ' ',
+};
+
+const hasReferenceError = (line) => !!line?.reference_object?.error;
+
+const wrapError = (renderFunc) => (agreementLine) => {
+  if (hasReferenceError(agreementLine)) return <></>;
+
+  return renderFunc(agreementLine);
+};
+
+const renderErroredExternalReference = (resource) => {
+  const output = [resource.authority, resource.reference].filter(Boolean).join(': ');
+
+  return resource.resourceName ? `${output} (${resource.resourceName})` : output;
 };
 
 const LinesList = ({
@@ -52,6 +78,47 @@ const LinesList = ({
     syncToLocation: false
   });
 
+  const renderDate = date => (
+    date ? <FormattedUTCDate value={date} /> : ''
+  );
+
+  const renderPOLines = (line) => {
+    if (!line.poLines || !line.poLines.length) return '';
+    if (!orderLines || !orderLines.length || areOrderLinesLoading) return <Spinner />;
+
+    const poLines = line.poLines.map(linePOL => orderLines.find(orderLine => orderLine.id === linePOL.poLineId));
+    if (!poLines.length) return <Spinner />;
+
+    return (
+      <div>
+        {poLines.map((poLine = {}, i) => (
+          !poLine.id ? <Spinner key={i} /> : (
+            <Tooltip
+              key={i}
+              id={`tooltip-${line.id}-${poLine.id}`}
+              placement="left"
+              text={poLine.titleOrPackage}
+            >
+              {({ ref, ariaIds }) => (
+                <div
+                  ref={ref}
+                  aria-labelledby={ariaIds.text}
+                >
+                  <Link
+                    data-test-po-line
+                    to={urls.poLineView(poLine.id)}
+                  >
+                    {poLine.poLineNumber}
+                  </Link>
+                </div>
+              )}
+            </Tooltip>
+          )
+        ))}
+      </div>
+    );
+  };
+
   const rowUpdater = () => orderLines.map(orderLine => orderLine.id);
   return lines ? (
     <MultiColumnList
@@ -62,7 +129,56 @@ const LinesList = ({
         coverage: { min: 250, max: 320 },
       }}
       contentData={lines}
-      formatter={getAgreementLinesListFormatter({ areOrderLinesLoading, orderLines })}
+      formatter={{
+        name: line => {
+          const resource = getResourceFromEntitlement(line);
+          if (!resource) return line.label;
+          if (isDetached(resource)) return resource.description;
+          if (isExternal(resource) && hasReferenceError(resource)) return renderErroredExternalReference(resource);
+          if (isExternal(resource) && !resource.reference_object?.label && !resource?.resourceName) return resource.reference;
+          return (
+            <EResourceLink
+              data-test-external-reference={line.reference}
+              data-test-resource-id={line?.resource?.id}
+              eresource={resource}
+            />
+          );
+        },
+        provider: wrapError((agreementLine) => <EResourceProvider resource={agreementLine.resource || agreementLine} />),
+        publicationType: wrapError((agreementLine) => {
+          const resource = getResourceFromEntitlement(agreementLine);
+          return isDetached(resource) ? <NoValue /> : <EResourceType resource={resource} />;
+        }),
+        activeFrom: wrapError((agreementLine) => <div data-test-active-from>{renderDate(agreementLine.startDate)}</div>),
+        activeTo: wrapError((agreementLine) => <div data-test-active-to>{renderDate(agreementLine.endDate)}</div>),
+        count: wrapError((agreementLine) => <EResourceCount resource={getResourceFromEntitlement(agreementLine)} />),
+        note: wrapError((agreementLine) => <div style={{ overflowWrap: 'break-word', maxWidth: 250, whiteSpace: 'pre-wrap' }}>{agreementLine.note}</div>),
+        coverage: wrapError((agreementLine) => <Coverage line={agreementLine} />),
+        isCustomCoverage: wrapError((agreementLine) => {
+          if (!agreementLine.customCoverage) return <></>;
+
+          return (
+            <Tooltip
+              id={`agreement-line-cc-tooltip-${agreementLine.rowIndex}`}
+              text={<FormattedMessage id="ui-agreements.customcoverages.tooltip" />}
+            >
+              {({ ref, ariaIds }) => <CustomCoverageIcon ref={ref} aria-labelledby={ariaIds.text} />
+              }
+            </Tooltip>
+          );
+        }),
+        poLines: wrapError((agreementLine) => (
+          <IfPermission perm="orders.po-lines.collection.get">
+            {({ hasPermission }) => {
+              if (hasPermission) return renderPOLines(agreementLine);
+
+              return agreementLine?.poLines?.length
+                ? <FormattedMessage id="ui-agreements.agreementLines.noPoLinePerm" />
+                : null;
+            }}
+          </IfPermission>
+        ))
+      }}
       id="agreement-lines"
       isEmptyMessage={<FormattedMessage id="ui-agreements.emptyAccordion.agreementLines" />}
       onRowClick={(e, row) => {
