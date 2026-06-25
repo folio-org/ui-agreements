50894a5059185869e8ece857d1ca3a462724be91
Yury Saukou <yury_saukou@epam.com>
2026-06-25 15:44:06 +0400

    feat: Extend sort in agreement lines search and filter results to include by `resourceName` and `id` -- ERM-4103

--- src/routes/AgreementLinesRoute/AgreementLinesRoute.js
+++ src/routes/AgreementLinesRoute/AgreementLinesRoute.js
@@ -35,6 +35,20 @@ const SEARCH_KEYS_DICT = {
 };
 const BASE_SEARCH_KEYS = [SEARCH_KEYS_DICT.REFERENCE, SEARCH_KEYS_DICT.DESCRIPTION, SEARCH_KEYS_DICT.NOTE];
 
+const SORT_KEYS_DICT = {
+  ID: 'id',
+  REFERENCE: 'reference',
+  RESOURCE_DOT_NAME: 'resource.name',
+  RESOURCE_NAME: 'resourceName',
+};
+// Order of keys in this array determines the order of sorting in the UI
+const SORT_KEYS = [
+  SORT_KEYS_DICT.RESOURCE_DOT_NAME,
+  SORT_KEYS_DICT.RESOURCE_NAME,
+  SORT_KEYS_DICT.REFERENCE,
+  SORT_KEYS_DICT.ID
+];
+
 const AgreementLinesRoute = ({
   children,
   history,
@@ -80,7 +94,7 @@ const AgreementLinesRoute = ({
       page: currentPage,
       perPage: RESULT_COUNT_INCREMENT_MEDIUM,
       sortKeys: {
-        name: 'resource.name,reference',
+        name: SORT_KEYS.join(),
       },
       fetchExternalResources: false,
     }, (query ?? {}))
